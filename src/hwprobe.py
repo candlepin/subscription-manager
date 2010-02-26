629fa9f1094b47838c99bbf36aaa91d0ae2750d1
pkilambi@REDHAT.COM <pkilambi@REDHAT.COM@c9825d83-557f-0410-84f4-87ca3e9d7cb7>
2010-02-26 11:55:06 +0000

    revamp the hwprobe module

--- src/hwprobe.py
+++ src/hwprobe.py
@@ -26,106 +26,6 @@ import gettext
 _ = gettext.gettext
 
 import dmidecode
-dmidict = {}
-
-def read_memory():
-    parser = re.compile(r'^(?P<key>\S*):\s*(?P<value>\d*)\s*kB' )
-    memdict = {}
-    meminfo = open('/proc/meminfo')
-    for info in meminfo:
-        match = parser.match(info)
-        if not match:
-            continue
-        key, value = match.groups(['key', 'value'])
-        memdict["memory."+key.lower().replace(" ", "_")] = int(value)
-
-    return memdict
-
-def read_dmi():
-    global dmidict
-    def _get_dmi_data(func, tag, ddict):
-        for key, value in func.items():
-            for key1,value1 in value['data'].items():
-                ddict[tag+key1.lower().replace(" ", "_")] = value1
-        return ddict
-
-    dmi_data = { 
-        "dmi.bios." : dmidecode.bios(),
-        "dmi.processor." : dmidecode.processor(),
-        "dmi.baseboard." : dmidecode.baseboard(),
-        "dmi.chassis." : dmidecode.chassis(),
-        "dmi.slot."  : dmidecode.slot(),
-        "dmi.system." : dmidecode.system(),
-        "dmi.memory." : dmidecode.memory(),
-        "dmi.connector." : dmidecode.connector(),
-    }
-
-    dmidict = {}
-    for tag, func in dmi_data.items():
-        dmidict = _get_dmi_data(func, tag, dmidict)
-
-    return dmidict
-
-
-def get_virt_info():
-    try:
-        if os.path.exists("/proc/xen/xsd_port"):
-            return get_fully_virt_info()
-    except IOError:
-        pass
-
-    # This is not a virt host system.  Check if it's a para-virt guest.
-    virtinfo = get_para_virt_info()
-    if virtinfo['virt.uuid'] is not None:
-        return virtinfo
-        
-    # This is not a para-virt guest.  Check if it's a fully-virt guest.
-    virtinfo = get_fully_virt_info()
-    if virtinfo['virt.uuid'] is not None:
-        return virtinfo
-
-    # If we got here, we have a system that does not have virtualization
-    # enabled.
-    return {'virt.type' : None, 'virt.uuid' : None}
-
-def get_para_virt_info():
-    virtinfo = {'virt.type' : None, 'virt.uuid' : None}
-    try:
-        uuid_file = open('/sys/hypervisor/uuid', 'r')
-        uuid = uuid_file.read()
-        uuid_file.close()
-        virtinfo['virt.uuid'] = uuid.lower().replace('-', '').rstrip("\r\n")
-        virtinfo['virt.type'] = "para"
-        return virtinfo
-    except IOError:
-        # Failed; must not be para-virt.
-        pass
-
-    return virtinfo
-
-def get_fully_virt_info():
-    vendor = dmidict["dmi.bios.vendor"]
-    uuid =   dmidict["dmi.system.uuid"]
-    virtinfo = {'virt.type' : None, 'virt.uuid' : None}
-    if vendor.lower() == "xen":
-        virtinfo['virt.uuid'] = uuid.lower().replace('-', '')
-        virtinfo['virt.type'] = "fully"
-    return virtinfo
-
-def _is_host_uuid(uuid):
-    uuid = eval('0x%s' % uuid)
-    return long(uuid) == 0L
-
-
-def read_network():
-    netinfo = {}
-    netinfo['network.hostname'] = gethostname()
-    try:
-        netinfo['network.ipaddr']   = gethostbyname(gethostname())
-    except:
-        netinfo['network.ipaddr'] = "127.0.0.1"
-
-    return netinfo
 
 class Hardware:
     def __init__(self):
@@ -134,37 +34,101 @@ class Hardware:
         self.meminfo = {}
         self.cpuinfo = {}
         self.virtinfo = {}
-
-    def getDmiInfo(self):
-        try:
-            self.dmiinfo = read_dmi()
-        except:
-            pass
-        self.allhw.append(self.dmiinfo)
-        return self.dmiinfo
-
-    def getVirtInfo(self):
-        try:
-            self.virtinfo = get_virt_info()
-        except:
-            print _("Error reading virt info:"), sys.exc_type
-        self.allhw.append(self.virtinfo)
-        return self.virtinfo
-
-    def getCpuInfo(self):
-        return self.cpuinfo
+        self.netinfo = {}
 
     def getMemInfo(self):
         try:
-            self.meminfo = read_memory()
+            parser = re.compile(r'^(?P<key>\S*):\s*(?P<value>\d*)\s*kB' )
+            self.meminfo = {}
+            memdata = open('/proc/meminfo')
+            for info in memdata:
+                match = parser.match(info)
+                if not match:
+                    continue
+                key, value = match.groups(['key', 'value'])
+                self.meminfo["memory."+key.lower().replace(" ", "_")] = \
+                                   int(value)
         except:
             print _("Error reading system memory information:"), sys.exc_type
         self.allhw.append(self.meminfo)
         return self.meminfo
 
+    def getDmiInfo(self):
+        try:
+            dmi_data = { 
+                "dmi.bios." : dmidecode.bios(),
+                "dmi.processor." : dmidecode.processor(),
+                "dmi.baseboard." : dmidecode.baseboard(),
+                "dmi.chassis." : dmidecode.chassis(),
+                "dmi.slot."  : dmidecode.slot(),
+                "dmi.system." : dmidecode.system(),
+                "dmi.memory." : dmidecode.memory(),
+                "dmi.connector." : dmidecode.connector(),
+            }
+
+            for tag, func in dmi_data.items():
+                self.dmiinfo = self._get_dmi_data(func, tag, self.dmiinfo)
+        except:
+            pass
+        self.allhw.append(self.dmiinfo)
+        return self.dmiinfo
+
+    def _get_dmi_data(self, func, tag, ddict):
+        for key, value in func.items():
+            for key1, value1 in value['data'].items():
+                ddict[tag+key1.lower().replace(" ", "_")] = value1
+        return ddict
+
+    def getVirtInfo(self):
+        try:
+            if os.path.exists("/proc/xen/xsd_port"):
+                self.virtinfo = self._get_fully_virt_info()
+
+            # Check if it's a para virt guest
+            if not self.virtinfo:
+                self.virtinfo = get_para_virt_info()
+        
+            # This is not a para-virt guest.Check if it's a fully-virt guest.
+            if not self.virtinfo:
+                self.virtinfo = get_fully_virt_info()
+        except:
+            print _("Error reading virt info:"), sys.exc_type
+        self.allhw.append(self.virtinfo)
+        return self.virtinfo
+
+    def _get_para_virt_info(self):
+        virtinfo = {'virt.type' : None, 'virt.uuid' : None}
+        try:
+            uuid_file = open('/sys/hypervisor/uuid', 'r')
+            uuid = uuid_file.read()
+            uuid_file.close()
+            virtinfo['virt.uuid'] = uuid.lower().replace('-', '').rstrip("\r\n")
+            virtinfo['virt.type'] = "para"
+            return virtinfo
+        except IOError:
+            # Failed; must not be para-virt.
+            pass
+        return virtinfo
+
+    def _get_fully_virt_info(self):
+        virtinfo = {'virt.type' : None, 'virt.uuid' : None}
+        vendor = self.dmiinfo["dmi.bios.vendor"]
+        uuid =   self.dmiinfo["dmi.system.uuid"]
+        if vendor.lower() == "xen":
+            virtinfo['virt.uuid'] = uuid.lower().replace('-', '')
+            virtinfo['virt.type'] = "fully"
+        return virtinfo
+
+    def getCpuInfo(self):
+        return self.cpuinfo
+
     def getNetworkInfo(self):
         try:
-            self.netinfo = read_network()
+            self.netinfo['network.hostname'] = gethostname()
+            try:
+                self.netinfo['network.ipaddr']   = gethostbyname(gethostname())
+            except:
+                self.netinfo['network.ipaddr'] = "127.0.0.1"
         except:
             print _("Error reading networking information:"), sys.exc_type
         self.allhw.append(self.netinfo)
