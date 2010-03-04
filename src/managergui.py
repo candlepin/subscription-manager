0988da491f2807a1548cdc8f33473437039f66bf
Pradeep Kilambi <pkilambi@redhat.com>
2010-03-04 18:36:41 -0500

    more changes for gui screens, separating firstboot flow from standalon flow

--- src/managergui.py
+++ src/managergui.py
@@ -15,8 +15,7 @@ _ = gettext.gettext
 gettext.textdomain("subscription-manager")
 gtk.glade.bindtextdomain("subscription-manager")
 
-gladegui = "../data/subsgui.glade"
-gladexml = "../data/subsMgr.glade"
+gladexml = "../data/standalone.glade"
 UEP = None
 class LoginPage:
     def __init__(self):
@@ -95,7 +94,6 @@ class LoginPage:
                  self.loginUname.get_text(), self.loginPw.get_text(),
                  self._get_register_info())
             self._write_consumer_cert(self.consumer)
-            print "consumer Written AAAAAAAAAAAAAAAAAAAA"
             # Try to Auto Subscribe users
             for product in managerlib.getInstalledProductStatus():
                 print "Binding ", product[0]
@@ -135,22 +133,35 @@ class LoginPage:
         f.write(consumerinfo['uuid'])
         f.close()
 
-class ReviewSubscriptionPage:
+class ManageSubscriptionPage:
     def __init__(self):
-        self.reviewSubscriptionXml = gtk.glade.XML(gladexml, "dialog_updates", domain="subscription-manager")
+        self.subsxml = gtk.glade.XML("../data/standalone.glade", "dialog_updates", domain="subscription-manager")
         self.vbox = \
-                        self.reviewSubscriptionXml.get_widget("dialog-vbox1")
+                        self.subsxml.get_widget("dialog-vbox1")
         self.populateProductDialog()
+        self.reviewSubscriptionPagePrepare()
+        dic = { "onFilechooserbuttonFileActivated" : \
+            self.provideCertificatePageApply,
+            }
+
+        self.subsxml.signal_autoconnect(dic)
+
+        self.mainWin = self.subsxml.get_widget("dialog_updates")
+        self.mainWin.connect("delete-event", gtk.main_quit)
+        self.mainWin.connect("hide", gtk.main_quit)
+
+        self.mainWin.show_all()
+
 
     def reviewSubscriptionPagePrepare(self):
         entdir = EntitlementDirectory()
-        self.vbox.show()
+        self.vbox.show_all()
 
     def populateProductDialog(self):
         self.productList = gtk.ListStore(gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING)
         for product in managerlib.getInstalledProductStatus():
             self.productList.append(product)
-        self.tv_products =  self.reviewSubscriptionXml.get_widget("treeview_updates")
+        self.tv_products =  self.subsxml.get_widget("treeview_updates")
         self.tv_products.set_model(self.productList)
         
         self.tv_products.set_rules_hint(True)
@@ -172,164 +183,25 @@ class ReviewSubscriptionPage:
 
         self.productList.set_sort_column_id(0, gtk.SORT_ASCENDING)
 
+    def provideCertificatePageApply(self):
+        self._provideCertificatePageApply()
+
+    def _provideCertificatePageApply(self):
+        """Does what the comment for provideCertificatePageApply says, but might 
+        raise various exceptions.
+        
+        """
+        try:
+            fileChooser = self.subsxml.get_widget("button_import")
+            certFile = fileChooser.get_filename()
+            if certFile is None:
+                pass 
+        except:
+            raise
+
     def reviewSubscriptionPageVbox(self):
         return self.vbox
 
-
-class FinishPage:
-    """The finish screen. This can show two different versions: successful and
-    unsuccessful.
-    """
-    def __init__(self):
-        self.failedFinishXml = gtk.glade.XML(gladexml,
-                                                "failedFinishWindowVbox",
-                                                domain="subscription-manager")
-        self.successfulFinishXml = gtk.glade.XML(gladexml,
-                                                "successfulFinishWindowVbox",
-                                                domain="subscription-manager")
-        self.finishContainerVbox = gtk.VBox()
-        self.failedFinishVbox = \
-                self.failedFinishXml.get_widget("failedFinishWindowVbox")
-        self.successfulFinishVbox = \
-                self.successfulFinishXml.get_widget("successfulFinishWindowVbox")
-        self.finishContainerVbox.pack_start(self.failedFinishVbox)
-
-    def finishPageVbox(self):
-        return self.finishContainerVbox
-
-    def finishPagePrepare(self):
-        #containerChildren = self.finishContainerVbox.get_children()
-        #assert len(containerChildren) == 1
-        self.finishContainerVbox.remove(containerChildren[0])
-        self.finishContainerVbox.pack_start(self.successfulFinishVbox)
-        sys.exit(0)
-
-class ConfirmQuitDialog:
-    def __init__(self):
-        self.xml = gtk.glade.XML(gladexml, "confirmQuitDialog", 
-                                 domain="subscription-manager")
-        self.dialog = self.xml.get_widget("confirmQuitDialog")
-        
-        self.rc = self.dialog.run()
-        if self.rc == gtk.RESPONSE_NONE:
-            self.rc = 0
-        #if self.rc == 1:
-        #    try:
-        #        rhnreg.createSystemRegisterRemindFile()
-        #    except (OSError, IOError), error:
-        #        log.log_me("Reminder file couldn't be written. Details: %s" %
-        #                   error)
-        self.dialog.destroy()
-
-class Gui(LoginPage, ReviewSubscriptionPage, FinishPage):
-    def __init__(self):
-        self.xml = gtk.glade.XML(gladegui, "mainWin", domain="subscription-manager") 
-        self.xml.signal_autoconnect (
-            {"onDruidCancel" : self.onDruidCancel,
-              "onLoginPagePrepare" : self.onLoginPagePrepare,
-              "onLoginPageNext" : self.onLoginPageNext,
-              "onReviewSubscriptionPagePrepare" : self.onReviewSubscriptionPagePrepare,
-              "onReviewSubscriptionPageNext" : self.onReviewSubscriptionPageNext,
-              "onFinishPagePrepare" : self.onFinishPagePrepare,
-              "onFinishPageFinish" : self.onFinishPageFinish,
-        })
-
-        LoginPage.__init__(self)
-        ReviewSubscriptionPage.__init__(self)
-        FinishPage.__init__(self)
-
-        contents = self.loginPageVbox()
-        container = self.xml.get_widget("loginPageVbox")
-        container.pack_start(contents, True)
-
-        contents = self.reviewSubscriptionPageVbox()
-        container = self.xml.get_widget("reviewSubscriptionPageVbox")
-        container.pack_start(contents) #, True)
-
-        contents = self.finishPageVbox()
-        container = self.xml.get_widget("finishPageVbox")
-        container.pack_start(contents, True)
-
-        self.druid = self.xml.get_widget("druid")
-        self.mainWin = self.xml.get_widget("mainWin")
-        self.mainWin.connect("delete-event", gtk.main_quit)
-        self.mainWin.connect("hide", gtk.main_quit)
-
-        self.loginPage = self.xml.get_widget("loginPage")
-        self.reviewSubscriptionPage = self.xml.get_widget("dialog_updates")
-        #self.finishPage = self.xml.get_widget("finishPage")
-
-        def mySetBusyCursor():
-            cursor = gtk.gdk.Cursor(gtk.gdk.WATCH)
-            self.mainWin.window.set_cursor(cursor)
-            while gtk.events_pending():
-                gtk.main_iteration(False)
-        def mySetArrowCursor():
-            cursor = gtk.gdk.Cursor(gtk.gdk.LEFT_PTR)
-            self.mainWin.window.set_cursor(cursor)
-            while gtk.events_pending():
-                gtk.main_iteration(False)
-        self.setBusyCursor = mySetBusyCursor
-        self.setArrowCursor = mySetArrowCursor
-
-        self.mainWin.show_all()
-
-    def onDruidCancel(self, dummy):
-        dialog = ConfirmQuitDialog()
-        if dialog.rc == 1:
-            self.druid.set_page(self.finishPage)
-        else:
-            return True
-
-    def onLoginPagePrepare(self, page, dummy):
-        self.loginPage.emit_stop_by_name("prepare")
-        self.loginXml.get_widget("loginUserEntry").grab_focus()
-        self.loginPagePrepare()
-
-    def onLoginPageNext(self, page, dummy):
-        """This must manually switch pages because another function calls it to
-        advance the druid. It returns True to inform the druid of this.
-        """
-        ret = self.loginPageVerify()
-        if ret:
-            return ret
-
-        ret = self.loginPageApply()
-        if ret:
-            return ret
-
-        self.goToPageAfterLogin()
-        return True
-
-    def goToPageAfterLogin(self):
-        self.druid.set_page(self.reviewSubscriptionPage)
-        return True
-
-    def onReviewSubscriptionPagePrepare(self, page, dummy):
-        self.reviewSubscriptionPagePrepare()
-        #self.druid.set_buttons_sensitive(False, True, False, False)
-        #self.reviewSubscriptionPage.emit_stop_by_name("prepare")
-
-    def onReviewSubscriptionPageNext(self, page, dummy):
-        print "NNNNNNNNNNNNN"
-        self.druid.set_page(self.finishPage)
-        return True
-
-    def onFinishPagePrepare(self, page=None, dummy=None):
-        self.druid.set_buttons_sensitive(False, False, False, False)
-        self.druid.set_show_finish(True)
-        self.finishPage.emit_stop_by_name("prepare")
-        self.druid.finish.set_label(_("_Finish"))
-        title = _("Finish setting up software updates")
-        #else:
-        #    self.druid.finish.set_label(_("_Exit software update setup"))
-        #    title = _("software updates setup unsuccessful")
-        self.finishPagePrepare()
-        self.mainWin.set_title(title)
-        self.finishPage.set_title(title)
-
-    def onFinishPageFinish(self, page, dummy=None):
-        gtk.main_quit()
 
 def unexpectedError(message, exc_info=None):
     #logFile = '/var/log/up2date'
@@ -381,7 +253,7 @@ def main():
         #rootWarning()
         sys.exit(1)
 
-    gui = Gui()
+    gui = ManageSubscriptionPage()
     gtk.main()
 
 
