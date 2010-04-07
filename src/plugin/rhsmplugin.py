673aa1e482ea017f35a22aa1b4ffa3b72eef44f6
Jeff Ortel <jortel@redhat.com>
2010-04-07 13:43:50 -0500

    Remove 'layered product versioning' prototype code.

--- src/plugin/rhsmplugin.py
+++ src/plugin/rhsmplugin.py
@@ -17,82 +17,19 @@
 
 import os, sys
 sys.path.append('/usr/share/rhsm')
-from yum import config
-from yum.plugins import TYPE_CORE, TYPE_INTERACTIVE, PluginYumExit
-from repolib import URL, RepoLib
+from yum.plugins import TYPE_CORE, TYPE_INTERACTIVE
+from repolib import RepoLib
 
 requires_api_version = '2.3'
 plugin_type = (TYPE_CORE, TYPE_INTERACTIVE)
 
 
-class SetVersionCommand:
-
-    def getNames(self):
-        return ["setversion", "set-version"]
-
-    def getUsage(self):
-        return "[repo][version]"
-
-    def getSummary(self):
-        return "Set the value for $version for the specified repo."
-
-    def doCheck(self, base, basecmd, extcmds):
-        if base.conf.uid:
-            raise PluginYumExit, \
-                'You need to be root to perform this command.'
-        if len(extcmds) < 2:
-            raise PluginYumExit, \
-                'You need to specify a repo and version'
-
-    def doCommand(self, base, basecmd, extcmds):
-        print 'set version %s' % str(extcmds)
-        # TODO: call repolib
-        return (0, ('done',))
-
-
-class ListVersionsCommand:
-
-    def getNames(self):
-        return ["listversions", "list-versions"]
-
-    def getUsage(self):
-        return "[repo]"
-
-    def getSummary(self):
-        return "List the available values for $version for the specified repo."
-
-    def doCheck(self, base, basecmd, extcmds):
-        if not len(extcmds):
-            raise PluginYumExit, \
-                'You need to specify a repo.'
-
-    def doCommand(self, base, basecmd, extcmds):
-        result = []
-        # TODO: call repolib
-        return (0, result)
-
-
-def init_hook(conduit):
-    for repo in conduit.getRepos().listEnabled():
-        version = getattr(repo, URL.VERSION[1:])
-        if version:
-            repo.baseurl = URL.replaceVersions(repo.baseurl, version)
-            continue
-        if URL.needsVersion(repo.baseurl):
-            repo.disable()
-
-
 def config_hook(conduit):
-    conduit.registerCommand(SetVersionCommand())
-    conduit.registerCommand(ListVersionsCommand())
-    setattr(config.RepoConf, URL.VERSION[1:], config.Option())
-    return
     try:
         if os.getuid() != 0:
             conduit.info(2, 'Not root, Red Hat repository not updated')
             return
         rl = RepoLib()
         rl.update()
-    except Exception, ex:
-        conduit.error(2, str(ex))
-
+    except Exception, e:
+        conduit.error(2, str(e))
