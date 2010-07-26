734f68059f9ed2d9e789ece26e0a31bfc1312661
Justin Harris <jharris@redhat.com>
2010-07-26 15:56:31 -0400

    Getting the basics of the register screen in and working.
    Still a lot of work to do.

--- src/gui/firstboot/rhms_login.py
+++ src/gui/firstboot/rhms_login.py
@@ -22,56 +22,43 @@ class moduleClass(Module,managergui.RegisterScreen):
     def __init__(self):
         Module.__init__(self)
         self.register_init()
-#        managergui.RegisterScreen.__init__(self)
-        self.priority = 50#this value is relative to when you want to load the screen
-                        # so check other modules before setting
+        self.priority = 50    #this value is relative to when you want to load the screen
+                              # so check other modules before setting
         self.sidebarTitle = _("RHSM Login Screen")
         self.title = _("Subscription Manager Screen")
 
     def register_init(self):
-        UEP = managergui.connection.UEPConnection(managergui.cfg['hostname'] or 'localhost', ssl_port=managergui.cfg['port'])
-        
-
-        self.registerxml = gtk.glade.XML(managergui.gladexml, "register_dialog", domain="subscription-manager")
-        dic = { "on_close_clicked" : self.cancel,
-                "on_register_button_clicked" : self.onRegisterAction, 
-            }
-        self.registerxml.signal_autoconnect(dic)
-#        print self.registerxml.get_widget("dialog-vbox3")
-#        self.registerWin = self.registerxml.get_widget("register_dialog")
-#        self.registerWin.connect("hide", self.cancel)
-#        self.registerWin.show_all()
+        self.UEP = managergui.connection.UEPConnection( \
+            managergui.cfg['hostname'] or 'localhost',  \
+            ssl_port=managergui.cfg['port'])
 
     def apply(self, interface, testing=False):
-        self.register(testing=testing)
-        return RESULT_SUCCESS
-        # Screen setup and loads the logic to run the screen
+        valid_registration = self.register(testing=testing)
 
-    def close_window(self):
-        pass
+        if valid_registration:
+            return RESULT_SUCCESS
+        else:
+            return RESULT_FAILURE
 
     def createScreen(self):
-        print "createScreen"
+        self.registerxml = gtk.glade.XML(managergui.gladexml, "register_dialog", domain="subscription-manager")
+
         self.vbox = gtk.VBox(spacing=10)
-        label = gtk.Label("This is a test label. Blippy. Foobar")
-        label.set_line_wrap(True)
-#        self.vbox.pack_start(label, expand=True, fill=True)
-        print "gh2"
         self.register_dialog = self.registerxml.get_widget("dialog-vbox3")
-        print "gh3"
-#        self.register_dialog.set_parent(None)
         self.register_dialog.reparent(self.vbox)
-        self.vbox.pack_start(self.register_dialog)
-#        self.register_dialog.show()
-        # Invoke your module here
-        # self.mymodule = MyModule()
+
+        self._destroy_widget('register_button')
+        self._destroy_widget('cancel_reg_button1')
 
     def initializeUI(self):
         pass
-        # prepare your screen
-        # self.mymodule.initialize()
 
-    def shouldAppear(self):
-        return True
-        # A boolean return based on condition whether to load this screen or Not
+    def focus(self):
+        # FIXME:  This is currently broken
+        login_text = self.registerxml.get_widget("account_login")
+        login_text.grab_focus()
+
+    def _destroy_widget(self, widget_name):
+        widget = self.registerxml.get_widget(widget_name)
+        widget.destroy()
 
