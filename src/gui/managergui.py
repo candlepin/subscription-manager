67786598eed9ea110661d667caa8fc0877daba28
Devan Goodwin <dgoodwin@redhat.com>
2010-10-14 15:29:53 -0300

    Get all subs treeview operational.

--- src/gui/managergui.py
+++ src/gui/managergui.py
@@ -187,8 +187,28 @@ class MainWindow(object):
         self.all_subs_vbox.reparent(self.all_subs_tab)
         self.all_subs_tab.pack_start(self.all_subs_vbox)
         self.all_subs_vbox.show()
+        self.all_subs_store = gtk.ListStore(str, str, str, str, str)
+        self.all_subs_view = self.all_subs_xml.get_widget('all_subs_treeview')
+        self.all_subs_view.set_model(self.all_subs_store)
+        self._add_column(_("Subscription"), 0)
+        self._add_column(_("# Bundled Products"), 0)
+        self._add_column(_("Total Contracts"), 0)
+        self._add_column(_("Total Subscriptions"), 0)
+        self._add_column(_("Available Subscriptions"), 0)
+        self.load_all_subs()
         
         self.main_window.show_all()
+
+    def load_all_subs(self):
+        self.all_subs_store.clear()
+        self.all_subs_store.append(['RHEL 5', '10', '10,000', '25,000', '1,000'])
+        self.all_subs_store.append(['RHEL 6', '10', '10,000', '25,000', '1,000'])
+
+    def _add_column(self, name, order):
+        column = gtk.TreeViewColumn(name, gtk.CellRendererText(), text=order)
+        self.all_subs_view.append_column(column)
+
+
 
 
 class ManageSubscriptionPage:
