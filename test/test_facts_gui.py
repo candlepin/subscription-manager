cce4a280bea2b6e9cb5c6c3b2a56c49abdbaf096
Devan Goodwin <dgoodwin@redhat.com>
2010-10-19 14:54:05 -0300

    Begin using mocks in tests.
    Uses the mock library, (easy_install mock).
    
    /dev/mem warnings still showing up, need to cleanup the getFacts()
    global code to be able to mock past these I think.

--- test/test_facts_gui.py
+++ test/test_facts_gui.py
@@ -2,33 +2,45 @@ import unittest
 
 import facts
 from gui import factsgui, managergui
+from mock import Mock
 
 class FactDialogTests(unittest.TestCase):
 
-    def test_facts_are_displayed(self):
+    def setUp(self):
+
         expected_facts = { 'fact1': 'one',
                            'fact2': 'two' }
-        found_facts = {}
-
         class StubFacts:
             def get_facts(self):
                 return expected_facts
 
-        def check_facts(fact):
-            found_facts[fact[0]] = fact[1]
+        self.expected_facts = expected_facts
 
         facts.getFacts = lambda: StubFacts()
 
-        dialog = factsgui.SystemFactsDialog()
+        self.consumer = Mock()
+        self.consumer.uuid = "MOCKUUID"
+        self.consumer.name = "MOCK CONSUMER"
+
+    def test_facts_are_displayed(self):
+        found_facts = {}
+
+        def check_facts(fact):
+            found_facts[fact[0]] = fact[1]
+
+        dialog = factsgui.SystemFactsDialog(self.consumer)
         dialog.facts_store.append = check_facts
         dialog.display_facts()
 
-        self.assertEquals(expected_facts, found_facts)
+        self.assertEquals(self.expected_facts, found_facts)
 
     def test_update_button_disabled(self):
-        managergui.consumer = None
+        # Need an unregistered consumer object:
+        unregistered_consumer = Mock()
+        unregistered_consumer.uuid = None
+        unregistered_consumer.name = None
 
-        dialog = factsgui.SystemFactsDialog()
+        dialog = factsgui.SystemFactsDialog(unregistered_consumer)
         dialog.show()
 
         enabled = dialog.update_button.get_sensitive()
@@ -39,7 +51,7 @@ class FactDialogTests(unittest.TestCase):
         managergui.consumer = { 'uuid': 'Random UUID',
                                 'consumer_name': 'system' }
 
-        dialog = factsgui.SystemFactsDialog()
+        dialog = factsgui.SystemFactsDialog(self.consumer)
         dialog.show()
 
         enabled = dialog.update_button.get_sensitive()
