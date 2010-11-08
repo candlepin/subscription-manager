69533ec6cf7f5e34f7010ff941db80bf692df6ed
Devan Goodwin <dgoodwin@redhat.com>
2010-11-08 11:28:57 -0400

    Display dummy products in compliance assistant.

--- src/gui/compliance.py
+++ src/gui/compliance.py
@@ -23,14 +23,45 @@ _ = gettext.gettext
 from logutil import getLogger
 log = getLogger(__name__)
 
+from dateselect import DateSelector
+from widgets import SubDetailsWidget
+
 prefix = os.path.dirname(__file__)
 COMPLIANCE_GLADE = os.path.join(prefix, "data/compliance.glade")
+
+PRODUCT_NAME_INDEX = 0
+CONTRACT_INDEX = 1
+EXPIRATION_INDEX = 2
 
 class ComplianceAssistant(object):
     """ Compliance Assistant GUI window. """
     def __init__(self):
         self.compliance_xml = gtk.glade.XML(COMPLIANCE_GLADE)
         self.window = self.compliance_xml.get_widget('compliance_assistant_window')
+        self.uncompliant_store = gtk.ListStore(str, str, str)
+        self.uncompliant_treeview = self.compliance_xml.get_widget(
+                'uncompliant_products_treeview')
+        self.uncompliant_treeview.set_model(self.uncompliant_store)
+        self._display_uncompliant()
+
+    def _display_uncompliant(self):
+        # These display the list of products uncompliant on the selected date:
+        self.uncompliant_store.clear()
+        self._add_column(_('Product'), PRODUCT_NAME_INDEX)
+        self._add_column(_('Contract'), CONTRACT_INDEX)
+        self._add_column(_('Expiration'), EXPIRATION_INDEX)
+
+        self.uncompliant_store.append(['Fake Product 1', 'FAKE01010010', '2010-01-01'])
+        self.uncompliant_store.append(['Fake Product 2', 'N/A', 'N/A'])
+
+        # Dummy data for now:
+
+    def _display_providing_subs(self):
+        pass
 
     def show(self):
         self.window.show()
+
+    def _add_column(self, name, order):
+        column = gtk.TreeViewColumn(name, gtk.CellRendererText(), text=order)
+        self.uncompliant_treeview.append_column(column)
