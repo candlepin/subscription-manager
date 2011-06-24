d9b69de2ca3d757eca50cea6c4662dd5a5670c21
Bryan Kearney <bkearney@redhat.com>
2011-06-24 16:33:17 -0400

    Improve the output from registering

--- src/subscription_manager/managercli.py
+++ src/subscription_manager/managercli.py
@@ -493,7 +493,9 @@ class RegisterCommand(UserPassCommand):
         except Exception, e:
             handle_exception(_("Error during registration: %s") % e, e)
 
-        managerlib.persist_consumer_cert(consumer)
+        consumer_info = managerlib.persist_consumer_cert(consumer)
+
+        print (_("The system has been registered with id: %s ")) % (consumer_info["uuid"])
 
         if self.options.autosubscribe:
             autosubscribe(admin_cp, consumer['uuid'], self.certlib)
