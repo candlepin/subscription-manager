e9f3abd1721f8bf91758d015acb4a549a93c2e3e
Devan Goodwin <dgoodwin@redhat.com>
2011-06-24 14:51:40 -0300

    Add a functional test suite.

---
+++ test/functional/connection-tests.py
@@ -0,0 +1,16 @@
+import unittest
+from rhsm.connection import *
+
+class ConnectionTests(unittest.TestCase):
+
+    def setUp(self):
+        self.cp = UEPConnection(username="admin", password="admin")
+
+    def test_supports_resource(self):
+        self.assertTrue(self.cp.supports_resource('consumers'))
+        self.assertTrue(self.cp.supports_resource('admin'))
+        self.assertFalse(self.cp.supports_resource('boogity'))
+
+    def test_resources_fetched(self):
+        self.assertTrue(len(self.cp.resources.keys()) > 0)
+
