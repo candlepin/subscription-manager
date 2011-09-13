e75f4f494e198a8b99fd0c19ada09a08fd3450e3
Chris Duryee (beav) <cduryee@redhat.com>
2011-09-13 13:45:33 -0400

    Applying "indent -linux -pcs -psl -ci8 -cs -cli8 -cp0" to codebase.

--- src/rhsmcertd.c
+++ src/rhsmcertd.c
@@ -27,182 +27,172 @@
 #define LOGFILE "/var/log/rhsm/rhsmcertd.log"
 #define LOCKFILE "/var/lock/subsys/rhsmcertd"
 #define UPDATEFILE "/var/run/rhsm/update"
-#define CERT_INTERVAL 240 /*4 hours*/
-#define HEAL_INTERVAL 1440 /*24 hours*/
-#define RETRY 10 /*10 min*/
+#define CERT_INTERVAL 240	/*4 hours */
+#define HEAL_INTERVAL 1440	/*24 hours */
+#define RETRY 10		/*10 min */
 #define BUF_MAX 256
 
 static FILE *log = 0;
 
-
-void printUsage()
+void
+printUsage ()
 {
-    printf("usage: rhsmcertd <interval>");
+	printf ("usage: rhsmcertd <interval>");
 }
 
-char *ts()
+char *
+ts ()
 {
-    time_t tm = time(0);
-    char *ts = asctime(localtime(&tm));
-    char *p = ts;
-    while(*p)
-    {
-        p++;
-        if(*p == '\n')
-        {
-           *p = 0;
-        }
-    }
-    return ts;
+	time_t tm = time (0);
+	char *ts = asctime (localtime (&tm));
+	char *p = ts;
+	while (*p) {
+		p++;
+		if (*p == '\n') {
+			*p = 0;
+		}
+	}
+	return ts;
 }
 
-void logUpdate(int delay)
+void
+logUpdate (int delay)
 {
-    time_t update = time(NULL);
-    struct tm update_tm = *localtime(&update);
-    char buf[BUF_MAX];
+	time_t update = time (NULL);
+	struct tm update_tm = *localtime (&update);
+	char buf[BUF_MAX];
 
-    update_tm.tm_min += delay;
-    strftime(buf, BUF_MAX, "%s", &update_tm);
+	update_tm.tm_min += delay;
+	strftime (buf, BUF_MAX, "%s", &update_tm);
 
-    FILE *updatefile = fopen(UPDATEFILE, "w");
-    fprintf(updatefile, "%s", buf);
-    fflush(updatefile);
-    fclose(updatefile);
+	FILE *updatefile = fopen (UPDATEFILE, "w");
+	fprintf (updatefile, "%s", buf);
+	fflush (updatefile);
+	fclose (updatefile);
 }
 
-int run(int interval, bool heal)
+int
+run (int interval, bool heal)
 {
-    int status = 0;
-    fprintf(log, "%s: started: interval = %d minutes\n", ts(), interval);
-    fflush(log);
+	int status = 0;
+	fprintf (log, "%s: started: interval = %d minutes\n", ts (), interval);
+	fflush (log);
 
-    while(1)
-    {
-        int pid = fork();
-        if(pid < 0)
-        {
-            fprintf(log, "%s: fork failed\n", ts());
-            fflush(log);
-            return EXIT_FAILURE;
-        }
-        if(pid == 0)
-        {
-          if(heal) {
-            execl("/usr/bin/python", "python", "/usr/share/rhsm/subscription_manager/certmgr.py", "--autoheal",
-              NULL);
-          }
-          else
-          {
-            execl("/usr/bin/python", "python", "/usr/share/rhsm/subscription_manager/certmgr.py",
-              NULL);
-          }
+	while (1) {
+		int pid = fork ();
+		if (pid < 0) {
+			fprintf (log, "%s: fork failed\n", ts ());
+			fflush (log);
+			return EXIT_FAILURE;
+		}
+		if (pid == 0) {
+			if (heal) {
+				execl ("/usr/bin/python", "python",
+				       "/usr/share/rhsm/subscription_manager/certmgr.py",
+				       "--autoheal", NULL);
+			} else {
+				execl ("/usr/bin/python", "python",
+				       "/usr/share/rhsm/subscription_manager/certmgr.py",
+				       NULL);
+			}
 
-        }
-        int delay = interval;
-        waitpid(pid, &status, 0);
-        status = WEXITSTATUS(status);
-        if(status == 0)
-        {
-            fprintf(log, "%s: certificates updated\n", ts());
-            fflush(log);
-        }
-        else
-        {
-            if(delay > RETRY) delay = RETRY;
-            fprintf(log, "%s: update failed (%d), retry in %d minutes\n", ts(), status, delay);
-            fflush(log);
-        }
+		}
+		int delay = interval;
+		waitpid (pid, &status, 0);
+		status = WEXITSTATUS (status);
+		if (status == 0) {
+			fprintf (log, "%s: certificates updated\n", ts ());
+			fflush (log);
+		} else {
+			if (delay > RETRY)
+				delay = RETRY;
+			fprintf (log,
+				 "%s: update failed (%d), retry in %d minutes\n",
+				 ts (), status, delay);
+			fflush (log);
+		}
 
-        logUpdate(delay);
-        sleep(delay*60);
-    }
+		logUpdate (delay);
+		sleep (delay * 60);
+	}
 
-    return status;
+	return status;
 }
 
-int get_lock()
+int
+get_lock ()
 {
-    int fdlock;
-    struct flock fl;
+	int fdlock;
+	struct flock fl;
 
-    fl.l_type = F_WRLCK;
-    fl.l_whence = SEEK_SET;
-    fl.l_start = 0;
-    fl.l_len = 1;
+	fl.l_type = F_WRLCK;
+	fl.l_whence = SEEK_SET;
+	fl.l_start = 0;
+	fl.l_len = 1;
 
-    if((fdlock = open(LOCKFILE, O_WRONLY|O_CREAT, 0640)) == -1)
-        return 1;
+	if ((fdlock = open (LOCKFILE, O_WRONLY | O_CREAT, 0640)) == -1)
+		return 1;
 
-    if(flock(fdlock, LOCK_EX|LOCK_NB) == -1)
-        return 1;
+	if (flock (fdlock, LOCK_EX | LOCK_NB) == -1)
+		return 1;
 
-    return 0;
+	return 0;
 }
 
-void run_parts(int cert_interval, int heal_interval)
+void
+run_parts (int cert_interval, int heal_interval)
 {
-    // TODO: This will become a more robust event loop. I need to verify that
-    // it's OK for sub-mgr to depend on glib
-    int pid = fork();
-    if(pid < 0)
-    {
-        fprintf(log, "%s: fork failed\n", ts());
-        fflush(log);
-        return EXIT_FAILURE;
-    }
-    if(pid == 0)
-    {
-       run(cert_interval, false); //cert
-    }
-    else
-    {
-       run(heal_interval, true);  //heal
-    }
+	// TODO: This will become a more robust event loop. I need to verify that
+	// it's OK for sub-mgr to depend on glib
+	int pid = fork ();
+	if (pid < 0) {
+		fprintf (log, "%s: fork failed\n", ts ());
+		fflush (log);
+		return EXIT_FAILURE;
+	}
+	if (pid == 0) {
+		run (cert_interval, false);	//cert
+	} else {
+		run (heal_interval, true);	//heal
+	}
 }
 
-
-int main(int argc, char *argv[])
+int
+main (int argc, char *argv[])
 {
-    log = fopen(LOGFILE, "a+");
-    if(log == 0)
-    {
-	    return EXIT_FAILURE;
-    }
-    if(argc < 3)
-    {
-        printUsage();
-        return EXIT_FAILURE;
-    }
+	log = fopen (LOGFILE, "a+");
+	if (log == 0) {
+		return EXIT_FAILURE;
+	}
+	if (argc < 3) {
+		printUsage ();
+		return EXIT_FAILURE;
+	}
 
-    int cert_interval = atoi(argv[1]);
-    int heal_interval = atoi(argv[2]);
+	int cert_interval = atoi (argv[1]);
+	int heal_interval = atoi (argv[2]);
 
-    if(cert_interval < 1)
-    {
-        cert_interval = CERT_INTERVAL;
-    }
-    if(heal_interval < 1)
-    {
-        heal_interval = HEAL_INTERVAL;
-    }
+	if (cert_interval < 1) {
+		cert_interval = CERT_INTERVAL;
+	}
+	if (heal_interval < 1) {
+		heal_interval = HEAL_INTERVAL;
+	}
 
-    int pid = fork();
-    if(pid == 0)
-    {
-        daemon(0, 0);
+	int pid = fork ();
+	if (pid == 0) {
+		daemon (0, 0);
 
-        if (get_lock() != 0)
-        {
-            fprintf(log, "%s: unable to get lock, exiting\n", ts());
-            fflush(log);
-            return EXIT_FAILURE;
-        }
+		if (get_lock () != 0) {
+			fprintf (log, "%s: unable to get lock, exiting\n",
+				 ts ());
+			fflush (log);
+			return EXIT_FAILURE;
+		}
 
-        run_parts(cert_interval, heal_interval);
-    }
-    fclose(log);
+		run_parts (cert_interval, heal_interval);
+	}
+	fclose (log);
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
-
