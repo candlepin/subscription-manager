da0131f2b16f32b2fcd37fbf6a2c570c773add02
Chris Duryee (beav) <cduryee@redhat.com>
2011-09-13 13:45:33 -0400

    Applying "indent -linux -pcs -psl -ci8 -cs -cli8 -cp0" to codebase.

--- src/rhsm_icon.c
+++ src/rhsm_icon.c
@@ -40,17 +40,16 @@ static bool check_immediately = false;
 
 #define NAME_TO_CLAIM "com.redhat.subscription-manager.Icon"
 
-static GOptionEntry entries[] =
-{
+static GOptionEntry entries[] = {
 	{"check-period", 'c', 0, G_OPTION_ARG_INT, &check_period,
-		N_("How often to check for validity (in seconds)"), NULL},
+	 N_("How often to check for validity (in seconds)"), NULL},
 	{"debug", 'd', 0, G_OPTION_ARG_NONE, &debug,
-		N_("Show debug messages"), NULL},
+	 N_("Show debug messages"), NULL},
 	{"force-icon", 'f', 0, G_OPTION_ARG_STRING, &force_icon,
-		N_("Force display of the icon (expired, partial or warning)"),
-		"TYPE"},
+	 N_("Force display of the icon (expired, partial or warning)"),
+	 "TYPE"},
 	{"check-immediately", 'i', 0, G_OPTION_ARG_NONE, &check_immediately,
-		N_("Run the first status check right away"), NULL},
+	 N_("Run the first status check right away"), NULL},
 	{NULL}
 };
 
@@ -72,23 +71,23 @@ typedef enum _StatusType {
 
 /* prototypes */
 
-static void destroy_icon(Context*);
-static void display_icon(Context*, StatusType);
-static void alter_icon(Context*, StatusType);
-static void run_smg(Context*);
-static void icon_clicked(GtkStatusIcon*, Context*);
-static void icon_right_clicked(GtkStatusIcon*, guint, guint, Context*);
-static void remind_me_later_clicked(NotifyNotification*, gchar*, Context*);
-static void manage_subs_clicked(NotifyNotification*, gchar*, Context*);
-static void do_nothing_logger(const gchar*, GLogLevelFlags, const gchar*, gpointer);
-static void status_changed_cb(NotifyNotification*, gint, Context*);
-static bool check_status(Context*);
-static StatusType check_status_over_dbus();
-static StatusType create_status_type(int);
-
+static void destroy_icon (Context *);
+static void display_icon (Context *, StatusType);
+static void alter_icon (Context *, StatusType);
+static void run_smg (Context *);
+static void icon_clicked (GtkStatusIcon *, Context *);
+static void icon_right_clicked (GtkStatusIcon *, guint, guint, Context *);
+static void remind_me_later_clicked (NotifyNotification *, gchar *, Context *);
+static void manage_subs_clicked (NotifyNotification *, gchar *, Context *);
+static void do_nothing_logger (const gchar *, GLogLevelFlags, const gchar *,
+			       gpointer);
+static void status_changed_cb (NotifyNotification *, gint, Context *);
+static bool check_status (Context *);
+static StatusType check_status_over_dbus ();
+static StatusType create_status_type (int);
 
 static StatusType
-create_status_type(int status)
+create_status_type (int status)
 {
 	switch (status) {
 		case 0:
@@ -108,59 +107,56 @@ create_status_type(int status)
 }
 
 static void
-destroy_icon(Context *context)
+destroy_icon (Context * context)
 {
 	if (!context->is_visible) {
 		return;
 	}
-	gtk_status_icon_set_visible(context->icon, false);
-	g_object_unref(context->icon);
+	gtk_status_icon_set_visible (context->icon, false);
+	g_object_unref (context->icon);
 	context->is_visible = false;
 
-	notify_notification_close(context->notification, NULL);
-	g_object_unref(context->notification);
+	notify_notification_close (context->notification, NULL);
+	g_object_unref (context->notification);
 }
 
 static void
-run_smg(Context *context)
+run_smg (Context * context)
 {
-	g_spawn_command_line_async("subscription-manager-gui", NULL);
-	destroy_icon(context);
+	g_spawn_command_line_async ("subscription-manager-gui", NULL);
+	destroy_icon (context);
 }
 
 static void
-icon_clicked(GtkStatusIcon *icon G_GNUC_UNUSED, Context *context)
+icon_clicked (GtkStatusIcon * icon G_GNUC_UNUSED, Context * context)
 {
-	g_debug("icon click detected");
-	run_smg(context);
+	g_debug ("icon click detected");
+	run_smg (context);
 }
 
 static void
-icon_right_clicked(GtkStatusIcon *icon G_GNUC_UNUSED,
-		   guint button G_GNUC_UNUSED,
-		   guint activate_time G_GNUC_UNUSED,
-		   Context *context)
+icon_right_clicked (GtkStatusIcon * icon G_GNUC_UNUSED,
+		    guint button G_GNUC_UNUSED,
+		    guint activate_time G_GNUC_UNUSED, Context * context)
 {
-	g_debug("icon right click detected");
-	run_smg(context);
+	g_debug ("icon right click detected");
+	run_smg (context);
 }
 
 static void
-remind_me_later_clicked(NotifyNotification *notification G_GNUC_UNUSED,
-			gchar *action G_GNUC_UNUSED,
-			Context *context)
+remind_me_later_clicked (NotifyNotification * notification G_GNUC_UNUSED,
+			 gchar * action G_GNUC_UNUSED, Context * context)
 {
-	g_debug("Remind me later clicked");
-	destroy_icon(context);
+	g_debug ("Remind me later clicked");
+	destroy_icon (context);
 }
 
 static void
-manage_subs_clicked(NotifyNotification *notification G_GNUC_UNUSED,
-		    gchar *action G_GNUC_UNUSED,
-		    Context *context)
+manage_subs_clicked (NotifyNotification * notification G_GNUC_UNUSED,
+		     gchar * action G_GNUC_UNUSED, Context * context)
 {
-	g_debug("Manage my subscriptions clicked");
-	run_smg(context);
+	g_debug ("Manage my subscriptions clicked");
+	run_smg (context);
 }
 
 /*
@@ -172,136 +168,140 @@ manage_subs_clicked(NotifyNotification *notification G_GNUC_UNUSED,
  * the icon (and trying to reshow the notification could mess things up).
  */
 static void
-on_icon_size_changed(GtkStatusIcon *icon,
-		     gint size G_GNUC_UNUSED,
-		     Context *context)
+on_icon_size_changed (GtkStatusIcon * icon,
+		      gint size G_GNUC_UNUSED, Context * context)
 {
-	notify_notification_show(context->notification, NULL);
-	g_signal_handler_disconnect(icon, context->handler_id);
+	notify_notification_show (context->notification, NULL);
+	g_signal_handler_disconnect (icon, context->handler_id);
 }
 
 static void
-display_icon(Context *context, StatusType status_type)
+display_icon (Context * context, StatusType status_type)
 {
 	static char *tooltip;
 	static char *notification_title;
 	static char *notification_body;
 
 	if (context->is_visible) {
-		g_debug("Icon already visible");
+		g_debug ("Icon already visible");
 		return;
 	}
 
 	if (status_type == RHSM_EXPIRED) {
 		tooltip = _("Invalid or Missing Entitlement Certificates");
-		notification_title = _("Invalid or Missing Entitlement Certificates");
-		notification_body = _("This system is missing one or more "
-			"valid entitlement certificates.");
+		notification_title =
+			_("Invalid or Missing Entitlement Certificates");
+		notification_body =
+			_("This system is missing one or more "
+			  "valid entitlement certificates.");
 	} else if (status_type == RHSM_PARTIALLY_VALID) {
 		tooltip = _("Partially Entitled Products");
 		notification_title = _("Partially Entitled Products");
 		notification_body = _("This system is missing one or more "
-			"valid entitlement certificates to fully cover its "
-			"products");
+				      "valid entitlement certificates to fully cover its "
+				      "products");
 	} else {
-		tooltip = _("Some of the system's subscriptions are about to expire");
+		tooltip =
+			_
+			("Some of the system's subscriptions are about to expire");
 		notification_title =
 			_("This System's Subscriptions Are About to Expire");
-		notification_body = _("One or more of this system's "
-			"subscriptions are about to expire.");
+		notification_body =
+			_("One or more of this system's "
+			  "subscriptions are about to expire.");
 	}
 
-	gtk_status_icon_set_tooltip(context->icon, tooltip);
-	g_signal_connect(context->icon, "activate",
-			 G_CALLBACK(icon_clicked), context);
-	g_signal_connect(context->icon, "popup-menu",
-			 G_CALLBACK(icon_right_clicked), context);
+	gtk_status_icon_set_tooltip (context->icon, tooltip);
+	g_signal_connect (context->icon, "activate",
+			  G_CALLBACK (icon_clicked), context);
+	g_signal_connect (context->icon, "popup-menu",
+			  G_CALLBACK (icon_right_clicked), context);
 	context->is_visible = true;
 
-	context->notification = notify_notification_new_with_status_icon(
-		notification_title, notification_body, "subscription-manager",
-		context->icon);
+	context->notification =
+		notify_notification_new_with_status_icon (notification_title,
+							  notification_body,
+							  "subscription-manager",
+							  context->icon);
 
-	notify_notification_add_action(context->notification,
-				       "remind-me-later",
-				       _("Remind Me Later"),
-				       (NotifyActionCallback)
-				       remind_me_later_clicked,
-				       context, NULL);
-	notify_notification_add_action(context->notification,
-				       "manage-subscriptions",
-				       _("Manage My Subscriptions..."),
-				       (NotifyActionCallback)
-				       manage_subs_clicked,
-				       context, NULL);
+	notify_notification_add_action (context->notification,
+					"remind-me-later",
+					_("Remind Me Later"),
+					(NotifyActionCallback)
+					remind_me_later_clicked, context, NULL);
+	notify_notification_add_action (context->notification,
+					"manage-subscriptions",
+					_("Manage My Subscriptions..."),
+					(NotifyActionCallback)
+					manage_subs_clicked, context, NULL);
 
-	gtk_status_icon_set_visible(context->icon, true);
-	context->handler_id = g_signal_connect(context->icon, "size-changed",
-					       (GCallback) on_icon_size_changed,
-					       context);
+	gtk_status_icon_set_visible (context->icon, true);
+	context->handler_id = g_signal_connect (context->icon, "size-changed",
+						(GCallback)
+						on_icon_size_changed, context);
 }
 
 static void
-alter_icon(Context *context, StatusType status_type) {
+alter_icon (Context * context, StatusType status_type)
+{
 	if ((status_type != RHN_CLASSIC) && (status_type != RHSM_VALID)) {
-		display_icon(context, status_type);
+		display_icon (context, status_type);
 	} else {
-		destroy_icon(context);
+		destroy_icon (context);
 	}
 }
 
-
 static void
-do_nothing_logger(const gchar *log_domain G_GNUC_UNUSED,
-		  GLogLevelFlags log_level G_GNUC_UNUSED,
-		  const gchar *message G_GNUC_UNUSED,
-		  gpointer data G_GNUC_UNUSED)
+do_nothing_logger (const gchar * log_domain G_GNUC_UNUSED,
+		   GLogLevelFlags log_level G_GNUC_UNUSED,
+		   const gchar * message G_GNUC_UNUSED,
+		   gpointer data G_GNUC_UNUSED)
 {
 	/* really, do nothing */
 }
 
 static StatusType
-check_status_over_dbus(Context *context)
+check_status_over_dbus (Context * context)
 {
 	GError *error;
 	int status;
 
 	error = NULL;
-	if (!dbus_g_proxy_call(context->entitlement_status_proxy,
-			       "check_status", &error, G_TYPE_INVALID,
-			       G_TYPE_INT, &status, G_TYPE_INVALID)) {
-		g_printerr("Error: %s\n", error->message);
-		g_error_free(error);
-		exit(1);
+	if (!dbus_g_proxy_call (context->entitlement_status_proxy,
+				"check_status", &error, G_TYPE_INVALID,
+				G_TYPE_INT, &status, G_TYPE_INVALID)) {
+		g_printerr ("Error: %s\n", error->message);
+		g_error_free (error);
+		exit (1);
 	}
 
-	return create_status_type(status);
+	return create_status_type (status);
 }
 
 static bool
-check_status(Context *context)
+check_status (Context * context)
 {
 	StatusType status_type;
-	g_debug("Running entitlement status check");
+	g_debug ("Running entitlement status check");
 
 	if (force_icon) {
-		g_debug("Forcing display of icon (simulated invalidity)");
-		if (g_str_equal(force_icon, "expired")) {
+		g_debug ("Forcing display of icon (simulated invalidity)");
+		if (g_str_equal (force_icon, "expired")) {
 			status_type = RHSM_EXPIRED;
-		} else if (g_str_equal(force_icon, "partial")) {
+		} else if (g_str_equal (force_icon, "partial")) {
 			status_type = RHSM_PARTIALLY_VALID;
-		} else if (g_str_equal(force_icon, "warning")) {
+		} else if (g_str_equal (force_icon, "warning")) {
 			status_type = RHSM_WARNING;
 		} else {
-			g_print(N_("Unknown argument to force-icon: %s\n"),
-				force_icon);
-			exit(1);
+			g_print (N_("Unknown argument to force-icon: %s\n"),
+				 force_icon);
+			exit (1);
 		}
 	} else {
-		status_type = check_status_over_dbus(context);
+		status_type = check_status_over_dbus (context);
 	}
 
-	alter_icon(context, status_type);
+	alter_icon (context, status_type);
 
 	return true;
 }
@@ -311,59 +311,59 @@ check_status(Context *context)
  * return false so it won't run again.
  */
 static bool
-initial_check_status(Context *context)
+initial_check_status (Context * context)
 {
-	check_status(context);
+	check_status (context);
 	return false;
 }
 
 static void
-status_changed_cb(NotifyNotification *notification G_GNUC_UNUSED,
-	   gint status, Context *context)
+status_changed_cb (NotifyNotification * notification G_GNUC_UNUSED,
+		   gint status, Context * context)
 {
-	g_debug("in callback, received value: %i", status);
-	alter_icon(context, create_status_type(status));
+	g_debug ("in callback, received value: %i", status);
+	alter_icon (context, create_status_type (status));
 }
 
-static DBusGProxy* 
-get_entitlement_status_proxy(void)
+static DBusGProxy *
+get_entitlement_status_proxy (void)
 {
 	DBusGConnection *connection;
 	GError *error;
 	DBusGProxy *proxy;
 
 	error = NULL;
-	connection = dbus_g_bus_get(DBUS_BUS_SYSTEM, &error);
+	connection = dbus_g_bus_get (DBUS_BUS_SYSTEM, &error);
 	if (connection == NULL) {
-		g_printerr(N_("Failed to open connection to bus: %s\n"),
-		      error->message);
-		g_error_free(error);
-		exit(1);
+		g_printerr (N_("Failed to open connection to bus: %s\n"),
+			    error->message);
+		g_error_free (error);
+		exit (1);
 	}
 
-	proxy = dbus_g_proxy_new_for_name(connection,
-		     "com.redhat.SubscriptionManager",
-		     "/EntitlementStatus",
-		     "com.redhat.SubscriptionManager.EntitlementStatus");
+	proxy = dbus_g_proxy_new_for_name (connection,
+					   "com.redhat.SubscriptionManager",
+					   "/EntitlementStatus",
+					   "com.redhat.SubscriptionManager.EntitlementStatus");
 
-	dbus_g_connection_unref(connection);
+	dbus_g_connection_unref (connection);
 	return proxy;
 }
 
 static void
-add_signal_listener(Context *context)
+add_signal_listener (Context * context)
 {
-	dbus_g_proxy_add_signal(context->entitlement_status_proxy,
-				"entitlement_status_changed", G_TYPE_INT,
-				G_TYPE_INVALID);
-	dbus_g_proxy_connect_signal(context->entitlement_status_proxy,
-				    "entitlement_status_changed",
-				    G_CALLBACK(status_changed_cb), context,
-				    NULL);
+	dbus_g_proxy_add_signal (context->entitlement_status_proxy,
+				 "entitlement_status_changed", G_TYPE_INT,
+				 G_TYPE_INVALID);
+	dbus_g_proxy_connect_signal (context->entitlement_status_proxy,
+				     "entitlement_status_changed",
+				     G_CALLBACK (status_changed_cb), context,
+				     NULL);
 }
 
 int
-main(int argc, char **argv)
+main (int argc, char **argv)
 {
 	GError *error = NULL;
 	GOptionContext *option_context;
@@ -373,15 +373,15 @@ main(int argc, char **argv)
 	Context context;
 	context.is_visible = false;
 
-	setlocale(LC_ALL, "");
-	bindtextdomain("rhsm", "/usr/share/locale");
-	textdomain("rhsm");
+	setlocale (LC_ALL, "");
+	bindtextdomain ("rhsm", "/usr/share/locale");
+	textdomain ("rhsm");
 
 	option_context = g_option_context_new ("rhsm icon");
 	g_option_context_add_main_entries (option_context, entries, NULL);
 	g_option_context_add_group (option_context,
 				    gtk_get_option_group (TRUE));
-	
+
 	if (!g_option_context_parse (option_context, &argc, &argv, &error)) {
 		g_print (N_("option parsing failed: %s\n"), error->message);
 		return 1;
@@ -390,76 +390,71 @@ main(int argc, char **argv)
 	g_option_context_free (option_context);
 
 	if (!debug) {
-		g_log_set_handler(NULL, G_LOG_LEVEL_DEBUG, do_nothing_logger,
-				  NULL);
+		g_log_set_handler (NULL, G_LOG_LEVEL_DEBUG, do_nothing_logger,
+				   NULL);
 	}
 
-	gtk_init(&argc, &argv);
+	gtk_init (&argc, &argv);
 
-
-	connection = dbus_g_bus_get(DBUS_BUS_SESSION, &error);
+	connection = dbus_g_bus_get (DBUS_BUS_SESSION, &error);
 	if (connection == NULL) {
-		g_printerr(N_("Failed to open connection to bus: %s\n"),
-			error->message);
-		g_error_free(error);
+		g_printerr (N_("Failed to open connection to bus: %s\n"),
+			    error->message);
+		g_error_free (error);
 		return 1;
 	}
 
-	proxy = dbus_g_proxy_new_for_name(connection,
-					  "org.freedesktop.DBus",
-					  "/org/freedesktop/DBus",
-					  "org.freedesktop.DBus");
+	proxy = dbus_g_proxy_new_for_name (connection,
+					   "org.freedesktop.DBus",
+					   "/org/freedesktop/DBus",
+					   "org.freedesktop.DBus");
 	error = NULL;
 	if (!dbus_g_proxy_call (proxy,
-		"RequestName",
-		&error,
-		G_TYPE_STRING, NAME_TO_CLAIM,
-		G_TYPE_UINT,  0,
-		G_TYPE_INVALID,
-		G_TYPE_UINT,   &result,
-		G_TYPE_INVALID)) {
-			g_printerr("Couldn't acquire name: %s\n",
-				   error->message);
-			return 1;
+				"RequestName",
+				&error,
+				G_TYPE_STRING, NAME_TO_CLAIM,
+				G_TYPE_UINT, 0,
+				G_TYPE_INVALID,
+				G_TYPE_UINT, &result, G_TYPE_INVALID)) {
+		g_printerr ("Couldn't acquire name: %s\n", error->message);
+		return 1;
 	}
 
 	if (result != DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER) {
 		if (error != NULL) {
 			g_warning ("Failed to acquire %s: %s",
-				NAME_TO_CLAIM, error->message);
-				g_error_free (error);
+				   NAME_TO_CLAIM, error->message);
+			g_error_free (error);
+		} else {
+			g_debug ("Failed to acquire %s", NAME_TO_CLAIM);
 		}
-		else {
-			g_debug("Failed to acquire %s", NAME_TO_CLAIM);
-		}
-		g_critical("rhsm-icon is already running. exiting.");
+		g_critical ("rhsm-icon is already running. exiting.");
 		return 0;
 	}
 
-	notify_init("rhsm-icon");
+	notify_init ("rhsm-icon");
 
 	context.icon =
-		gtk_status_icon_new_from_icon_name("subscription-manager");
-	gtk_status_icon_set_visible(context.icon, false);
+		gtk_status_icon_new_from_icon_name ("subscription-manager");
+	gtk_status_icon_set_visible (context.icon, false);
 
-	context.entitlement_status_proxy = get_entitlement_status_proxy();
+	context.entitlement_status_proxy = get_entitlement_status_proxy ();
 	if (check_immediately) {
-		check_status(&context);
+		check_status (&context);
 	} else {
-		g_timeout_add(INITIAL_DELAY * 1000,
-			      (GSourceFunc) initial_check_status, &context);
+		g_timeout_add (INITIAL_DELAY * 1000,
+			       (GSourceFunc) initial_check_status, &context);
 	}
 	//convert sec to msec before passing in
-	g_timeout_add(check_period * 1000, (GSourceFunc) check_status,
-			      &context);
+	g_timeout_add (check_period * 1000, (GSourceFunc) check_status,
+		       &context);
 
-	add_signal_listener(&context);
-	gtk_main();
+	add_signal_listener (&context);
+	gtk_main ();
 
-	g_debug("past main");
-	g_object_unref(context.entitlement_status_proxy);
-	g_object_unref(proxy);
-	dbus_g_connection_unref(connection);
+	g_debug ("past main");
+	g_object_unref (context.entitlement_status_proxy);
+	g_object_unref (proxy);
+	dbus_g_connection_unref (connection);
 	return 0;
 }
-
