cfaa9c8682807e05b56eccf618956fbc76770bdc
Chris Duryee (beav) <cduryee@redhat.com>
2011-12-15 13:59:28 -0500

    757697: report xen dom0 as host, not guest

--- src/subscription_manager/hwprobe.py
+++ src/subscription_manager/hwprobe.py
@@ -305,6 +305,11 @@ class Hardware:
             # Otherwise there was an error running virt-what - who knows
             virt_dict['virt.is_guest'] = 'Unknown'
 
+        # xen dom0 is a guest for virt-what's purposes, but is a host for
+        # our purposes. Adjust is_guest accordingly. (#757697)
+        if virt_dict['virt.host_type'].find('dom0') > -1:
+            virt_dict['virt.is_guest'] = False
+
         self.allhw.update(virt_dict)
         return virt_dict
 
