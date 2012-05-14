87f7ed9f2eb2812ab6815bb287ed9a7aa90b84f6
Adrian Likins <alikins@redhat.com>
2012-05-14 10:56:18 -0400

    820671: Fix issues finding translated strings from glade
    Use i18n.configure_i18n(with_glade=True) here so glade
    text domains get created correctly.
    
    Just do this in firstboot_base here since the other
    modules subclass it.

--- src/subscription_manager/gui/firstboot_base.py
+++ src/subscription_manager/gui/firstboot_base.py
@@ -17,6 +17,8 @@ import sys
 
 sys.path.append("/usr/share/rhsm")
 from subscription_manager.certlib import ConsumerIdentity
+from subscription_manager.i18n import configure_i18n
+configure_i18n(with_glade=True)
 
 try:
     _version = "el6"
