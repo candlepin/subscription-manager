dbb76f8f275964cfe2b8afaf7c9583c4ff6484f6
Bryan Kearney <bkearney@redhat.com>
2012-08-15 12:50:28 -0400

    847795: String and terminology clean up

--- src/subscription_manager/branding/redhat_branding.py
+++ src/subscription_manager/branding/redhat_branding.py
@@ -10,21 +10,21 @@ class Branding(object):
                 _("This system is registered to RHN Classic.")
         self.REGISTERED_TO_OTHER_WARNING = _("WARNING") + \
             "\n\n" + \
-            _("This system has already been registered with RHN using RHN Classic technology.") + \
+            _("This system has already been registered with Red Hat using RHN Classic technology.") + \
             "\n\n" + \
-            _("The tool you are using is attempting to re-register using Red Hat Subscription Management technology. Red Hat recommends (except in a few cases) that customers only register once.") + \
+            _("The tool you are using is attempting to re-register using Red Hat Subscription Management technology. Red Hat recommends that customers only register once.") + \
             "\n\n" + \
-            _("To learn more about RHN registration and technologies please consult this Knowledge Base Article: https://access.redhat.com/kb/docs/DOC-45563")
+            _("To learn how to unregister from either service please consult this Knowledge Base Article: https://access.redhat.com/kb/docs/DOC-45563")
         self.REGISTERED_TO_OTHER_SUMMARY = _("RHN Classic")
 
         self.GUI_REGISTRATION_HEADER = \
                 _("Please enter your Red Hat account information:")
         self.REGISTERED_TO_BOTH_WARNING = \
-                _("This system is registered using both RHN Classic technology and RHN Certificate-Based technology.") + \
+                _("This system is registered using both RHN Classic and Red Hat Subscription Management.") + \
                 "\n\n" + \
-                _("Red Hat recommends (except in a few cases) that customers only register with RHN via one method.") + \
+                _("Red Hat recommends that customers only register to one service.") + \
                 "\n\n" + \
                 _("To learn more about RHN registration and technologies please consult this Knowledge Base Article: https://access.redhat.com/kb/docs/DOC-45563")
-        self.REGISTERED_TO_BOTH_SUMMARY = _("RHN classic and subscription management service")
+        self.REGISTERED_TO_BOTH_SUMMARY = _("RHN classic and Red Hat Subscription Management")
         self.GUI_FORGOT_LOGIN_TIP = \
                 _("Tip: Forgot your login or password? Look it up at http://red.ht/lost_password")
