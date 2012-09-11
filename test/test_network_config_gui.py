09f66f726c635814fc205eb6dd3c4c66502262c1
Adrian Likins <alikins@redhat.com>
2012-09-11 14:20:59 -0400

    Remove stub madness from net config dialog tests
    We use a test suite wide TestConfig now, so StubConfig is
    unneeded.
    
    Mock out test IOError instead of making StubConfig do it.

--- test/test_network_config_gui.py
+++ test/test_network_config_gui.py
@@ -3,7 +3,8 @@ import unittest
 import rhsm_display
 rhsm_display.set_display()
 
-import stubs
+import mock
+
 from subscription_manager.gui import networkConfig
 
 
@@ -11,15 +12,14 @@ class NetworkConfigDialog(unittest.TestCase):
 
     def test_network_config_write_values(self):
         nc = networkConfig.NetworkConfigDialog()
-        stubConfig = stubs.StubConfig()
-        nc.cfg = stubConfig
         nc.xml.get_widget("enableProxyButton").set_active(True)
         nc.xml.get_widget("proxyEntry").set_text("example.com:10000")
+        # our test config has a disarmed .save, so thism is ok
         nc.writeValues()
 
     def test_network_config_write_fail(self):
         nc = networkConfig.NetworkConfigDialog()
-        stubConfig = stubs.StubConfig()
-        stubConfig.raise_io = True
-        nc.cfg = stubConfig
+        # mock the config so we can simulate a write failure
+        nc.cfg = mock.Mock()
+        nc.cfg.save.side_effect = IOError()
         nc.writeValues()
