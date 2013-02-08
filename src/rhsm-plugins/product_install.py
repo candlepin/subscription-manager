77b42b735edd3fc86acfaf7d20036e4cec45b45c
Adrian Likins <alikins@redhat.com>
2013-02-08 16:54:28 -0500

    Add plugin to handle post_product_id_install

---
+++ src/rhsm-plugins/product_install.py
@@ -0,0 +1,26 @@
+#
+# Copyright (c) 2013 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+#
+# Red Hat trademarks are not licensed under GPLv2. No permission is
+# granted to use or replicate Red Hat trademarks that are incorporated
+# in this software or its documentation.
+#
+
+from subscription_manager.base_plugin import SubManPlugin
+requires_api_version = "1.0"
+
+class ProductInstallPlugin(SubManPlugin):
+    def post_product_id_install_hook(self, conduit):
+        conduit.log.info("post product id called")
+
+        # we need to know what product/product cert
+        products = conduit.getProductList()
+        for product in products:
+            print "product ", product
