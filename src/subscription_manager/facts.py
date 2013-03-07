7bcf0d758d90ce8b5d264daae0224215ed623b01
Devan Goodwin <dgoodwin@redhat.com>
2013-03-07 12:14:12 -0400

    Remove unused uep in Facts class.

--- src/subscription_manager/facts.py
+++ src/subscription_manager/facts.py
@@ -41,12 +41,11 @@ class Facts(CacheManager):
     """
     CACHE_FILE = "/var/lib/rhsm/facts/facts.json"
 
-    def __init__(self, ent_dir=None, prod_dir=None, uep=None):
+    def __init__(self, ent_dir=None, prod_dir=None):
         self.facts = {}
 
         self.entitlement_dir = ent_dir or certdirectory.EntitlementDirectory()
         self.product_dir = prod_dir or certdirectory.ProductDirectory()
-        self.uep = uep
         # see bz #627962
         # we would like to have this info, but for now, since it
         # can change constantly on laptops, it makes for a lot of
