724b786fbb7db631b887b3b45b2bb110840cc3ee
Adrian Likins <alikins@redhat.com>
2013-04-15 11:54:27 -0400

    add "assert_string_equals" that diffs multiline strings
    Test cases that need to compare large multiline strings
    should probably use assert_string_equals, as it will
    show a unified diff between the two strings it is
    checking for equality.
    
    Note that python2.7+ does this automatically for
    assertEqual for strings.

--- test/fixture.py
+++ test/fixture.py
@@ -1,8 +1,10 @@
+import difflib
 import unittest
 
 from mock import Mock, NonCallableMock, patch
-import subscription_manager.injection as inj
+
 import stubs
+import subscription_manager.injection as inj
 
 
 class SubManFixture(unittest.TestCase):
@@ -37,3 +39,19 @@ class SubManFixture(unittest.TestCase):
 
     def tearDown(self):
         self.dbus_patcher.stop()
+
+    # use our naming convention here to make it clear
+    # this is our extension. Note that python 2.7 adds a
+    # assertMultilineEquals that assertEqual of strings does
+    # automatically
+    def assert_string_equals(self, first_str, second_str, msg=None):
+        if first_str != second_str:
+            first_lines = first_str.splitlines(True)
+            second_lines = second_str.splitlines(True)
+            delta = difflib.unified_diff(first_lines, second_lines)
+            message = ''.join(delta)
+
+            if msg:
+                message += " : " + msg
+
+            self.fail("Multi-line strings are unequal:\n" + message)
