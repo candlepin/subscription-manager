bf8c86e585b4fb70bbb773bd5c222049c8a392cd
Adrian Likins <alikins@redhat.com>
2013-05-22 11:06:38 -0400

    Add info comments for test_hwprobe.sh

--- scripts/test_hwprobe.sh
+++ scripts/test_hwprobe.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-SYS_DUMPS_PATH="$1"
+# Runs hwprobe.py against a set of collection hardware
+# inventories, and validates that the cpu.* facts
+# match those of lscpu.*.
+#
+# Aka, make sure the new socket counting code matches
+# the output of 'lscpu'
+#
+# see "socket-count-test-data" repo to get a copy of
+# the dumps.
+#
+# Run like:
+#
+#    ./test_hwprobe.sh /path/to/socket-count-test-data/dumps/
+#
 
+SYS_DUMPS_PATH="$1"
 SYS_DUMPS=$(find "${SYS_DUMPS_PATH}" -mindepth 1 -maxdepth 1 -type d)
 
 for sys_dump in ${SYS_DUMPS}
