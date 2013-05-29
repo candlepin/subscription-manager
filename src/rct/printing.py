ce467d9b8ec18deac106df14678afcfd94cd363c
Bryan Kearney <bkearney@redhat.com>
2013-05-29 10:34:41 -0400

    968364: show the issuer for certs in rct.
    The main driver for this is answering "where did this cert come from".
    The CA information is a good proxy for this question.

--- src/rct/printing.py
+++ src/rct/printing.py
@@ -107,6 +107,7 @@ class CertificatePrinter(object):
         s.append("\t%s: %s" % (_("End Date"), xstr(cert.end)))
         self._append_to_cert_section(cert, s)
         s.append("\n%s" % xstr(self._get_subject(cert)))
+        s.append("\n%s" % xstr(self._get_issuer(cert)))
         return "%s" % '\n'.join(s)
 
     def printc(self, cert):
@@ -117,6 +118,13 @@ class CertificatePrinter(object):
         s.append(_("Subject:"))
         for key in sorted(cert.subject):
             s.append("\t%s: %s" % (key, cert.subject[key]))
+        return "%s\n" % '\n'.join(s)
+
+    def _get_issuer(self, cert):
+        s = []
+        s.append(_("Issuer:"))
+        for key in sorted(cert.issuer):
+            s.append("\t%s: %s" % (key, cert.issuer[key]))
         return "%s\n" % '\n'.join(s)
 
     def _append_to_cert_section(self, cert, str_parts_list):
