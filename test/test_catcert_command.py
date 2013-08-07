d2a1c887f2791b0ed3ef179384947bddeff86342
Adrian Likins <alikins@redhat.com>
2013-08-07 16:12:08 -0400

    Add product certs with os_name in certdata

--- test/test_catcert_command.py
+++ test/test_catcert_command.py
@@ -98,6 +98,13 @@ class CatCertCommandTests(SubManFixture):
         cert_output = cap.out
         self.assert_string_equals(certdata.PRODUCT_CERT_V1_0_OUTPUT, cert_output)
 
+    def test_product_cert_with_os_name_output(self):
+        command = CatCertCommandStub(certdata.PRODUCT_CERT_WITH_OS_NAME_V1_0)
+        command.main(['will_use_stub'])
+
+        cert_output = self.mock_stdout.buffer
+        self.assert_string_equals(certdata.PRODUCT_CERT_WITH_OS_NAME_V1_0_OUTPUT, cert_output)
+
     def test_identity_cert_output(self):
         with Capture() as cap:
             command = CatCertCommandStub(certdata.IDENTITY_CERT)
