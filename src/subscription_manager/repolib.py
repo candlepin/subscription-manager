1ece40e2d19d0ef85cebdd86e1f6774713760a57
Adrian Likins <alikins@redhat.com>
2013-11-05 09:31:42 -0500

    Fix mismerge and merge cleanups
    certlib is now entcertlib, not both.

--- src/subscription_manager/repolib.py
+++ src/subscription_manager/repolib.py
@@ -29,9 +29,12 @@ from rhsm.connection import RemoteServerException, RestlibException
 from rhsm.utils import UnsupportedOperationException
 
 # FIXME: local imports
-from certlib import ActionLock, ActionReport, DataLib
 
-from certdirectory import Path, ProductDirectory, EntitlementDirectory
+from subscription_manager.certlib import ActionReport, DataLib
+from subscription_manager.lock import ActionLock
+from subscription_manager.certdirectory import Path, ProductDirectory, EntitlementDirectory
+from subscription_manager.utils import UnsupportedOperationException
+
 
 from subscription_manager import injection as inj
 
@@ -46,11 +49,10 @@ _ = gettext.gettext
 
 class RepoLib(DataLib):
 
-
     def __init__(self, lock=ActionLock(), uep=None, cache_only=False):
         self.cache_only = cache_only
         DataLib.__init__(self, lock, uep)
-        self.identity = identity or inj.require(inj.IDENTITY)
+        self.identity = inj.require(inj.IDENTITY)
 
     def _do_update(self):
         action = RepoUpdateAction(uep=self.uep, cache_only=self.cache_only)
@@ -62,8 +64,8 @@ class RepoLib(DataLib):
 
     def get_repos(self, apply_overrides=True):
         action = RepoUpdateAction(uep=self.uep,
-							      cache_only=self.cache_only,
-							      apply_overrides=apply_overrides)
+                                  cache_only=self.cache_only,
+                                  apply_overrides=apply_overrides)
         repos = action.get_unique_content()
         if self.identity.is_valid() and action.override_supported:
             return repos
@@ -183,8 +185,7 @@ class RepoUpdateAction:
             existing = repo_file.section(cont.id)
             if existing is None:
                 repo_file.add(cont)
-
-				self.report_add(cont)
+                self.report_add(cont)
             else:
                 # In the non-disconnected case, destroy the old repo and replace it with
                 # what's in the entitlement cert plus any overrides.
@@ -193,9 +194,8 @@ class RepoUpdateAction:
                 else:
                     self.update_repo(existing, cont)
                     repo_file.update(existing)
-		        # TODO: add repoting for overrides
-				self.report_update(cont)
-
+                # TODO: add repoting for overrides
+                self.report_update(cont)
 
         for section in repo_file.sections():
             if section not in valid:
@@ -387,6 +387,7 @@ class RepoUpdateAction:
                 changes_made += 1
 
         return changes_made
+
     def report_update(self, repo):
         self.report.repo_updates.append(repo)
 
@@ -444,7 +445,6 @@ class RepoActionReport(ActionReport):
         # deleted are former repo sections, but they are the same type
         s.append(self.format_sections(self.repo_deleted))
         return '\n'.join(s)
->>>>>>> Add a RepoActionReport formatter
 
 
 class Repo(dict):
