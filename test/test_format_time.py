77a069783bb6a0f83506ba766b4f7ed29f06d4b0
Alex Wood <awood@redhat.com>
2014-01-27 15:46:11 -0500

    Use dateutil.tz instead of pytz.

--- test/test_format_time.py
+++ test/test_format_time.py
@@ -1,15 +1,16 @@
 import unittest
-from datetime import datetime
-import pytz
 import locale
 import sys
+
+from datetime import datetime
+from dateutil.tz import tzutc
 
 try:
     from freezegun import freeze_time
 except ImportError:
     from nose.plugins.skip import SkipTest
     sys.stderr.write("TestFormatTime is skipped: A package unavailable. Please 'pip install freezegun'.")
-    raise SkipTest()
+    raise SkipTest("Missing freezegun module")
 
 from subscription_manager import managerlib
 
@@ -24,56 +25,56 @@ class TestFormatTime(unittest.TestCase):
 
     @freeze_time("2013-9-14")  # During DST
     def test_system_dst(self):
-        test = datetime(2014, 12, 21, 4, 59, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 12, 21, 4, 59, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '12/20/2014')
-        test = datetime(2014, 12, 21, 5, 0, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 12, 21, 5, 0, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '12/21/2014')
-        test = datetime(2014, 12, 21, 5, 1, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 12, 21, 5, 1, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '12/21/2014')
-        test = datetime(2014, 12, 21, 3, 49, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 12, 21, 3, 49, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '12/20/2014')
-        test = datetime(2014, 12, 21, 4, 0, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 12, 21, 4, 0, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '12/20/2014')
-        test = datetime(2014, 12, 21, 4, 1, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 12, 21, 4, 1, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '12/20/2014')
 
-        test = datetime(2014, 5, 21, 4, 59, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 5, 21, 4, 59, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '05/21/2014')
-        test = datetime(2014, 5, 21, 5, 0, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 5, 21, 5, 0, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '05/21/2014')
-        test = datetime(2014, 5, 21, 5, 1, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 5, 21, 5, 1, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '05/21/2014')
-        test = datetime(2014, 5, 21, 3, 59, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 5, 21, 3, 59, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '05/20/2014')
-        test = datetime(2014, 5, 21, 4, 0, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 5, 21, 4, 0, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '05/21/2014')
-        test = datetime(2014, 5, 21, 4, 1, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 5, 21, 4, 1, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '05/21/2014')
 
     @freeze_time("2013-12-14")  # During EST
     def test_system_est(self):
-        test = datetime(2014, 12, 21, 4, 59, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 12, 21, 4, 59, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '12/20/2014')
-        test = datetime(2014, 12, 21, 5, 0, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 12, 21, 5, 0, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '12/21/2014')
-        test = datetime(2014, 12, 21, 5, 1, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 12, 21, 5, 1, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '12/21/2014')
-        test = datetime(2014, 12, 21, 3, 49, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 12, 21, 3, 49, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '12/20/2014')
-        test = datetime(2014, 12, 21, 4, 0, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 12, 21, 4, 0, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '12/20/2014')
-        test = datetime(2014, 12, 21, 4, 1, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 12, 21, 4, 1, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '12/20/2014')
 
-        test = datetime(2014, 5, 21, 4, 59, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 5, 21, 4, 59, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '05/21/2014')
-        test = datetime(2014, 5, 21, 5, 0, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 5, 21, 5, 0, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '05/21/2014')
-        test = datetime(2014, 5, 21, 5, 1, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 5, 21, 5, 1, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '05/21/2014')
-        test = datetime(2014, 5, 21, 3, 59, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 5, 21, 3, 59, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '05/20/2014')
-        test = datetime(2014, 5, 21, 4, 0, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 5, 21, 4, 0, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '05/21/2014')
-        test = datetime(2014, 5, 21, 4, 1, 0, tzinfo=pytz.utc)
+        test = datetime(2014, 5, 21, 4, 1, 0, tzinfo=tzutc())
         self.assertEquals(managerlib.format_date(test), '05/21/2014')
