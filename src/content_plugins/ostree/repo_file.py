c06f4ef67c73fa81e1af2f4c99ae65035031326a
Adrian Likins <alikins@redhat.com>
2014-05-05 19:31:41 -0400

    Use regular config parser instead of pyxdg
    May revisit.

--- src/content_plugins/ostree/repo_file.py
+++ src/content_plugins/ostree/repo_file.py
@@ -1,8 +1,12 @@
 
 
-import xdg.DesktopEntry
+from iniparse import ConfigParser
+
+
+class RepoFileConfigParser(ConfigParser):
+    pass
 
 
 class RepoFile(object):
     def __init__(self):
-        self.xdg  = xdg.DesktopEntry.DesktopEntry()
+        self.config_parser = RepoFileConfigParser()
