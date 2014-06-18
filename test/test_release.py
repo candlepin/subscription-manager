499780935c612029cc6c6f0c4f5425ecc66012a0
Adrian Likins <alikins@redhat.com>
2014-06-18 13:49:33 -0400

    Add test cases for 'rhel-5-workstation' tags.

--- test/test_release.py
+++ test/test_release.py
@@ -178,9 +178,29 @@ class TestReleaseBackend(fixture.SubManFixture):
                                        ["rhel-5-workstation"])
         self.assertFalse(icr)
 
+    def test_is_correct_rhel_content_rhel_5_workstation_1108257(self):
+        icr = self.cdn_rv_provider._is_correct_rhel(["rhel-5-client-workstation", "rhel-5-workstation"],
+                                                    ["rhel-5-workstation"])
+        self.assertTrue(icr)
+
+    def test_is_correct_rhel_content_rhel_5_workstation_1108257_just_workstation(self):
+        icr = self.cdn_rv_provider._is_correct_rhel(["rhel-5-workstation"],
+                                                    ["rhel-5-workstation"])
+        self.assertTrue(icr)
+
+    def test_is_correct_rhel_content_rhel_5_workstation_1108257_just_client_workstation(self):
+        icr = self.cdn_rv_provider._is_correct_rhel(["rhel-5-client-workstation"],
+                                                    ["rhel-5-workstation"])
+        self.assertFalse(icr)
+
     def test_is_correct_rhel_content_variant_match(self):
         icr = self.rb._is_correct_rhel(["rhel-5"],
                                        ["rhel-5-workstation"])
+        self.assertFalse(icr)
+
+    def test_is_correct_rhel_content_variant_client_workstation(self):
+        icr = self.cdn_rv_provider._is_correct_rhel(["rhel-5"],
+                                                    ["rhel-5-client-workstation"])
         self.assertFalse(icr)
 
     def test_is_correct_rhel_content_variant_no_match(self):
