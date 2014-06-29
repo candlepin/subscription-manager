b2d580523c23c4e3027150eeafdefab983b76cd1
Adrian Likins <alikins@redhat.com>
2014-06-28 20:04:03 -0400

    Fix iniparse tidy import.

--- src/subscription_manager/plugin/ostree/config.py
+++ src/subscription_manager/plugin/ostree/config.py
@@ -20,7 +20,7 @@ import re
 # but it's always there if ostree is
 iniparse_tidy = None
 try:
-    import iniparse.utils.tidy as iniparse_tidy
+    from iniparse.utils import tidy as iniparse_tidy
 except ImportError:
     pass
 
