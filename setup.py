3dd59e032e7143dea762e8f6abf49605da655039
Adrian Likins <alikins@redhat.com>
2014-07-22 18:18:45 -0400

    Add tests to "packages" config in setup.py
    That is wrong, but we aren't using the install
    command at the moment. Workaround for flake8
    not looking for test code.

--- setup.py
+++ setup.py
@@ -27,5 +27,9 @@ setup(name="subscription-manager",
                 'src/subscription_manager/plugin',
                 'src/subscription_manager/plugin/ostree',
                 'src/rhsm_debug',
-                'src/rct'],
+                'src/rct',
+                # There isn't a good way to run flake8 command
+                # on tests as well. Note this is wrong for
+                # our unused "install" case
+                'test/'],
       test_suite='nose.collector')
