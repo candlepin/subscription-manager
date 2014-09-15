f3c85a8e682f5bbfe729c894841e9e216835f652
Adrian Likins <alikins@redhat.com>
2014-09-15 13:06:26 -0400

    1134963: Fix 'release --list' on some systems.
    Encode the url path we request the release 'listing'
    file from unicode. Depending if 'python-simplejson'
    is installed, the Content.url str could be a 'str' or
    a 'unicode'. The unicode string is not valid for
    inclusion in an URL. This was causing failed GET
    requests to the CDN, resulting in 'release --list'
    return no results after a 20s timeout.
    
    The exception from the timeout was causing a
    httplib.BadStatusLine exception to be logged.
    
    python-simplejson defaults to decoding any
    json strings that can be represented as 'str'
    to 'str' vs 'unicode'. The default 'json' decoder
    always returns unicode strings.
    
    For performance reasons, we attempt to use
    python-simplejson if installed, but it is
    not a package dep. If it's not installed, the
    'release --list' command would fail.

--- src/subscription_manager/release.py
+++ src/subscription_manager/release.py
@@ -148,9 +148,12 @@ class CdnReleaseVersionProvider(object):
     def _build_listing_path(self, content_url):
         listing_parts = content_url.split('$releasever', 1)
         listing_base = listing_parts[0]
-        listing_path = "%s/listing" % listing_base
+        listing_path = u"%s/listing" % listing_base
         # FIXME: cleanup paths ("//"'s, etc)
-        return listing_path
+
+        # Make sure content URLS are encoded to the default utf8
+        # as unicode strings aren't valid. See rhbz#1134963
+        return listing_path.encode()
 
     # require tags provided by installed products?
 
