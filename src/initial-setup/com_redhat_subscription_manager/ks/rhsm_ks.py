7be72ccb27ba8b4171cb3c018c5a5e5563598d20
Adrian Likins <alikins@redhat.com>
2015-08-27 14:22:15 -0400

    1242998, 1254550: Fix "already reg'ed" in initial-setup
    Handle initial-setup running on a system that is potentially
    already registered and/or attached to subscriptions better.
    
    The RegisterWidget as used in initial-setup now detects
    if the system is already registered. Current behavior is
    to automatically move ahead to 'attach' stage of registration
    if needed, or to the done screen if already registered and
    attached.
    
    RegisterWidget now runs choose_initial_screen as an idle_add
    callback, so initial setup idle_add's it, then the callback
    decides which screen to immedidetly move to from
    initial_screen, and emit's signals to do it.
    
    Refactor RegisterDialog so AutoBindWizard can subclass it easier.
    
    Give AutoAttachWidget it's own choose_initial_screen
    since it doesn't need to detect if a system is registered.
    
    Populate gui with info from kickstart (mostly).
    
    Add a Screen.populate() method in registergui for
    populating the screens widgets with defaults or
    the values from a kickstart.
    
    Change _on_refresh_cb to accept an unused msg arg
    for consistency. (1254550)
    
    Add 'register-message' signal for non error info (for
    example, displaying the info that a system is already
    registered and attached).
    
    Move to having RegisterWidget driven by RegInfo
    
    Add a pre() to ChooseServerPage() that it can use to
    decide if it should start there are go to attach, etc.
    
    Some screen bookeeping simplified.
    
    Add a RegisterWidget.current_screen python prop
    
    Rearrange the methods in this class to make some sense
    
    Cleanup, refactoring, docs, test fixes, etc.

--- src/initial-setup/com_redhat_subscription_manager/ks/rhsm_ks.py
+++ src/initial-setup/com_redhat_subscription_manager/ks/rhsm_ks.py
@@ -15,15 +15,21 @@
 # License and may only be used or replicated with the express permission of
 # Red Hat, Inc.
 #
-# Red Hat Author(s): Vratislav Podzimek <vpodzime@redhat.com>
-#
-
 """Module with the RHSM initial-setup class."""
 
 import logging
+import sys
 
 from pyanaconda.addons import AddonData
+from pyanaconda.iutil import getSysroot
 
+log = logging.getLogger(__name__)
+
+RHSM_PATH = "/usr/share/rhsm"
+sys.path.append(RHSM_PATH)
+
+# For now, until we move all the logic out of managercli
+from subscription_manager import managercli
 
 # export RHSMAddonData class to prevent Anaconda's collect method from taking
 # AddonData class instead of the RHSMAddonData class
@@ -31,14 +37,217 @@ from pyanaconda.addons import AddonData
 __all__ = ["RHSMAddonData"]
 
 
+"""
+# Sample ks file ideas
+
+# all on the header
+%addon com_redhat_subscription_manager --serverUrl=https://grimlock.usersys.redhat.com/candlepin --activationkey=SOMEKEY
+
+%end
+
+%addon com_redhat_subscription_manager
+    # only one
+    server-url = https://grimlock.usersys.redhat.com:8443/candlepin
+
+    # can have many act keys. Does order matter?
+    activation-key = SOMEKEY-1
+    activation-key = SOMEKEY-2
+
+    # If we should attempt to auto-attach
+%end
+
+NOTES:
+# Add a version 'cli' so I can change the format later
+%addon com_redhat_subscription_manager --ksversion=1.0
+
+"""
+
+# To test
+# utf8 streams
+# inlined base64 blobs (ie, cert pems)
+# inline yum.repo
+# if anything needs to match a value from a cert,
+#  need to verify the encodings all work
+
+
+class KickstartRhsmData(object):
+    # A holder of rhsm info, that is not a gobject...
+    def __init__(self):
+        # TODO: make this a data class
+
+        # Server config related
+        self.serverurl = None
+        # self.ca_certs = []
+        # self.baseurl = []
+        # self.proxy
+        #    - proxy url
+        #    - proxy auth
+        #       - username
+        #       - password, etc
+
+        # identifying info
+        self.username = None
+        # Unsure if we should support this at all
+        self.password = None
+
+        self.org = None
+
+        # Register behaviour
+        self.auto_attach = True
+        # self.force = False
+        # self.type   # consumer type
+
+        # Register options
+        self.servicelevel = None
+        self.activationkeys = []
+        # self.environment
+        # self.consumerid
+        # self.name  (the consumer name, ie hostname)
+
+
 class RHSMAddonData(AddonData):
-    log = logging.getLogger(__name__)
+    """This is a common parent class for loading and storing
+       3rd party data to kickstart. It is instantiated by
+       kickstart parser and stored as ksdata.addons.<name>
+       to be used in the user interfaces.
 
-    def setup(self, storage, ksdata, instClass):
-        super(RHSMAddonData, self).setup(storage, ksdata, instClass)
+       The mandatory method handle_line receives all lines
+       from the corresponding addon section in kickstart and
+       the mandatory __str__ implementation is responsible for
+       returning the proper kickstart text (to be placed into
+       the %addon section) back.
 
-        self.log.debug("storage %s", storage)
-        self.log.debug("ksdata %s", ksdata)
-        self.log.debug("instClass %s", instClass)
+       There is also a mandatory method execute, which should
+       make all the described changes to the installed system.
+    """
 
-    # NOTE: no execute() or handle_line() yet
+    def __init__(self, name):
+        AddonData.__init__(self, name)
+
+        self.name = name
+        self.content = ""
+        self.header_args = ""
+
+        self.rhsm_data = KickstartRhsmData()
+
+        # TODO: make this a data class
+        self.serverurl = None
+        self.activationkeys = []
+        self.auto_attach = True
+        self.username = None
+        # Unsure if we should support this at all
+        self.password = None
+        self.org = None
+        self.servicelevel = []
+        self.force = False
+
+        self.arg_names = {}
+        self.line_handlers = {'serverurl': self._parse_serverurl,
+                              'activationkey': self._parse_activationkey,
+                              'auto-attach': self._parse_auto_attach,
+                              'username': self._parse_username,
+                              'password': self._parse_password,
+                              'servicelevel': self._parse_servicelevel,
+                              'force': self._parse_force,
+                              'org': self._parse_org}
+
+    def __str__(self):
+        return "%%addon %s %s\n%s%%end\n" % (self.name, self.header_args, self.content)
+
+    def setup(self, storage, ksdata, instclass):
+        """Make the changes to the install system.
+
+           This method is called before the installation
+           is started and directly from spokes. It must be possible
+           to call it multiple times without breaking the environment."""
+        AddonData.setup(self, storage, ksdata, instclass)
+
+    def execute(self, storage, ksdata, instClass, users):
+
+        """Make the changes to the underlying system.
+
+           This method is called only once in the post-install
+           setup phase.
+        """
+        log.debug("Read RHSM ks info, but non gui ks is currently not implemented.")
+
+    def handle_header(self, lineno, args):
+        """Process additional arguments to the %addon line.
+
+           This function receives any arguments on the %addon line after the
+           addon ID. For example, for the line:
+
+               %addon com_example_foo --argument='example'
+
+           This function would be called with args=["--argument='example'"].
+
+           By default AddonData.handle_header just preserves the passed
+           arguments by storing them and adding them to the __str__ output.
+
+        """
+
+        if args:
+            self.header_args += " ".join(args)
+
+    # TODO: verify this is consistent with other kickstart bool option handling
+    def _bool(self, value):
+        if value == 'true':
+            return True
+        return False
+
+    # TODO
+    # FIXME: move these to KickstartRhsmData
+
+    def _parse_activationkey(self, value):
+        self.activationkeys.append(value)
+
+    # Allow multiple SLAs in order of preference
+    def _parse_servicelevel(self, value):
+        self.servicelevel.append(value)
+
+    def _parse_auto_attach(self, value):
+        self.auto_attach = self._bool(value)
+
+    def _parse_force(self, value):
+        self.force = self._bool(value)
+
+    # TODO: these could all use a generic setter
+    def _parse_org(self, value):
+        self.org = value
+
+    def _parse_username(self, value):
+        self.username = value
+
+    def _parse_password(self, value):
+        self.password = value
+
+    def _parse_serverurl(self, value):
+        self.serverurl = value
+
+    def handle_line(self, line):
+        """Process one kickstart line."""
+        self.content += line
+
+        line = line.strip()
+        (lhs, sep, rhs) = line.partition('=')
+        lhs = lhs.strip()
+        sep = sep.strip()
+        # could trailing space be valid for a value?
+        # raw_rhs = rhs[:]
+        rhs = rhs.strip()
+        rhs = rhs.strip('"')
+
+        if lhs[0] == '#':
+            return
+
+        try:
+            self.line_handlers[lhs](rhs)
+        except KeyError:
+            log.debug("Parse error, unknown RHSM addon ks cmd %s", lhs)
+
+    def finalize(self):
+        """No additional data will come.
+
+           Addon should check if all mandatory attributes were populated.
+        """
+        pass
