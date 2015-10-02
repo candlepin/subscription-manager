c4a118b11a2e832ff65c8c92df75bc5fffdfa367
Alex Wood <awood@redhat.com>
2015-10-02 15:02:05 -0400

    1268102: Stop main window from opening duplicate dialogs.

--- src/subscription_manager/gui/managergui.py
+++ src/subscription_manager/gui/managergui.py
@@ -359,6 +359,16 @@ class MainWindow(widgets.SubmanBaseWidget):
 
     def _register_item_clicked(self, widget):
         registration_dialog = registergui.RegisterDialog(self.backend, self.facts)
+        if widget:
+            def disable(x):
+                widget.set_sensitive(False)
+
+            def enable(x):
+                widget.set_sensitive(True)
+
+            registration_dialog.register_dialog.connect('show', disable)
+            registration_dialog.register_dialog.connect('destroy', enable)
+
         registration_dialog.initialize()
         registration_dialog.show()
 
@@ -427,6 +437,17 @@ class MainWindow(widgets.SubmanBaseWidget):
     def _update_certificates_button_clicked(self, widget):
         autobind_wizard = registergui.AutobindWizardDialog(self.backend,
                                                            self.facts)
+        if widget:
+            def disable(x):
+                widget.set_sensitive(False)
+
+            def enable(x):
+                widget.set_sensitive(True)
+
+            autobind_wizard.register_dialog.connect('show', disable)
+            autobind_wizard.register_dialog.connect('destroy', enable)
+            autobind_wizard.register_dialog.connect('hide', enable)
+
         autobind_wizard.initialize()
         autobind_wizard.show()
 
