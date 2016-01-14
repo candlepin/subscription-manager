e18d612a71e6082071b0aabf599b596ac7a2b094
Adrian Likins <alikins@redhat.com>
2016-01-14 12:14:35 -0500

    Replace watchdog code with a resetable timer
    Instead of idle_add'ing the check_if_ran_once call
    back at the end of each StatusChecker method call
    to exit the main loop and exit the service, add a
    default timeout, after which the loop/service quits.
    And reset the timeout after any activity.
    
    This avoids starting/stopping rhsm_d service as often,
    while also providing a way to automatically shut it down
    after a timeout. The timeout starts when the service done,
    and resets after any activity.
    
    Make watchdog into a context manager
    
    Something running in the watchdog() context will
    not cause a timeout, but an idle process will
    timeout in 240 seconds.
    
    Get rid of StatusChecker.has_run attribute, we don't use
    it anymore.
    
    Add DBus Introspectable and Properties to default
    dbus policy context for subscription-manager
    (though note our object has no properties at the momemnt).
    
    Add timeout_add_seconds to ga_gtk2/

--- src/subscription_manager/ga_impls/ga_gtk2/GLib.py
+++ src/subscription_manager/ga_impls/ga_gtk2/GLib.py
@@ -1,6 +1,7 @@
 import gobject
 
 timeout_add = gobject.timeout_add
+timeout_add_seconds = gobject.timeout_add_seconds
 idle_add = gobject.idle_add
 
-__all__ = [timeout_add, idle_add]
+__all__ = [timeout_add, timeout_add_seconds, idle_add]
