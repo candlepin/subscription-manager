f520762dc56f4daea03e94703b3146d49ca71fe8
Adrian Likins <alikins@redhat.com>
2016-02-01 10:53:29 -0500

    Rudimentary 'make install' for facts service
    Adds __init__.py to service package dirs.

---
+++ src/dbus/services/facts/decorators.py
@@ -0,0 +1,35 @@
+
+import decorator
+import logging
+
+import dbus
+import dbus.exceptions
+
+log = logging.getLogger("rhsm_dbus.facts_service." + __name__)
+
+# TODO: mv to shared config/constants module
+FACTS_DBUS_INTERFACE = "com.redhat.Subscriptions1.Facts"
+
+
+# From system-config-firewall
+class SubscriptionFactsDBusException(dbus.DBusException):
+    _dbus_error_name = "%s.Exception" % FACTS_DBUS_INTERFACE
+
+
+@decorator
+def dbus_handle_exceptions(func, *args, **kwargs):
+    """Decorator to handle exceptions, log and report them into D-Bus
+
+    :Raises DBusException: on a firewall error code problems.
+    """
+    try:
+        return func(*args, **kwargs)
+#    except FirewallError as error:
+#        log.error(str(error))
+#        raise FirewallDBusException(str(error))
+    except dbus.DBusException as e:
+        # only log DBusExceptions once
+        raise e
+    except Exception as e:
+        log.exception()
+        raise SubscriptionFactsDBusException(str(e))
