02f5018ce48b0d69cf21a90f2cfd69e70a3ac5da
Adrian Likins <alikins@redhat.com>
2016-02-04 20:33:19 -0500

    Start moving stuff to a base service

---
+++ src/dbus/services/decorators.py
@@ -0,0 +1,46 @@
+
+import decorator
+import logging
+
+import dbus
+import dbus.exceptions
+
+log = logging.getLogger("rhsm-app." + __name__)
+
+# TODO: mv to shared config/constants module
+DBUS_INTERFACE = "com.redhat.Subscriptions1"
+
+
+# From system-config-firewall
+class Subscriptions1DBusException(dbus.DBusException):
+    _dbus_error_name = "%s.Exception" % DBUS_INTERFACE
+
+
+@decorator.decorator
+def dbus_handle_exceptions(func, *args, **kwargs):
+    """Decorator to handle exceptions, log and report them into D-Bus
+
+    :Raises DBusException: on a firewall error code problems.
+    """
+    try:
+        log.debug("about to call %s %s", args, kwargs)
+        ret = func(*args, **kwargs)
+        log.debug("about to return ret=%s", ret)
+        return ret
+#    except FirewallError as error:
+#        log.error(str(error))
+#        raise FirewallDBusException(str(error))
+    except dbus.DBusException as e:
+        # only log DBusExceptions once
+        log.debug("DbusException caught")
+        log.exception(e)
+        raise e
+    except Exception as e:
+        log.debug("Exception caught")
+        log.exception(e)
+        raise Subscriptions1DBusException(str(e))
+
+
+def dbus_service_method(*args, **kwargs):
+    kwargs.setdefault("sender_keyword", "sender")
+    return dbus.service.method(*args, **kwargs)
