b9364c324054429a048eff9532186b720a7f1b0c
Adrian Likins <alikins@redhat.com>
2016-02-04 20:11:08 -0500

    More cleanup

--- src/dbus/services/facts/server.py
+++ src/dbus/services/facts/server.py
@@ -3,62 +3,33 @@
 import logging
 import os
 import sys
-#print sys.path
-# Entry point needs to setup sys.path so subscription_manager
-# is found. Ie, add /usr/share/rhsm/ to sys.path
 
 log = logging.getLogger('rhsm-app.' + __name__)
 
-
-#from subscription_manager.ga import GObject as ga_GObject
-#from subscription_manager.ga import GLib as ga_GLib
-
-#import pprint
-#pprint.pprint(sys.modules)
-
-#print "sys.modules['gobject']: %s" % sys.modules.get('gobject', 'No gobject found')
+# gobject and gi and python module loading tricks are fun.
 gmodules = [x for x in sys.modules.keys() if x.startswith('gobject')]
 for gmodule in gmodules:
     del sys.modules[gmodule]
 
-gmodules = [x for x in sys.modules.keys() if x.startswith('gobject')]
-#pprint.pprint(gmodules)
-
-#print "sys.modules['gobject']: %s" % sys.modules.get('gobject', 'No gobject found')
 
 import slip._wrappers
 slip._wrappers._gobject = None
 
 from gi.repository import GLib
-#sys.modules['gobject'] = GObject
-
-
-import datetime
 
 import dbus
 import dbus.service
 import dbus.mainloop.glib
-
-#import slip
-
-# FIXME: hack, monkey patch slip._wrappers._gobject so it doesn't try to outsmart gobject import
-#import slip._wrappers
-#slip._wrappers._gobject = ga_GObject
 
 import slip.dbus
 import slip.dbus.service
 
 from rhsm.dbus.services.facts import decorators
 
-
 # TODO: move these to a config/constants module
 FACTS_DBUS_INTERFACE = "com.redhat.Subscriptions1.Facts"
 FACTS_DBUS_PATH = "/com/redhat/Subscriptions1/Facts"
 PK_FACTS_COLLECT = "com.redhat.Subscriptions1.Facts.collect"
-
-
-class OrgFreedesktopDBusInterfaceMixin(object):
-    pass
 
 
 class Facts(slip.dbus.service.Object):
@@ -111,7 +82,6 @@ class Facts(slip.dbus.service.Object):
 
     # TODO: figure out why the few codebases that use slip/python-dbus and implement
     #       Dbus.Properties do it with a staticmethod like this.
-    #@staticmethod
     def get_dbus_property(self, prop):
         log.debug("get_dbus_property, self=%s, prop=%s", self, prop)
         if prop in self._props:
@@ -138,8 +108,8 @@ class Facts(slip.dbus.service.Object):
             raise dbus.exceptions.DBusException("Cant getAll properties for %s" % interface_name)
 
         log.debug("GetAll interface_name=%s, sender=%s", interface_name, sender)
+
         # TODO/FIXME: error handling, etc
-        log.debug("GetAll returning %s", self.props)
         return self.props
 
     @dbus.service.signal(dbus.PROPERTIES_IFACE, signature='sa{sv}as')
@@ -163,18 +133,14 @@ def run():
     dbus.mainloop.glib.threads_init()
 
     bus = dbus.SystemBus()
-    #bus = dbus.SessionBus()
 
     name = dbus.service.BusName(FACTS_DBUS_INTERFACE, bus)
     service = Facts(name, FACTS_DBUS_PATH)
 
     mainloop = GLib.MainLoop()
-    #mainloop = ga_GObject.MainLoop()
     slip.dbus.service.set_mainloop(mainloop)
 
     GLib.idle_add(start_signal, service)
-
-    GLib.timeout_add_seconds(7, start_signal_timer, service)
 
     try:
         mainloop.run()
