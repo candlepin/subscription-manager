a396270124d992ead7303c0d9bdd5edb482a273e
Adrian Likins <alikins@redhat.com>
2016-02-08 15:10:29 -0500

    Add a facts_root service
    Fix issues with org.freedesktop.DBus.Properties. It was
    lack of a default polkit action. That seems to manifest
    itself in the form of method call timeouts, with no
    method return or signal ever seen.
    
    Add a rudimentary base_properties.py with a BaseProperties
    class for DBus.Properties. This will almost surely get
    replaced by something more idiomatic (gobject_services?
    depends on what works with gtk2/3 and python 2/3).
    
    Makefile changes to install everything.

---
+++ src/dbus/services/base_properties.py
@@ -0,0 +1,75 @@
+import logging
+
+import dbus
+
+log = logging.getLogger(__name__)
+
+
+# TODO: Make properties class a gobject, so we can reused it's prop handling
+#       (And maybe python-dbus can do something useful with a Gobject?
+class BaseProperties(object):
+    def __init__(self, interface,
+                 data=None,
+                 prop_changed_callback=None):
+        self.data = data
+        self.interface = interface
+        self.prop_changed_callback = prop_changed_callback
+
+    def get(self, interface=None, prop=None):
+        self._check_interface(interface)
+        self._check_prop(prop)
+
+        try:
+            return self.data[prop]
+        except KeyError, e:
+            log.exception(e)
+            msg = "org.freedesktop.DBus.Error.AccessDenied: "
+            "Property '%s' isn't exported (or may not exist) on interface: %s" % (prop, self.interface)
+
+            raise dbus.exceptions.DBusException(msg)
+
+    def get_all(self, interface=None):
+        self._check_interface(interface)
+
+        # For now at least, likely need to filter
+        return self.data
+
+    def set(self, interface, prop, value):
+        self._check_interface(interface)
+        self._check_prop(prop)
+
+        # FIXME/TODO: Plug in access checks and data validation
+        try:
+            prop = str(prop)
+            self.data[prop] = value
+        except Exception, e:
+            self._set_error(e, prop, value)
+
+        self._prop_changed_callback(self, prop, value)
+
+    def _check_interface(self, interface):
+        if interface and interface != self.interface:
+            msg = "%s does not handle properties for %s" % (self.interface, interface)
+            raise dbus.exceptions.DBusException(msg)
+        # Unset None/'' interface is default
+
+    def _check_prop(self, prop):
+        if prop not in self.data:
+            msg = "org.freedesktop.DBus.Error.AccessDenied: "
+            "Property '%s' does not exist" % property
+            raise dbus.exceptions.DBusException(msg)
+
+    # FIXME: This would def be better with a gobject and it's properties, so
+    #        we can let it take care of coaalescing.
+    def _prop_changed_callback(self, *args, **kwargs):
+        log.debug("args=%s kwargs=%s", *args, **kwargs)
+        if self.prop_changed_callback:
+            self.prop_changed_callback(*args, **kwargs)
+
+    def _set_error(self, exception, prop, value):
+        log.exception(exception)
+        msg = "Error setting property %s=%s on interface=%s: %s" % (prop,
+                                                                    value,
+                                                                    self.interface,
+                                                                    exception)
+        raise dbus.exceptions.DBusException(msg)
