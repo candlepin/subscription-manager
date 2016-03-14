195ad9fc486e774bf9c9670fea0c614a5b86c12e
Christopher Snyder <csnyder@redhat.com>
2016-03-14 10:39:39 -0400

    1309553: Stylish fixes for consumer fixes

--- src/subscription_manager/managercli.py
+++ src/subscription_manager/managercli.py
@@ -1081,10 +1081,10 @@ class RegisterCommand(UserPassCommand):
                         self.options.consumerid)
                 consumer = admin_cp.getConsumer(self.options.consumerid,
                         self.username, self.password)
-                try:
-                    consumer_type = consumer['type']
-                except KeyError:
+
+                if 'type' not in consumer:
                     log.warn('Unable to determine consumer type, proceeding with registration.')
+
                 if consumer.get('type', {}).get('manifest', {}):
                     log.error("registration attempted with consumerid = Subscription Management Application's uuid: %s" % self.options.consumerid)
                     system_exit(os.EX_USAGE, _("Error: Cannot register with an ID of a Subscription Management Application: %s" % self.options.consumerid))
