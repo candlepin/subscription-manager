1e892de052700b13ac25e8d2b05d147530e2ef83
Adrian Likins <alikins@redhat.com>
2016-03-22 16:09:14 -0400

    unit tests updates to handle facts changes.
    Some of previous test_facts.py needs to move to
    a rhsmlib.facts unit test.
    
    factlib tests can likely remain but will need updating
    to mock the call/return of the dbus service.
    
    A few other cases test_facts_gui.py were making assumptions
    about gui stuff being sync which is no longer true.

--- src/subscription_manager/gui/registergui.py
+++ src/subscription_manager/gui/registergui.py
@@ -1098,8 +1098,8 @@ class PerformUnregisterScreen(NoGuiScreen):
 class PerformPackageProfileSyncScreen(NoGuiScreen):
     screen_enum = UPLOAD_PACKAGE_PROFILE_PAGE
 
-    def __init__(self, reg_info, async_backend, facts, parent_window):
-        super(PerformPackageProfileSyncScreen, self).__init__(reg_info, async_backend, facts, parent_window)
+    def __init__(self, reg_info, async_backend, parent_window):
+        super(PerformPackageProfileSyncScreen, self).__init__(reg_info, async_backend, parent_window)
         self.pre_message = _("Uploading package profile")
 
     def _on_update_package_profile_finished_cb(self, result, error=None):
