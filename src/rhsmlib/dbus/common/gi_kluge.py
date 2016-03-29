7ac805b176289d495f3a170c2282497be4ff9039
Adrian Likins <alikins@redhat.com>
2016-03-29 10:47:16 -0400

    Fix missed use of 'print' statement for py3 compat

--- src/rhsmlib/dbus/common/gi_kluge.py
+++ src/rhsmlib/dbus/common/gi_kluge.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 
 def kluge_it():
     import sys
@@ -11,4 +12,4 @@ def kluge_it():
         import slip._wrappers
         slip._wrappers._gobject = None
     except ImportError:
-	print 'fix me, workaround for rhel6'
+	print('fix me, workaround for rhel6')
