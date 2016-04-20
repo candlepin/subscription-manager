99c1af2939615a13b8650633e341026c18d64279
Alex Wood <awood@redhat.com>
2016-04-20 14:55:22 -0400

    Add example private DBus service.
    Invoke with dbus-daemon --config-file=private-bus.conf --nofork

---
+++ src/rhsmlib/dbus/private/client.py
@@ -0,0 +1,9 @@
+import dbus
+import rhsmlib.dbus.private.register_service as service
+
+
+if __name__ == '__main__':
+    client = dbus.bus.BusConnection("unix:path=/tmp/subman.sock")
+    proxy = client.get_object(service.DBUS_NAME, service.DBUS_PATH)
+    obj = dbus.Interface(proxy, service.DBUS_NAME)
+    print(obj.reverse("hello"))
