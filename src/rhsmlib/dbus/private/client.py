50263e4f8202ff17527fff81e850274909868157
Alex Wood <awood@redhat.com>
2016-04-26 10:27:06 -0400

    Clean up proof-of-concept code.  Provide naive server.

--- src/rhsmlib/dbus/private/client.py
+++ src/rhsmlib/dbus/private/client.py
@@ -3,7 +3,7 @@ import rhsmlib.dbus.private.register_service as service
 
 
 if __name__ == '__main__':
-    client = dbus.bus.BusConnection("unix:path=/tmp/subman.sock")
+    client = dbus.connection.Connection("unix:path=/tmp/subman.sock")
     proxy = client.get_object(service.DBUS_NAME, service.DBUS_PATH)
     obj = dbus.Interface(proxy, service.DBUS_NAME)
     print(obj.reverse("hello"))
