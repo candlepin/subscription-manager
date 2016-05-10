3ca0b1e10704ebde7f480b57e4b418947716c647
Alex Wood <awood@redhat.com>
2016-05-10 13:54:46 -0400

    Don't use super() with ElementTree.XMLParser.
    super() fails in Python 2.6 with the familiar "super() argument 1 must
    be type, not classobj".  Perhaps XMLParser isn't a new-style class in
    2.6?

--- build_ext/utils.py
+++ build_ext/utils.py
@@ -45,14 +45,14 @@ class LineNumberingParser(ET.XMLParser):
     def _start_list(self, *args, **kwargs):
         # Here we assume the default XML parser which is expat
         # and copy its element position attributes into output Elements
-        element = super(self.__class__, self)._start_list(*args, **kwargs)
+        element = ET.XMLParser._start_list(self, *args, **kwargs)
         element._start_line_number = self.parser.CurrentLineNumber
         element._start_column_number = self.parser.CurrentColumnNumber
         element._start_byte_index = self.parser.CurrentByteIndex
         return element
 
     def _end(self, *args, **kwargs):
-        element = super(self.__class__, self)._end(*args, **kwargs)
+        element = ET.XMLParser._end(self, *args, **kwargs)
         element._end_line_number = self.parser.CurrentLineNumber
         element._end_column_number = self.parser.CurrentColumnNumber
         element._end_byte_index = self.parser.CurrentByteIndex
