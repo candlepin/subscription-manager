73dcb6e98ae08f2f25940ecbf79454a9b022db22
Chris Rog <crog@redhat.com>
2016-07-29 12:21:22 -0400

    1360909: The refresh command now requests entitlement cert regeneration
    - When the refresh command is issued on the CLI, subman will request
      entitlement certificate regeneration (lazily) for the active consumer

--- src/subscription_manager/ga_loader.py
+++ src/subscription_manager/ga_loader.py
@@ -237,8 +237,7 @@ def init_ga(gtk_version=None):
         else:
             gtk_version_from_build = "3"
         if gtk_version_from_environ is None:
-            warnings.warn("GTK_VERSION is unset in version.py.  Using GTK %s"
-                          % gtk_version_from_build)
+            warnings.warn("GTK_VERSION is unset in version.py.  Using GTK %s" % gtk_version_from_build)
 
     GTK_VERSION = gtk_version_from_environ or gtk_version or gtk_version_from_build \
         or DEFAULT_GTK_VERSION
