a46b05086c17aed9a40588d878d5c23c44fec4a7
Christopher Snyder <csnyder@redhat.com>
2017-04-06 14:12:41 -0400

    Add short option for splay setting in bash completion

--- etc-conf/rhsmcertd.completion.sh
+++ etc-conf/rhsmcertd.completion.sh
@@ -11,7 +11,7 @@ _rhsmcertd()
 	first="${COMP_WORDS[1]}"
 	cur="${COMP_WORDS[COMP_CWORD]}"
 	prev="${COMP_WORDS[COMP_CWORD-1]}"
-	opts="-h --help -c --cert-check-interval --cert-interval -d --debug --heal-interval -i --auto-attach-interval -n --now --max-splay-minutes"
+	opts="-h --help -c --cert-check-interval --cert-interval -d --debug --heal-interval -i --auto-attach-interval -n --now -m --max-splay-minutes"
 
 	case "${cur}" in
 		-*)
