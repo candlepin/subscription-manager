66d2961ecea4e9589169b8ff748a4de85a3f953b
Jiri Hnidek <jhnidek@redhat.com>
2018-01-15 17:19:43 +0100

    Python 3: allow to run rhsm_d using Python 3

--- src/subscription_manager/scripts/rhsm_d.py
+++ src/subscription_manager/scripts/rhsm_d.py
@@ -21,9 +21,11 @@ from __future__ import print_function, division, absolute_import
 # hack to allow bytes/strings to be interpolated w/ unicode values (gettext gives us bytes)
 # Without this, for example, "Формат: %s\n" % u"foobar" will fail with UnicodeDecodeError
 # See http://stackoverflow.com/a/29832646/6124862 for more details
+import six
 import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
+if six.PY2:
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
 
 enable_debug = False
 
