85262b8484e7722e60f6d2bc01117336feecc876
Jiri Hnidek <jhnidek@redhat.com>
2018-05-07 16:34:07 +0200

    Automatic rebuilding of updates.img on PXE Server
    * When PXE server is provisioned, then new service is started.
      This service starts shell script watching for changes in
      source code of subscription-manager using inotify. When
      source is changed, then new updates.img for Anaconda installer
      is build.
    * Source code of subscription-manager has to be synced using
      "vagrant rsync pxe-server" or "vagrant rsync-auto pxe-server"
    * Note: I added option: "-W" to vagrant rsync configuration,
      because command: "vagrant rsync pxe-server" was failing due to
      inotify running on pxe-server.

---
+++ scripts/autobuild_updates_img.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# Bash script used for automatic rebuilding of updates.img used by Anaconda installer.
+# This script is usualy started as service by systemd, but it can be also started manualy
+# On PXE server providing updates.img to PXE client.
+
+usage() {
+    cat << USAGE
+
+Usage: $0 [options]
+OPTIONS:
+  -p | --python <python_interpreter>  Python interpreter to use. Use to differentiate between Python 2
+                                        and Python 3.
+  -s | --src <src_dir>                Directory with source code used for creating updates.img
+  -d | --dest <dest_dir>              Destination directory, where updates.img will be created.
+  -h | --help                         Print this help text
+
+USAGE
+}
+
+SHORT_OPTIONS="hs:d:p:"
+LONG_OPTIONS="help,src:,dest:,python:"
+
+ARGS=$(getopt -s bash --options $SHORT_OPTIONS --longoptions $LONG_OPTIONS -- "$@")
+
+eval set -- $ARGS
+
+while [[ $# -gt 0 ]]
+do
+    case $1 in
+        -p|--python)
+            # Note to future self, shift moves all positional parameters to n-1
+            # (e.g. after shift the value of $1 becomes whatever $2 was prior)
+            PYTHON_BIN=$2
+            shift
+            ;;
+        -s|--src)
+            SRC_DIR=$2
+            shift
+            ;;
+        -d|--dest)
+            DEST_DIR=$2
+            shift
+            ;;
+        -h|--help)
+            usage
+            exit
+            ;;
+        *)
+            # Stop parsing on anything other than a long or short option we expected.
+            break
+            ;;
+    esac
+    shift
+done
+
+
+# Setting default values, when no arguments were provided
+if [ "${PYTHON_BIN}" = "" ]; then
+    PYTHON_BIN="python"
+    echo "Python interpreter not specified, using ${PYTHON_BIN}"
+fi
+if [ "${SRC_DIR}" = "" ]; then
+    SRC_DIR=$PWD
+    echo "Source directory not specified, using ${PWD}"
+fi
+if [ "${DEST_DIR}" = "" ]; then
+    DEST_DIR=$PWD
+    echo "Destination directory not specified, using ${PWD}"
+fi
+
+
+# Never ending loop
+while inotifywait -r "${SRC_DIR}"; do
+	pushd "${SRC_DIR}"
+	echo "Building new updates.img ..."
+	${0%/*}/build_updates_img.sh --python ${PYTHON_BIN}
+	echo "Done"
+	if [ $? -eq 0 ]; then
+		mv updates.img "${DEST_DIR}"
+	fi
+	popd
+	# Wait 10 seconds.
+	sleep 10
+done
