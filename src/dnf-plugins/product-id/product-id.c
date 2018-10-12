6859e94a7f64017cfb0077a21c17d0b8d12a35ee
Jiri Hnidek <jhnidek@redhat.com>
2018-10-12 16:33:37 +0200

    Faster method of fetching active repos.

--- src/dnf-plugins/product-id/product-id.c
+++ src/dnf-plugins/product-id/product-id.c
@@ -18,8 +18,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-void fetchProductId(DnfRepo *repo);
+int fetchProductId(DnfRepo *repo);
 
 // This stuff could go in a header file, I guess
 static const PluginInfo pinfo = {
@@ -100,6 +101,9 @@ void getEnabled(const GPtrArray *repos, GPtrArray *enabledRepos) {
 void getActive(DnfContext *context, const GPtrArray *repos, GPtrArray *activeRepos) {
     DnfSack *dnfSack = dnf_context_get_sack(context);
 
+    // TODO: this query does not provide fresh list of installed packages
+    // Currently installed/removed package is not listed in the query.
+    // The problem is with sack. We have to get it in different way.
     HyQuery query = hy_query_create_flags(dnfSack, 0);
     hy_query_filter(query, HY_PKG_NAME, HY_GLOB, "*");
 
@@ -113,32 +117,78 @@ void getActive(DnfContext *context, const GPtrArray *repos, GPtrArray *activeRep
 
         if (id) {
             g_ptr_array_add(installedPackages, pkg);
+            printf("Package: %s from: %s\n",
+                    dnf_package_get_nevra(pkg),
+                    dnf_package_get_reponame(pkg));
         }
     }
 
     for (int i = 0; i < repos->len; i++) {
         DnfRepo* repo = g_ptr_array_index(repos, i);
-        printf("Looking in %s against %i packages\n", dnf_repo_get_id(repo), installedPackages->len);
+        printf("Repository: %s\n", dnf_repo_get_id(repo));
 
-        for (int j = 0; j < installedPackages->len; j++) {
-            DnfPackage *instPkg = g_ptr_array_index(installedPackages, j);
-            HyQuery repoQuery = hy_query_create_flags(dnfSack, 0);
-            hy_query_filter(repoQuery, HY_PKG_NEVRA, HY_EQ, dnf_package_get_nevra(instPkg));
-            hy_query_filter(repoQuery, HY_PKG_REPONAME, HY_EQ, dnf_repo_get_id(repo));
+        HyQuery avail_query = hy_query_create_flags(dnfSack, 0);
+        // TODO: this filter is not correct
+        hy_query_filter(avail_query, HY_PKG_REPONAME, HY_NEQ, dnf_repo_get_id(repo));
+        GPtrArray *availPackageList = hy_query_run(avail_query);
+        hy_query_free(avail_query);
 
-            GPtrArray *repoPackageList = hy_query_run(repoQuery);
-            hy_query_free(repoQuery);
+        printf("Number of available packages: %d\n", availPackageList->len);
 
-            if (repoPackageList->len) {
-                const char *nvra = dnf_package_get_nevra(instPkg);
-                const char *repoName = dnf_package_get_reponame(instPkg);
-                printf("%s is from %s\n", nvra, repoName);
-                g_ptr_array_add(activeRepos, repo);
+        // Go through all available packages from repository
+        for (int i = 0; i < availPackageList->len; i++) {
+            int package_found = 0;
+            DnfPackage *pkg = g_ptr_array_index(availPackageList, i);
+            guint64 id = dnf_package_get_rpmdbid(pkg);
+
+            if (id) {
+                printf("Available Package: %s from: %s\n",
+                       dnf_package_get_nevra(pkg),
+                       dnf_package_get_reponame(pkg));
+                // Try to find if this available package is in the list of installed packages
+                for(int j=0; j < installedPackages->len; j++) {
+                    DnfPackage *instPkg = g_ptr_array_index(installedPackages, j);
+                    if(strcmp(dnf_package_get_nevra(pkg), dnf_package_get_nevra(instPkg)) == 0) {
+                        printf("Installed package %s is from repository: %s\n",
+                               dnf_package_get_nevra(pkg),
+                               dnf_repo_get_id(repo));
+                        g_ptr_array_add(activeRepos, repo);
+                        package_found = 1;
+                        break;
+                    }
+                }
+            }
+
+            if(package_found == 1) {
                 break;
             }
-            g_ptr_array_unref(repoPackageList);
         }
+        g_ptr_array_unref(availPackageList);
     }
+
+//    for (int i = 0; i < repos->len; i++) {
+//        DnfRepo* repo = g_ptr_array_index(repos, i);
+//        printf("Looking in %s against %i packages\n", dnf_repo_get_id(repo), installedPackages->len);
+//
+//        for (int j = 0; j < installedPackages->len; j++) {
+//            DnfPackage *instPkg = g_ptr_array_index(installedPackages, j);
+//            HyQuery repoQuery = hy_query_create_flags(dnfSack, 0);
+//            hy_query_filter(repoQuery, HY_PKG_NEVRA, HY_EQ, dnf_package_get_nevra(instPkg));
+//            hy_query_filter(repoQuery, HY_PKG_REPONAME, HY_EQ, dnf_repo_get_id(repo));
+//
+//            GPtrArray *repoPackageList = hy_query_run(repoQuery);
+//            hy_query_free(repoQuery);
+//
+//            if (repoPackageList->len) {
+//                const char *nvra = dnf_package_get_nevra(instPkg);
+//                const char *repoName = dnf_package_get_reponame(instPkg);
+//                printf("%s is from %s\n", nvra, repoName);
+//                g_ptr_array_add(activeRepos, repo);
+//                break;
+//            }
+//            g_ptr_array_unref(repoPackageList);
+//        }
+//    }
     g_ptr_array_unref(installedPackages);
     g_ptr_array_unref(packageList);
 }
@@ -159,12 +209,16 @@ int pluginHook(PluginHandle *handle, PluginHookId id, void *hookData, PluginHook
 
     if (id == PLUGIN_HOOK_ID_CONTEXT_PRE_TRANSACTION) {
         DnfContext *dnfContext = handle->initData;
+        // List of all repositories
         GPtrArray *repos = dnf_context_get_repos(dnfContext);
+        // List of enabled repositories
         GPtrArray *enabledRepos = g_ptr_array_sized_new(repos->len);
+        // Enabled repositories with product id certificate
+        GPtrArray *enabledProdIDRepos = g_ptr_array_sized_new(repos->len);
+        // Enabled repositories with prouctid cert that are actively used
         GPtrArray *activeRepos = g_ptr_array_sized_new(repos->len);
 
         getEnabled(repos, enabledRepos);
-        getActive(dnfContext, repos, activeRepos);
 
         for (unsigned int i = 0; i < enabledRepos->len; i++) {
             DnfRepo *repo = g_ptr_array_index(enabledRepos, i);
@@ -179,18 +233,24 @@ int pluginHook(PluginHandle *handle, PluginHookId id, void *hookData, PluginHook
             else {
                 LrYumRepoMdRecord *repoMdRecord = lr_yum_repomd_get_record(repoMd, "productid");
                 if (repoMdRecord) {
-                    fetchProductId(repo);
+                    int ret = fetchProductId(repo);
+                    if(ret == 1) {
+                        g_ptr_array_add(enabledProdIDRepos, repo);
+                    }
                 }
             }
         }
+
+        getActive(dnfContext, enabledProdIDRepos, activeRepos);
     }
     return 1;
 }
 
-void fetchProductId(DnfRepo *repo) {
+int fetchProductId(DnfRepo *repo) {
     GError *tmp_err = NULL;
     LrHandle *lrHandle = dnf_repo_get_lr_handle(repo);
     char *downloadList[] = {"productid", NULL};
+    int ret_val = 0;
 
     LrHandle *h = lr_handle_init();
     LrResult *r = lr_result_init();
@@ -209,11 +269,14 @@ void fetchProductId(DnfRepo *repo) {
     if (ret) {
         char *destdir;
         lr_handle_getinfo(h, &tmp_err, LRI_DESTDIR, &destdir);
-        printf("Dest dir is %s\n", destdir);
+        printf("Product id cert downloaded to dest dir is %s\n", destdir);
+        ret_val = 1;
     } else {
         printError(tmp_err);
     }
 
     lr_handle_free(h);
     lr_result_free(r);
+
+    return ret_val;
 }
