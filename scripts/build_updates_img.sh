01854cfe46909f48d3c6ebc901faab452f9fb472
Jiri Hnidek <jhnidek@redhat.com>
2019-01-28 12:24:41 +0100

    More reliable PXE server and PXE client
    * Subman requires more modules on Fedora 29 (ethtool)
    * Updated build script to copy Python eggs
    * Stop autobuild service during provisioning, because
      command: `vagrant provision pxe-server` did not work properly
    * Clean all *.pyc file, because `vagrant rsync pxe-server`
      copy all files to VM. *.pyc files in ./build directory can
      break bulding of updates.img

--- scripts/build_updates_img.sh
+++ scripts/build_updates_img.sh
@@ -51,6 +51,9 @@ if [ "$PYTHON_BIN" = "" ]; then
     echo "Python interpreter not specified, using ${PYTHON_BIN}"
 fi
 
+# Delete all *.pyc file, because Python 2.7 .pyc files can break building of subman at system
+# using Python 3.x and vice versa
+find . -name "*.pyc" -delete
 
 # Install files to the temporary build dir
 export DESTDIR=$UPDATE_DIR
@@ -64,23 +67,50 @@ make -e install
 PYTHON_SITE_PACKAGE_DIRS=`${PYTHON_BIN} -c "import site; print(' '.join(site.getsitepackages()))"`
 
 # List of required Python packages
-PY_PACKAGES="dateutil"
+PY_PACKAGES="dateutil ethtool"
 
 # Copy required Python modules to updates.img too
 for pkg in `echo ${PY_PACKAGES}`
 do
+        echo "Trying to find: ${pkg} in directory ..."
         for site_pkg_dir in `echo ${PYTHON_SITE_PACKAGE_DIRS}`
         do
+                echo -e "\t${site_pkg_dir}"
+                # Common package
                 if [ -d "${site_pkg_dir}/${pkg}" ]
                 then
                         mkdir -p "${UPDATE_DIR}/${site_pkg_dir}"
-                        echo "copying ${site_pkg_dir}/${pkg} to ${UPDATE_DIR}/${site_pkg_dir}"
+                        echo "Copying ${site_pkg_dir}/${pkg} to ${UPDATE_DIR}/${site_pkg_dir}"
                         cp -R "${site_pkg_dir}/${pkg}" "${UPDATE_DIR}/${site_pkg_dir}"
-                        break
+                fi
+                # Binary module
+                if [ -f "${site_pkg_dir}/${pkg}"*".so" ]
+                then
+                        mkdir -p "${UPDATE_DIR}/${site_pkg_dir}"
+                        echo "Copying ${site_pkg_dir}/${pkg}"*".so to ${UPDATE_DIR}/${site_pkg_dir}"
+                        cp "${site_pkg_dir}/${pkg}"*".so" "${UPDATE_DIR}/${site_pkg_dir}"
                 fi
         done
 done
 
+# List of Python eggs (yes, names can be different from package names)
+PY_PACKAGES="python_dateutil ethtool"
+
+# Copy required Python eggs to updates.img too
+for pkg in `echo ${PY_PACKAGES}`
+do
+        echo "Trying to find: ${pkg} in directory ..."
+        for site_pkg_dir in `echo ${PYTHON_SITE_PACKAGE_DIRS}`
+        do
+                # Python egg
+                if [ -d "${site_pkg_dir}/${pkg}"*".egg-info" ]
+                then
+                        mkdir -p "${UPDATE_DIR}/${site_pkg_dir}"
+                        echo "Copying ${site_pkg_dir}/${pkg}"*".egg-info to ${UPDATE_DIR}/${site_pkg_dir}"
+                        cp -R "${site_pkg_dir}/${pkg}"*".egg-info" "${UPDATE_DIR}/${site_pkg_dir}"
+                fi
+        done
+done
 pushd $UPDATE_DIR
 
 # Build img file (See https://fedoraproject.org/wiki/Anaconda/Updates)
