d9097fa42e668bf74ae6ff0d990c874c3e7abd43
Christopher Snyder <csnyder@redhat.com>
2019-11-18 16:51:11 +0100

    Automatic commit of package [subscription-manager] release [1.27.0-1].
    Created by command:
    
    /usr/local/bin/tito tag --use-version 1.27.0 --use-release 1%{?dist}

--- setup.py
+++ setup.py
@@ -347,7 +347,7 @@ cmdclass = {
 
 setup(
     name="subscription-manager",
-    version='1.26.4',
+    version='1.27.0',
     url="http://www.candlepinproject.org",
     description="Manage subscriptions for Red Hat products.",
     license="GPLv2",
