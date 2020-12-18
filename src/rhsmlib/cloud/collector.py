de8af64af19819ebb4382fb6ccce44986b6d177e
Jiri Hnidek <jhnidek@redhat.com>
2020-12-18 16:28:55 +0100

    Detecting of cloud providers; ENT-3288 (#2367)
    * Detecting of cloud providers; ENT-3288
    
    * Created new package rhsmlib.cloud. This package contains
      two modules with base classes for:
      - Detecting cloud providers (CloudDetector)
      - Collecting metadata from cloud providers (CloudCollector)
    * The Python package 'cloud' contains another package called 'providers'.
      This package contains Python modules implementing subclasses of
      CloudCollector. Each subclass implement detecting of one cloud
      provider. Three main cloud providers are supported ATM:
      - Amazone Web Services (AWS)
      - Microsoft Azure
      - Google Cloud Platform
    * Each detector includes two methods for detecting cloud provider.
      First method 'is_running_on_cloud()' uses some unique
      system facts to identify cloud provider. Values of these
      facts can change over time. For this reason there is
      another method called 'is_likely_running_on_cloud' and this
      method uses some heuristics.
    * It will be easy to add support for other cloud providers
    * Collecting of metadata is not implemented ATM and it will be
      implemented in another commit/PR
    * Detecting of AWS cloud provider is used in module cloud_facts
    * Changed a little bit logic of collecting facts in 'all'
      Python module to reuse collected facts
    * Collecting of cloud facts is not done in host_collector
      to avoid cyclic collecting in the future.
    * Added some smoke tests to code to be able to test code
      on VM running on public clouds
    * Added several unit tests
    * Modified unit tests
    
    * WIP: Fix stylish error and rpm build; renamed one file and fixed minor issues.
    
    * Fixed some issues.
    
    * Fixed few comments.
    
    * Fix the case, when there is conflict in strong signs.

---
+++ src/rhsmlib/cloud/collector.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2020 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+#
+# Red Hat trademarks are not licensed under GPLv2. No permission is
+# granted to use or replicate Red Hat trademarks that are incorporated
+# in this software or its documentation.
+#
+
+"""
+This module implements base class for collecting metadata from cloud provider
+"""
+
+
+class CloudCollector(object):
+    """
+    Base class for collecting metadata and signature of metadata from cloud
+    provider. The most of logic is implemented in this class. Subclasses
+    for concrete cloud providers usually contains only default values in
+    class attributes. All values will be usually loaded from configuration
+    files of cloud providers. It is/will be still possible to implement
+    custom method for e.g. getting metadata from cloud provider.
+    """
+
+    # Unique ID of cloud provider
+    # (e.g. "aws", "azure", "gcp", etc.)
+    CLOUD_PROVIDER_ID = None
+
+    # Path to configuration file of collector (ini file)
+    # (e.g. /etc/rhsm/cloud_providers/cool_cloud.conf
+    COLLECTOR_CONF_FILE = None
+
+    # Default value of server URL providing metadata
+    # (e.g. http://1.2.3.4./path/to/metadata/document)
+    CLOUD_PROVIDER_METADATA_URL = None
+
+    # Type of metadata document returned by server
+    # (e.g. json, xml)
+    CLOUD_PROVIDER_METADATA_TYPE = None
+
+    # Default value of server URL providing signature of metadata
+    # (e.g. http://1.2.3.4/path/to/signature/document)
+    CLOUD_PROVIDER_SIGNATURE_URL = None
+
+    # Type of signature document returned by server
+    # (e.g. json, xml, pem)
+    CLOUD_PROVIDER_SIGNATURE_TYPE = None
+
+    # Default value of path to cache file holding metadata
+    # (e.g. /var/lib/rhsm/cache/cool_cloud_metadata.json)
+    METADATA_CACHE_FILE = None
+
+    # Default value of path to holding signature of metadata
+    # (e.g. /var/lib/rhsm/cache/cool_cloud_signature.json)
+    SIGNATURE_CACHE_FILE = None
+
+    def __init__(self):
+        """
+        Initialize instance of CloudCollector
+        """
+        self.metadata = None
+        self.signature = None
+
+    def _get_collector_configuration_from_file(self):
+        """
+        Get configuration of collector from json file
+        :return: True, when it was possible to load configuration file of collector
+        """
+        raise NotImplementedError
+
+    def _get_metadata_from_cache(self):
+        """
+        Method for gathering metadata from cache file
+        :return: string containing metadata
+        """
+        raise NotImplementedError
+
+    def _get_metadata_from_server(self):
+        """
+        Method for gathering metadata from server
+        :return: string containing metadata
+        """
+        raise NotImplementedError
+
+    def _get_signature_from_cache_file(self):
+        """
+        Try to get signature from cache file
+        :return: string containing signature
+        """
+        raise NotImplementedError
+
+    def _get_signature_from_server(self):
+        """
+        Method for gathering signature of metadata from server
+        :return:
+        """
+        raise NotImplementedError
+
+    def get_signature(self):
+        """
+        Public method for getting signature (cache file or server)
+        :return:
+        """
+        raise NotImplementedError
+
+    def get_metadata(self):
+        """
+        Public method for getting metadata (cache file or server)
+        :return:
+        """
+        raise NotImplementedError
---
+++ src/rhsmlib/cloud/detector.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2020 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+#
+# Red Hat trademarks are not licensed under GPLv2. No permission is
+# granted to use or replicate Red Hat trademarks that are incorporated
+# in this software or its documentation.
+#
+
+"""
+This module implements the base class for detecting cloud provider
+"""
+
+
+class CloudDetector(object):
+    """
+    Base class used for detecting cloud provider
+    """
+
+    ID = None
+
+    def __init__(self, hw_info):
+        """
+        Initialize cloud detector
+        """
+        self.hw_info = hw_info
+
+    def is_vm(self):
+        """
+        Is current system virtual machine?
+        :return: Return True, when it is virtual machine; otherwise return False
+        """
+        return 'virt.is_guest' in self.hw_info and self.hw_info['virt.is_guest'] is True
+
+    def is_running_on_cloud(self):
+        """
+        Try to guess cloud provider using collected hardware information (output of dmidecode, virt-what, etc.)
+        :return: True, when we detected sign of cloud provider in hw info; Otherwise return False
+        """
+        raise NotImplementedError
+
+    def is_likely_running_on_cloud(self):
+        """
+        When all subclasses cannot detect cloud provider using method is_running_on_cloud, because cloud provider
+        started to provide something else in output of dmidecode, then try to use this heuristics method
+        :return: Float value representing probability that vm is running using specific cloud provider
+        """
+        raise NotImplementedError
