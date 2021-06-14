2fe0df61f90a4d47b04d35009d24dd3b67de2646
Pino Toscano <ptoscano@redhat.com>
2021-06-14 13:56:13 +0200

    Rename internal variable for syspurpose --show
    By default, a '--show' option in ArgumentParser is saved as a namespace
    variable 'show'. While it is currently not a problem for the '--show'
    option of the 'syspurpose' command, it will be a problem when adding the
    options of the other syspurpose subcommands as subparsers of the
    'syspurpose' command.
    
    Hence, bind the ArgumentParser option result to a less generic variable
    name to avoid clashes.

--- src/subscription_manager/cli_command/syspurpose.py
+++ src/subscription_manager/cli_command/syspurpose.py
@@ -52,6 +52,7 @@ class SyspurposeCommand(CliCommand):
         )
         self.parser.add_argument(
             "--show",
+            dest='syspurpose_show',
             action="store_true",
             help=_("show current system purpose")
         )
@@ -62,8 +63,8 @@ class SyspurposeCommand(CliCommand):
         :return: None
         """
         # When no CLI options are provided, then show current syspurpose values
-        if self.options.show is not True:
-            self.options.show = True
+        if self.options.syspurpose_show is not True:
+            self.options.syspurpose_show = True
 
     def _do_command(self):
         """
@@ -73,7 +74,7 @@ class SyspurposeCommand(CliCommand):
         self._validate_options()
 
         content = {}
-        if self.options.show is True:
+        if self.options.syspurpose_show is True:
             if self.is_registered():
                 try:
                     self.cp = self.cp_provider.get_consumer_auth_cp()
