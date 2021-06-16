1b23a23ad2fdfdce8eb215fa96e0036724347399
James Molet <jmolet@redhat.com>
2021-06-16 15:46:44 -0400

    fixing tito tests in containers

--- jenkins/tito-tests.sh
+++ jenkins/tito-tests.sh
@@ -10,9 +10,16 @@ pushd "${WORKSPACE}"
 mkdir tito/
 
 # Use nexus mirror if available
-NPM_REGISTRY="$(ping -c1 -W1 repository.engineering.redhat.com > /dev/null 2>&1 &&\
-       	echo 'https://repository.engineering.redhat.com/nexus/repository/registry.npmjs.org' ||\
-       	echo 'https://registry.npmjs.org')"
+NPM_REGISTRY='https://registry.npmjs.org'
+if ping -c1 -W1 repository.engineering.redhat.com > /dev/null 2>&1; then
+	NPM_REGISTRY='https://repository.engineering.redhat.com/nexus/repository/registry.npmjs.org'
+	if [ -f /run/secrets/rh-it.crt ]; then
+		sudo cp /run/secrets/rh-it.crt /etc/pki/ca-trust/source/anchors/
+		sudo /usr/bin/update-ca-trust
+	else
+		npm config set strict-ssl false
+	fi
+fi
 npm config set registry $NPM_REGISTRY
 sudo yum-builddep subscription-manager.spec -y || true
 
