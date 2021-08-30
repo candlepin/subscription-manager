ad4d8c4158aa51c704ac80a7006767ecbf514113
Christopher Snyder <csnyder@redhat.com>
2021-08-30 16:10:26 -0400

    Add number of argument checking to toolbox-run.sh

--- jenkins/toolbox-run.sh
+++ jenkins/toolbox-run.sh
@@ -3,6 +3,11 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 
 # Run one of our tests inside a newly created toolbox container
 
+if (( $# != 2)); then
+    >&2 echo -e "This script requires two arguments.\nUsage: $0 [END_OF_TAG] [PATH_TO_SCRIPT_TO_RUN_IN_CONTAINER]\nThis script will create a toolbox container, run the script provided in it, and forcefully destroy the toolbox container."
+    exit
+fi
+
 pushd $PROJECT_ROOT
 toolbox create -i quay.io/candlepin/subscription-manager:PR-$CHANGE_ID -c PR-$CHANGE_ID-$1
 toolbox run -c PR-$CHANGE_ID-$1 sh $2
