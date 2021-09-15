5bdc45a672056fc0be9f47d86cd33aba9e1af328
James Molet <jmolet@redhat.com>
2021-09-15 10:53:51 -0400

    bash linter ran, and dnf where on fedora

--- jenkins/toolbox-run.sh
+++ jenkins/toolbox-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-PROJECT_ROOT=$(git rev-parse --show-toplevel)
+PROJECT_ROOT="$(git rev-parse --show-toplevel)"
 
 # Run one of our tests inside a newly created toolbox container
 
@@ -13,10 +13,10 @@ EOF
     exit
 fi
 
-pushd $PROJECT_ROOT
-toolbox create -i quay.io/candlepin/subscription-manager:PR-$CHANGE_ID -c PR-$CHANGE_ID-$1
-toolbox run -c PR-$CHANGE_ID-$1 sh $2
+pushd "$PROJECT_ROOT" || exit 1
+toolbox create -i "quay.io/candlepin/subscription-manager:PR-$CHANGE_ID" -c "PR-$CHANGE_ID-$1"
+toolbox run -c "PR-$CHANGE_ID-$1" sh "$2"
 RETVAL=$?
-toolbox rm --force PR-$CHANGE_ID-$1
+toolbox rm --force "PR-$CHANGE_ID-$1"
 popd
-exit $RETVAL
+exit "$RETVAL"
