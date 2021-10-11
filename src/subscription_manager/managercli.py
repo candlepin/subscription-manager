76b25c43111aca40d1f88f9c3c3525d6dafd3a46
Christopher Snyder <csnyder@redhat.com>
2021-10-11 14:55:09 -0400

    2011843: Clear content access mode cache on refresh

--- src/subscription_manager/managercli.py
+++ src/subscription_manager/managercli.py
@@ -1052,6 +1052,11 @@ class RefreshCommand(CliCommand):
             content_access = inj.require(inj.CONTENT_ACCESS_CACHE)
             if content_access.exists():
                 content_access.remove()
+            # Also remove the content access mode cache to be sure we display
+            # SCA or regular mode correctly
+            content_access_mode = inj.require(inj.CONTENT_ACCESS_MODE_CACHE)
+            if content_access_mode.exists():
+                content_access_mode.delete_cache()
 
             if self.options.force is True:
                 # get current consumer identity
