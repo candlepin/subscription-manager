5e75dcdd1b12cfdfe06d98b57e0ceb8db4947cec
Jiri Hnidek <jhnidek@redhat.com>
2021-10-19 11:58:08 +0200

    Use pytest --forked for D-Bus unit tests
    * Each D-Bus unit test is run in it's own process, because unit tests
      of D-Dus are not reliable without this change
    * Marked od D-Bus unit cases subman_marker_dbus was modified in
      test/__init__.py
    * Configuration file of pytest.ini was modified
      - dbus unit test are allowed again
      - The module pytest-forked is required
    * Module pytest-forked was add to test-requirements.txt

--- test/__init__.py
+++ test/__init__.py
@@ -10,12 +10,21 @@ rootdir = pathlib.Path(__file__).parent.parent
 sys.path.insert(0, str(rootdir / "src"))
 
 
-subman_marker_dbus = pytest.mark.dbus
 subman_marker_functional = pytest.mark.functional
 subman_marker_zypper = pytest.mark.zypper
 subman_marker_slow = pytest.mark.slow
 # This allows us to set higher timeout limit for tests that are known to be slow
 subman_marker_slow_timeout = pytest.mark.timeout(40)
+
+
+def subman_marker_dbus(func: Callable) -> Callable:
+    """
+    Fork each D-Bus unit test into it's own process, because we use threads
+    and mocking in these tests and such tests are not reliable without forking.
+    :param func: function of D-Bus unit test
+    :return: function of unit test marked as forked
+    """
+    return pytest.mark.dbus(pytest.mark.forked(func))
 
 
 def subman_marker_needs_envvars(*envvars: List[str]) -> Callable:
