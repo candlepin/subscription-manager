772aa5d9a886b8c71bd10af499313df95ee67186
Christopher Snyder <csnyder@redhat.com>
2021-11-11 17:47:31 -0500

    Clean up old toolbox containers

--- containers/build_and_push.sh
+++ containers/build_and_push.sh
@@ -3,6 +3,8 @@ CURRENT_GIT_HASH="$(git rev-parse HEAD)"
 PROJECT_ROOT="$(git rev-parse --show-toplevel)"
 
 pushd "$PROJECT_ROOT" || exit 1
+pgrep -f toolbox | xargs --no-run-if-empty -iPID kill PID
+toolbox rm --all --force || true
 podman build -f ./containers/Containerfile -t "quay.io/candlepin/subscription-manager:$CURRENT_GIT_HASH"
 podman push --creds "$QUAY_CREDS" "quay.io/candlepin/subscription-manager:$CURRENT_GIT_HASH"
 podman tag "quay.io/candlepin/subscription-manager:$CURRENT_GIT_HASH" "quay.io/candlepin/subscription-manager:PR-$CHANGE_ID"
