78b720c5e2c409d6a8c2aa2f34dcb77b5f3ba225
Jiri Hnidek <jhnidek@redhat.com>
2022-05-04 20:52:47 +0200

    2073354: Print correct status, when access mode has changed
    * BZ: https://bugzilla.redhat.com/show_bug.cgi?id=2073354
    * Card ID: ENT-4885
    * When content access mode has changed from SCA mode to
      entitlement mode, then sub-man should resurrect from this
      situation gracefully, when status command is triggered
    * Added new service refresh, because it is necessary to
      refresh certificate not only from refresh command, but
      status needs this functionality in edge cases too
    * Refactored code of status command
    * Fixed some unit tests
    * Added unit tests for status command and for this case

--- src/subscription_manager/cli_command/status.py
+++ src/subscription_manager/cli_command/status.py
@@ -22,6 +22,7 @@ from rhsm.certificate2 import CONTENT_ACCESS_CERT_TYPE
 from rhsm.connection import ConnectionException
 
 from rhsmlib.services import entitlement
+from rhsmlib.services.refresh import Refresh
 
 from subscription_manager import syspurposelib
 from subscription_manager.cli import system_exit
@@ -46,26 +47,28 @@ class StatusCommand(CliCommand):
             ),
         )
 
-    def _do_command(self):
-        # list status and all reasons it is not valid
+    def _get_date_cli_option(self):
+        """
+        Try to get and validate command line options date
+        :return: Return date or None, when date was not provided
+        """
         on_date = None
         if self.options.on_date:
             try:
                 on_date = entitlement.EntitlementService.parse_date(self.options.on_date)
             except ValueError as err:
                 system_exit(os.EX_DATAERR, err)
+        return on_date
+
+    def _print_status(self, service_status):
+        """
+        Print only status
+        :return: Print overall status
+        """
 
         print("+-------------------------------------------+")
         print("   " + _("System Status Details"))
         print("+-------------------------------------------+")
-
-        service_status = entitlement.EntitlementService(None).get_status(on_date)
-        reasons = service_status["reasons"]
-
-        if service_status["valid"]:
-            result = 0
-        else:
-            result = 1
 
         ca_message = ""
         has_cert = _(
@@ -73,11 +76,33 @@ class StatusCommand(CliCommand):
         )
 
         certs = self.entitlement_dir.list_with_content_access()
-        ca_certs = [cert for cert in certs if cert.entitlement_type == CONTENT_ACCESS_CERT_TYPE]
-        if ca_certs:
-            ca_message = has_cert
+        sca_certs = [cert for cert in certs if cert.entitlement_type == CONTENT_ACCESS_CERT_TYPE]
+        sca_mode_detected = False
+
+        refresh_service = Refresh(cp=self.cp, ent_cert_lib=self.entcertlib)
+
+        if sca_certs:
+            sca_mode_detected = True
         else:
+            # When there are no entitlement SCA certificates, but status_id is "disabled", then
+            # it means that content access mode has changed on the server and entitlement certificates
+            # have to be refreshed
+            if service_status["status_id"] == "disabled":
+                refresh_service.refresh()
             if is_simple_content_access(uep=self.cp, identity=self.identity):
+                sca_mode_detected = True
+
+        if sca_mode_detected is True:
+            # When SCA mode was detected using cache or installed SCA entitlement certificates, but status_id
+            # is not "disabled", then it means that content access mode has changed on the server and entitlement
+            # certificates have to be refreshed
+            status_id = service_status["status_id"]
+            if status_id != "disabled":
+                log.debug(
+                    f"Found SCA cert, but status ID is not 'disabled' ({status_id}). Refreshing entitlement certs..."
+                )
+                refresh_service.refresh()
+            else:
                 ca_message = has_cert
 
         print(
@@ -86,6 +111,14 @@ class StatusCommand(CliCommand):
             )
         )
 
+    def _print_reasons(self, service_status):
+        """
+        Print reasons for overall status
+        :param service_status:
+        :return: None
+        """
+        reasons = service_status["reasons"]
+
         columns = get_terminal_width()
         for name in reasons:
             print(format_name(name + ":", 0, columns))
@@ -93,6 +126,11 @@ class StatusCommand(CliCommand):
                 print("- {name}".format(name=format_name(message, 2, columns)))
             print("")
 
+    def _print_syspurpose_status(self, on_date):
+        """
+        Print syspurpose status
+        :return: None
+        """
         try:
             store = syspurposelib.get_sys_purpose_store()
             if store:
@@ -111,5 +149,26 @@ class StatusCommand(CliCommand):
                 for reason in reasons:
                     print("- {reason}".format(reason=reason))
         print("")
+
+    def _do_command(self):
+        """
+        Print status and all reasons it is not valid
+        """
+
+        # First get/check if provided date is valid
+        on_date = self._get_date_cli_option()
+
+        service_status = entitlement.EntitlementService(cp=self.cp).get_status(on_date)
+
+        self._print_status(service_status)
+
+        self._print_reasons(service_status)
+
+        self._print_syspurpose_status(on_date)
+
+        if service_status["valid"]:
+            result = 0
+        else:
+            result = 1
 
         return result
