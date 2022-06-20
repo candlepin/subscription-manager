a5e58e656736a481ddc5b72c187f9eb63afc1659
Pino Toscano <ptoscano@redhat.com>
2022-06-20 16:35:56 +0200

    facts: add dmidecode parser and facts collector
    Create a new parser for parsing the output of the dmidecode(1) tool,
    collecting all the data from it. Add a test for it with existing outputs
    to check, as dmidecode information can be read only as root, and they
    would change on each system. The data-based tests check that the parser
    works fine, and extracts at least the system UUID.
    The test data have sensible details (UUIDs, serial numbers, product
    names, and part numbers) replaced with placeholders; this also helps
    testing that the parsed output contains the values we expect.
    
    In addition to that, add a facts collector that uses this new parser;
    to make sure the facts resemble somehow what the python-dmidecode-based
    one returns, do few quirks to adapt the data read from dmidecode(1).
    Add a test for this new collector, using the aforementioned test data,
    and checking that at least some subtags of "dmi" facts are collected.
    
    (cherry picked from commit b100b50f4385286b2604bf60bf4777d3e85963b5)

--- src/rhsmlib/facts/dmiinfo.py
+++ src/rhsmlib/facts/dmiinfo.py
@@ -16,10 +16,12 @@
 Note: This module will fail to import if dmidecode fails to import.
       firmware_info.py expects that and handles it, and any other
       module that imports it should handle an import error as well."""
+import contextlib
 import logging
 import os
 
 from rhsmlib.facts import collector
+from rhsmlib.facts.dmidecodeparser import DmidecodeParser
 
 log = logging.getLogger(__name__)
 
@@ -124,3 +126,99 @@ class DmiFirmwareInfoCollector(collector.FactsCollector):
         if dmiwarnings:
             log.warning(f"Warnings while reading system DMI information:\n{dmiwarnings}")
             dmidecode.clear_warnings()
+
+
+class DmidecodeFactCollector(collector.FactsCollector):
+    def __init__(self, prefix=None, testing=None, collected_hw_info=None):
+        super(DmidecodeFactCollector, self).__init__(
+            prefix=prefix, testing=testing, collected_hw_info=collected_hw_info
+        )
+
+        self._dmidecode_output = None
+
+    def set_dmidecode_output(self, filename):
+        self._dmidecode_output = filename
+
+    def get_all(self):
+        """
+        Collect facts from the dmidecode output, if available.
+
+        There are different quirks done to make the facts returned closer
+        to the way python-dmidecode used to return them.
+        """
+        parser = DmidecodeParser()
+        try:
+            if self._dmidecode_output is not None:
+                parser.parse_file(self._dmidecode_output)
+            else:
+                parser.parse()
+        except Exception as exc:
+            log.warning("Failed to parse the dmidecode output: {exc}")
+            log.exception(exc)
+            return {}
+
+        dmiinfo = {}
+        socket_designations = 0
+        # map the various DMI types to the various subtags of "dmi" facts;
+        # there can be multiple types for the same subtag, as python-dmidecode
+        # aggregated them
+        tags = {
+            DmidecodeParser.DmiTypes.BIOS_INFORMATION: "dmi.bios.",
+            DmidecodeParser.DmiTypes.BIOS_LANGUAGE_INFORMATION: "dmi.bios.",
+            DmidecodeParser.DmiTypes.PROCESSOR_INFORMATION: "dmi.processor.",
+            DmidecodeParser.DmiTypes.BASEBOARD_INFORMATION: "dmi.baseboard.",
+            DmidecodeParser.DmiTypes.SYSTEM_ENCLOSURE_OR_CHASSIS: "dmi.chassis.",
+            DmidecodeParser.DmiTypes.SYSTEM_SLOTS: "dmi.slot.",
+            DmidecodeParser.DmiTypes.SYSTEM_INFORMATION: "dmi.system.",
+            DmidecodeParser.DmiTypes.SYSTEM_CONFIGURATION_OPTIONS: "dmi.system.",
+            DmidecodeParser.DmiTypes.MEMORY_DEVICE: "dmi.memory.",
+            DmidecodeParser.DmiTypes.PHYSICAL_MEMORY_ARRAY: "dmi.memory.",
+            DmidecodeParser.DmiTypes.PORT_CONNECTOR_INFORMATION: "dmi.connector.",
+        }
+        for dmi_type, facts_tag in tags.items():
+            try:
+                sections = parser.get_sections(dmi_type)
+            except KeyError:
+                continue
+            # quirk: use the last handle (likely the one with an higher value)
+            # in a similar way to what python-dmidecode did
+            section = sections[-1]
+            for key, value in section.items():
+                if not isinstance(value, str):
+                    # we are skipping lists
+                    continue
+
+                nkey = "".join([facts_tag, key.lower()]).replace(" ", "_")
+                nvalue = value
+                if nvalue.startswith("0x"):
+                    # quirk: hex value, lowercase it like python-dmidecode did
+                    nvalue = value.lower()
+                elif key == "UUID":
+                    # quirk: UUID, uppercase it like python-dmidecode did
+                    nvalue = value.upper()
+                dmiinfo[nkey] = nvalue
+
+        try:
+            sections = parser.get_sections(DmidecodeParser.DmiTypes.PROCESSOR_INFORMATION)
+        except KeyError:
+            pass
+        else:
+            socket_designations = sum(1 for s in sections for k in s.keys() if k == "Socket Designation")
+            # Populate how many socket descriptions we have in a faux-fact,
+            # so we can use it to munge lscpu info later if needed.
+            if socket_designations > 0:
+                dmiinfo["dmi.meta.cpu_socket_count"] = str(socket_designations)
+
+        try:
+            sections = parser.get_sections(DmidecodeParser.DmiTypes.MEMORY_DEVICE)
+        except KeyError:
+            pass
+        else:
+            # quirk: set dmi.memory.size based on the first "useful" value
+            # among all the memory devices available
+            with contextlib.suppress(StopIteration, KeyError):
+                dmiinfo["dmi.memory.size"] = next(
+                    s["Size"] for s in sections if s["Size"] != "No Module Installed"
+                )
+
+        return dmiinfo
