ff22226aaf553656077a3b67c20933b94cf07935
Martin Pitt <martin@piware.de>
2022-07-25 07:13:31 +0200

    webpack: Avoid md4 hash for OpenSSL 3 compatibility
    CentOS/RHEL 9 switched to OpenSSL 3, which does not support the `md4`
    hash any more. webpack 5 hardcodes that in no less than 21 places, so
    monkey-patch `crypto.createHash()` to substitute sha256 for md4.
    
    Hack to work around https://github.com/webpack/webpack/issues/13572
    
    Cherry-picked from subscription-manager-cockpit commit 8e480295a89

--- cockpit/webpack.config.js
+++ cockpit/webpack.config.js
@@ -4,6 +4,10 @@ const fs = require("fs");
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+// HACK: OpenSSL 3 does not support md4 any more, but webpack hardcodes it all over the place: https://github.com/webpack/webpack/issues/13572
+const crypto = require("crypto");
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
 const webpack = require("webpack");
 const CompressionPlugin = require("compression-webpack-plugin");
 const extract = require("mini-css-extract-plugin");
