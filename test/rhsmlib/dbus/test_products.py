684e9d22e09004614fa642b67f194f8a27df5ade
Matyas Horky <mhorky@redhat.com>
2022-09-05 10:41:28 +0200

    ENT-5317: Update DBus tests of Products object
    * Card ID: ENT-5317
    
    (Cherry-picked from eaac363)

--- test/rhsmlib/dbus/test_products.py
+++ test/rhsmlib/dbus/test_products.py
@@ -10,20 +10,13 @@
 # Red Hat trademarks are not licensed under GPLv2. No permission is
 # granted to use or replicate Red Hat trademarks that are incorporated
 # in this software or its documentation.
-import dbus
 import mock
 import json
 import datetime
 
-from test.rhsmlib.base import DBusObjectTest, InjectionMockingTest
-
-from subscription_manager import injection as inj
-from subscription_manager.cp_provider import CPProvider
-
 from rhsmlib.dbus.objects import ProductsDBusObject
-from rhsmlib.dbus import constants
 
-from test import subman_marker_dbus
+from test.rhsmlib.base import DBusServerStubProvider
 
 
 START_DATE = datetime.datetime.now() - datetime.timedelta(days=100)
@@ -31,34 +24,23 @@ NOW_DATE = datetime.datetime.now()
 END_DATE = datetime.datetime.now() + datetime.timedelta(days=265)
 
 
-@subman_marker_dbus
-class TestProductsDBusObject(DBusObjectTest, InjectionMockingTest):
-    def setUp(self):
-        super(TestProductsDBusObject, self).setUp()
-        self.proxy = self.proxy_for(ProductsDBusObject.default_dbus_path)
-        self.interface = dbus.Interface(self.proxy, constants.PRODUCTS_INTERFACE)
+class TestProductsDBusObject(DBusServerStubProvider):
+    dbus_class = ProductsDBusObject
+    dbus_class_kwargs = {}
 
-        products_patcher = mock.patch("rhsmlib.dbus.objects.products.InstalledProducts")
-        self.mock_products = products_patcher.start().return_value
-        self.addCleanup(products_patcher.stop)
-        self.mock_identity.is_valid.return_value = True
-        self.mock_identity.uuid = "id"
+    @classmethod
+    def setUpClass(cls) -> None:
+        list_patch = mock.patch(
+            "rhsmlib.dbus.objects.products.InstalledProducts.list",
+            name="list",
+        )
+        cls.patches["list"] = list_patch.start()
+        cls.addClassCleanup(list_patch.stop)
 
-    def injection_definitions(self, *args, **kwargs):
-        if args[0] == inj.IDENTITY:
-            return self.mock_identity
-        elif args[0] == inj.CP_PROVIDER:
-            provider = mock.Mock(spec=CPProvider, name="CPProvider")
-            provider.get_consumer_auth_cp.return_value = mock.Mock(name="MockCP")
-            return provider
-        else:
-            return None
+        super().setUpClass()
 
-    def dbus_objects(self):
-        return [ProductsDBusObject]
-
-    def test_list_installed_products_without_filter(self):
-        expected_result = [
+    def test_ListInstalledProducts__no_filter(self):
+        expected = [
             (
                 "Red Hat Enterprise Linux Server",
                 "69",
@@ -81,11 +63,7 @@ class TestProductsDBusObject(DBusObjectTest, InjectionMockingTest):
             ),
         ]
 
-        def assertions(*args):
-            result = args[0]
-            self.assertEqual(result, json.dumps(expected_result))
+        self.patches["list"].return_value = expected
 
-        self.mock_products.list.return_value = expected_result
-
-        dbus_method_args = ["", {}, ""]
-        self.dbus_request(assertions, self.interface.ListInstalledProducts, dbus_method_args)
+        result = self.obj.ListInstalledProducts.__wrapped__(self.obj, "", {}, self.LOCALE)
+        self.assertEqual(json.dumps(expected), result)
