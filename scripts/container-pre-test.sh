fcfe5bbbe23374d368422cc2e2ed0e4e960c86f4
Jiri Hnidek <jhnidek@redhat.com>
2024-01-17 11:59:47 +0100

    fix: Add missing package intltool to test script

--- scripts/container-pre-test.sh
+++ scripts/container-pre-test.sh
@@ -9,7 +9,7 @@ yum install -y \
 # Install system, build and runtime packages
 yum install -y \
   gtk3-devel python-ethtool \
-  openssl-devel swig
+  openssl-devel swig intltool
 
 localedef -c -i en_US -f ISO-8859-1 en_US
 localedef -c -i en_US -f UTF-8 en_US.UTF-8
