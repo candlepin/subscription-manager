c24b18af4d5e39b3a2ae65017127e2597a2875eb
Martin Grünwald <mgrunwal@redhat.com>
2024-06-14 13:11:55 +0200

    feat: remove command 'redeem' from subscription-manager
    Code lines referencing or using	'redeem' command object were deleted.
    Assocciated files defining command 'redeem' and test for it were
    removed
    Command	'redeem' was removed from bash autocompletion and man pages.
    
    CARD ID: CCT-242
    
    Signed-off-by: Martin Grünwald <mgrunwal@redhat.com>

--- test/cli_command/test_redeem.py
+++
@@ -1,6 +0,0 @@
-from ..test_managercli import TestCliProxyCommand
-from subscription_manager import managercli
-
-
-class TestRedeemCommand(TestCliProxyCommand):
-    command_class = managercli.RedeemCommand
