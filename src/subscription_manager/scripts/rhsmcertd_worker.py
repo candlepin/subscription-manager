9908293fe38006d689d4d2e6f6492d7db60e7cc1
Pino Toscano <ptoscano@redhat.com>
2024-08-20 10:59:39 +0200

    feat: forcefully switch automatic cloud registration to v1
    - call the cloud endpoint without the new version, and with the Accept
      for the existing endpoint
    - use cache.CloudTokenCache._get_from_server() directly: the v1
      autoregistration does not need any caching of the received token, so
      use the internal API of CloudTokenCache only to fetch the token
    - the received token is the JWT token to use directly, without any
      inspection or unpacking: pass it directly to _auto_register_standard()
    - drop the leftover commented code for autoregistration v2 for now

--- src/subscription_manager/scripts/rhsmcertd_worker.py
+++ src/subscription_manager/scripts/rhsmcertd_worker.py
@@ -191,7 +191,7 @@ def _auto_register(cp_provider: "CPProvider") -> ExitStatus:
 
     # Obtain automatic registration token
     try:
-        token: Dict[str, str] = cache.CloudTokenCache.get(
+        token: Dict[str, str] = cache.CloudTokenCache._get_from_server(
             uep=uep,
             cloud_id=cloud_info["cloud_id"],
             metadata=cloud_info["metadata"],
@@ -201,33 +201,14 @@ def _auto_register(cp_provider: "CPProvider") -> ExitStatus:
         log.exception("Cloud token could not be obtained. Unable to perform automatic registration.")
         return ExitStatus.NO_REGISTRATION_TOKEN
 
-    if token["tokenType"] == "CP-Cloud-Registration":
-        try:
-            _auto_register_standard(uep=uep, token=token)
-        except Exception:
-            log.exception("Standard automatic registration failed.")
-            return ExitStatus.REGISTRATION_FAILED
-        else:
-            log.info("Standard automatic registration was successful.")
-            return ExitStatus.OK
-
-    # Uncomment the following block to enable automatic cloud registration v2.
-    # This feature has been disabled temporarily to ensure we do not cause
-    # regressions by not having the backend fully prepared and tested.
-
-    # if token["tokenType"] == "CP-Anonymous-Cloud-Registration":
-    #     try:
-    #         _auto_register_anonymous(uep=uep, token=token)
-    #         cache.CloudTokenCache.delete_cache()
-    #     except Exception:
-    #         log.exception("Anonymous automatic registration failed.")
-    #         return ExitStatus.REGISTRATION_FAILED
-    #     else:
-    #         log.info("Anonymous automatic registration was successful.")
-    #         return ExitStatus.OK
-
-    log.error(f"Unsupported token type for automatic registration: {token['tokenType']}.")
-    return ExitStatus.BAD_TOKEN_TYPE
+    try:
+        _auto_register_standard(uep=uep, token=token)
+    except Exception:
+        log.exception("Standard automatic registration failed.")
+        return ExitStatus.REGISTRATION_FAILED
+    else:
+        log.info("Standard automatic registration was successful.")
+        return ExitStatus.OK
 
 
 def _auto_register_standard(uep: "UEPConnection", token: Dict[str, str]) -> None:
@@ -240,7 +221,7 @@ def _auto_register_standard(uep: "UEPConnection", token: Dict[str, str]) -> None
     log.debug("Registering the system through standard automatic registration.")
 
     service = RegisterService(cp=uep)
-    service.register(org=None, jwt_token=token["token"])
+    service.register(org=None, jwt_token=token)
 
 
 def _auto_register_anonymous(uep: "UEPConnection", token: Dict[str, str]) -> None:
