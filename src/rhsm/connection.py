d6cbb0aef1b03d75ad0b02fea689c35795f21f4b
mhorky <mhorky@redhat.com>
2024-09-23 10:35:58 +0200

    fix: Handle Retry-After headers better for 429 responses
    * Card ID: CCT-759
    
    We have to ensure we normalize the headers before we search for the
    Retry-After header.
    
    (cherry picked from commit 4c8a44a366fcf0537a6a20458e6e6cbd75010f81)

--- src/rhsm/connection.py
+++ src/rhsm/connection.py
@@ -453,7 +453,11 @@ class RateLimitExceededException(RestlibException):
     def __init__(self, code, msg=None, headers=None):
         super(RateLimitExceededException, self).__init__(code, msg)
         self.headers = headers or {}
-        self.retry_after = safe_int(self.headers.get('retry-after'))
+        self.retry_after = None
+        for header, value in self.headers.items():
+            if header.lower() == "retry-after":
+                self.retry_after = safe_int(value)
+                break
         self.msg = msg or "Access rate limit exceeded"
         if self.retry_after is not None:
             self.msg += ", retry access after: %s seconds." % self.retry_after
