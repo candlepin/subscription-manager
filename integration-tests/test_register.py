a6eb3fa15b01a1e0dfc5ea66155a755e62af66eb
Jan Stavěl @ RedHat <jstavel@redhat.com>
2024-11-27 09:47:10 +0100

    tests for register with proxy added

--- integration-tests/test_register.py
+++ integration-tests/test_register.py
@@ -4,13 +4,14 @@ import contextlib
 from pytest_client_tools.util import Version
 from conftest import RHSMPrivateBus
 from constants import RHSM, RHSM_REGISTER_SERVER, RHSM_REGISTER
+from dasbus.error import DBusError
 
 import sh
 import subprocess
 import json
 import logging
 
-log = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 """
 It is important to run tests as root. Since RegisterServer is a system dbus service.
@@ -28,7 +29,7 @@ def test_register(external_candlepin, subman, test_config):
         private_proxy = private_bus.get_proxy(RHSM.service_name,
                                               RHSM_REGISTER.object_path)
         locale = os.environ.get("LANG", "")
-        private_proxy.Register("",#test_config.get("organization"),
+        private_proxy.Register("",
                                test_config.get("candlepin","username"),
                                test_config.get("candlepin","password"),
                                {},
@@ -36,3 +37,71 @@ def test_register(external_candlepin, subman, test_config):
                                locale)
     assert subman.is_registered
     
+
+def test_register_with_org(external_candlepin, subman, test_config): 
+    """
+    https://www.candlepinproject.org/docs/subscription-manager/dbus_objects.html#methods-6
+    """
+    assert not subman.is_registered
+    proxy = RHSM.get_proxy(RHSM_REGISTER_SERVER)
+    with RHSMPrivateBus(proxy) as private_bus:
+        private_proxy = private_bus.get_proxy(RHSM.service_name,
+                                              RHSM_REGISTER.object_path)
+        locale = os.environ.get("LANG", "")
+        private_proxy.Register(test_config.get("candlepin","org"),
+                               test_config.get("candlepin","username"),
+                               test_config.get("candlepin","password"),
+                               {},
+                               {},
+                               locale)
+    assert subman.is_registered
+   
+
+def test_register_with_wrong_values(external_candlepin, subman, test_config, subtests): 
+    """
+    https://www.candlepinproject.org/docs/subscription-manager/dbus_objects.html#methods-6
+    """
+    assert not subman.is_registered
+    proxy = RHSM.get_proxy(RHSM_REGISTER_SERVER)
+    with RHSMPrivateBus(proxy) as private_bus:
+        private_proxy = private_bus.get_proxy(RHSM.service_name,
+                                              RHSM_REGISTER.object_path)
+        locale = os.environ.get("LANG", "")
+
+        with subtests.test(msg="Register with wrong username"):
+            with pytest.raises(DBusError) as excinfo:
+                private_proxy.Register("",
+                                       "wrong-username",
+                                       test_config.get("candlepin","password"),
+                                       {},
+                                       {},
+                                       locale)
+            logger.debug(f"raised exception: {excinfo}")
+            assert "Invalid username or password." in str(excinfo.value)
+            assert not subman.is_registered
+            
+        with subtests.test(msg="Register with wrong password"):
+            with pytest.raises(DBusError) as excinfo:
+                private_proxy.Register("",
+                                       test_config.get("candlepin","username"),
+                                       "wrong-password",
+                                       {},
+                                       {},
+                                       locale)
+            logger.debug(f"raised exception: {excinfo}")
+            assert "Invalid username or password." in str(excinfo.value)
+            assert not subman.is_registered
+            
+        with subtests.test(msg="Register with wrong organization"):
+            wrong_org="wrong-organization"
+            with pytest.raises(DBusError) as excinfo:
+                private_proxy.Register(wrong_org,
+                                       test_config.get("candlepin","username"),
+                                       test_config.get("candlepin","password"),
+                                       {},
+                                       {},
+                                       locale)
+            logger.debug(f"raised exception: {excinfo}")
+            assert f"Organization {wrong_org} does not exist." in str(excinfo.value)
+            assert not subman.is_registered
+
---
+++ integration-tests/test_register_with_proxy.py
@@ -0,0 +1,83 @@
+import os
+import pytest
+import contextlib
+from pytest_client_tools.util import Version
+from conftest import RHSMPrivateBus
+from constants import RHSM, RHSM_REGISTER_SERVER, RHSM_REGISTER
+from dasbus.error import DBusError
+from dasbus.typing import get_variant, Str
+
+import sh
+import subprocess
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+
+"""
+It is important to run tests as root. Since RegisterServer is a system dbus service.
+And it provides a unix socket connection.
+"""
+
+
+def test_register_with_noauth_proxy(external_candlepin, subman, test_config):
+    """
+    https://www.candlepinproject.org/docs/subscription-manager/dbus_objects.html#methods-6
+
+    a message aka:
+    2024-11-27 03:16:10,163 [DEBUG] subscription-manager:23907:MainThread @connection.py:773 - Using proxy: auto-services.usersys.redhat.com:3129
+    should appear in /var/log/rhsm/rhsm.log file.
+    """
+    assert not subman.is_registered
+    proxy = RHSM.get_proxy(RHSM_REGISTER_SERVER)
+    with RHSMPrivateBus(proxy) as private_bus:
+        private_proxy = private_bus.get_proxy(RHSM.service_name,
+                                              RHSM_REGISTER.object_path)
+        locale = os.environ.get("LANG", "")
+        private_proxy.Register("",
+                               test_config.get("candlepin","username"),
+                               test_config.get("candlepin","password"),
+                               {},
+                               {
+                                   "proxy_hostname": get_variant(Str,test_config.get("noauth_proxy","host")),
+                                   "proxy_port": get_variant(Str,str(test_config.get("noauth_proxy","port")))
+                                },
+                               locale)
+    
+    assert subman.is_registered
+    with open("/var/log/rhsm/rhsm.log","rt") as logfile:
+        assert f"Using proxy:" in logfile.read()
+
+
+@pytest.mark.skip(msg="will be subject of troubleshooting in future")
+def test_register_with_auth_proxy(external_candlepin, subman, test_config):
+    """
+    https://www.candlepinproject.org/docs/subscription-manager/dbus_objects.html#methods-6
+
+    a message aka:
+    2024-11-27 03:16:10,163 [DEBUG] subscription-manager:23907:MainThread @connection.py:773 - Using proxy: auto-services.usersys.redhat.com:3129
+    should appear in /var/log/rhsm/rhsm.log file.
+    """
+    assert not subman.is_registered
+    proxy = RHSM.get_proxy(RHSM_REGISTER_SERVER)
+    with RHSMPrivateBus(proxy) as private_bus:
+        private_proxy = private_bus.get_proxy(RHSM.service_name,
+                                              RHSM_REGISTER.object_path)
+        locale = os.environ.get("LANG", "")
+        private_proxy.Register("",
+                               test_config.get("candlepin","username"),
+                               test_config.get("candlepin","password"),
+                               {},
+                               {
+                                   "proxy_hostname": get_variant(Str,test_config.get("auth_proxy","host")),
+                                   "proxy_port": get_variant(Str,str(test_config.get("auth_proxy","port"))),
+                                   "proxy_userame": get_variant(Str,str(test_config.get("auth_proxy","username"))),
+                                   "proxy_password": get_variant(Str,str(test_config.get("auth_proxy","password")))
+                                },
+                               locale)
+    
+    assert subman.is_registered
+    with open("/var/log/rhsm/rhsm.log","rt") as logfile:
+        assert f"Using proxy:" in logfile.read()
+        
+
