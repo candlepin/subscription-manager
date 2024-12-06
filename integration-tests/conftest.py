7487ee6287863261fae6f1e07aaefbbf24f66009
Jan Stavěl @ RedHat <jstavel@redhat.com>
2024-12-06 22:31:40 +0100

    utils created, parametrized activation keys registration

--- integration-tests/conftest.py
+++ integration-tests/conftest.py
@@ -51,25 +51,6 @@ def register_subman(
     yield subman_session
 
 
-def loop_until(predicate, poll_sec=5, timeout_sec=120):
-    """
-    An helper function to handle a time periond waiting for an external service
-    to update its state.
-
-    an example:
-
-       assert loop_until(lambda: insights_client.is_registered)
-
-    The loop function will retry to run predicate every 5secs
-    until the total time exceeds timeout_sec.
-    """
-    start = time.time()
-    ok = False
-    while (not ok) and (time.time() - start < timeout_sec):
-        time.sleep(poll_sec)
-        ok = predicate()
-    return ok
-
 class RHSMPrivateBus(MessageBus):
     """Representation of RHSM private bus connection that can be used as a context manager."""
 
