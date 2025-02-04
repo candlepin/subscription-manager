4602a95a2910651f08d79bfd83aef11d9db1584e
Pino Toscano <ptoscano@redhat.com>
2025-02-04 10:53:30 +0100

    Automatic commit of package [subscription-manager] release [1.30.5-1].
    Created by command:
    
    /usr/bin/tito tag

--- setup.py
+++ setup.py
@@ -263,7 +263,7 @@ cmdclass = {
 
 setup(
     name="subscription-manager",
-    version="1.30.4",
+    version="1.30.5",
     url="http://www.candlepinproject.org",
     description="Manage subscriptions for Red Hat products.",
     license="GPLv2",
