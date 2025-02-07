f91fa2d7df5c92b7318896a6e0835f7235f0192c
Pino Toscano <ptoscano@redhat.com>
2025-02-07 12:33:22 +0100

    tests: move StubIdentity to stubs
    As it may be useful also in other tests outside the test_identitycertlib
    module, then
    - move it together with the other stubs
    - adapt test_identitycertlib to it
    
    This is a simple refactoring with no behaviour changes.

--- test/stubs.py
+++ test/stubs.py
@@ -38,6 +38,7 @@ from subscription_manager.facts import Facts
 from rhsm.certificate import GMT
 from rhsm.certificate2 import Version
 from subscription_manager.certdirectory import EntitlementDirectory, ProductDirectory
+from subscription_manager.identity import Identity
 
 from rhsm.certificate import parse_tags
 from rhsm.certificate2 import EntitlementCertificate, ProductCertificate, Product, Content, Order
@@ -461,6 +462,13 @@ class StubConsumerIdentity:
     @classmethod
     def keypath(cls):
         return ""
+
+
+class StubIdentity(Identity):
+    _consumer = None
+
+    def _get_consumer_identity(self):
+        return self._consumer
 
 
 class StubUEP:
