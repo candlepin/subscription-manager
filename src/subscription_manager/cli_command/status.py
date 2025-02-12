45e5f9a8e368a9bb8fa8f5a4b6c530cdabb31e82
Pino Toscano <ptoscano@redhat.com>
2025-02-12 17:29:04 +0100

    feat: drop non-SCA support in status command
    subscription-manager supports registering only to SCA organizations;
    because of this, it is possible to greatly simplify the "status"
    command; in particular:
    - drop any attempt to determine the content access mode, potentially
      refreshing caches; since all it matters is whether the system is
      registered, there is no need for that
    - drop any handling of the syspurpose compliance status, and compliance
      in general, as they are specific to the entitlement mode
    - drop the additional message saying that the system is in SCA mode and
      content is available, as it is what happens already now
    
    On the actual implementation:
    - fold the printing of the "registered" status inline in _do_command()
    - drop all the tests related to syspurpose, and entitlement mode
    - rename the leftover test for a registered system dropping the
      references to SCA
    - drop all the patching in tests that is no more needed now
    - mark the command as not requiring network connection, as it does not
      do network communication anymore
    - update the help text of "status" to mention only the system
    - simplify the man page text for "status"
    - drop the network options from the bash completion
    
    The handling of the --ondate option is left for compatibility with
    corner-case tests that check for it; it might be cleaned up at a later
    step.
    
    Related to: CCT-848

--- src/subscription_manager/cli_command/status.py
+++ src/subscription_manager/cli_command/status.py
@@ -16,20 +16,12 @@
 #
 import logging
 import os
-import subscription_manager.injection as inj
-
-from rhsm.certificate2 import CONTENT_ACCESS_CERT_TYPE
-from rhsm.connection import ConnectionException
 
 from rhsmlib.services import entitlement
-from rhsmlib.services.refresh import Refresh
 
-from subscription_manager import syspurposelib
 from subscription_manager.cli import system_exit
 from subscription_manager.cli_command.cli import CliCommand
 from subscription_manager.i18n import ugettext as _
-from subscription_manager.printing_utils import format_name
-from subscription_manager.utils import is_simple_content_access, get_terminal_width
 from time import localtime, strftime
 
 log = logging.getLogger(__name__)
@@ -37,7 +29,7 @@ log = logging.getLogger(__name__)
 
 class StatusCommand(CliCommand):
     def __init__(self):
-        shortdesc = _("Show status information for this system's subscriptions and products")
+        shortdesc = _("Show status information for this system")
         super(StatusCommand, self).__init__("status", shortdesc, True)
         self.parser.add_argument(
             "--ondate",
@@ -46,6 +38,9 @@ class StatusCommand(CliCommand):
                 example=strftime("%Y-%m-%d", localtime())
             ),
         )
+
+    def require_connection(self):
+        return False
 
     def _get_date_cli_option(self):
         """
@@ -65,133 +60,21 @@ class StatusCommand(CliCommand):
         print("   " + _("System Status Details"))
         print("+-------------------------------------------+")
 
-    def _determine_whether_content_access_mode_is_sca(self, service_status):
-        """
-        Try hard to determine whether the content access mode is SCA,
-        refreshing the caches if needed
-        :return: True if SCA, False if entitlement mode
-        """
-
-        certs = self.entitlement_dir.list_with_content_access()
-        sca_certs = [cert for cert in certs if cert.entitlement_type == CONTENT_ACCESS_CERT_TYPE]
-        sca_mode_detected = False
-
-        refresh_service = Refresh(cp=self.cp, ent_cert_lib=self.entcertlib)
-
-        if sca_certs:
-            sca_mode_detected = True
-        else:
-            # When there are no entitlement SCA certificates, but status_id is "disabled", then
-            # it means that content access mode has changed on the server and entitlement certificates
-            # have to be refreshed
-            if service_status["status_id"] == "disabled":
-                refresh_service.refresh()
-            if is_simple_content_access(uep=self.cp, identity=self.identity):
-                sca_mode_detected = True
-
-        if sca_mode_detected is True:
-            # When SCA mode was detected using cache or installed SCA entitlement certificates, but status_id
-            # is not "disabled", then it means that content access mode has changed on the server and
-            # entitlement certificates have to be refreshed
-            status_id = service_status["status_id"]
-            if status_id != "disabled":
-                log.debug(
-                    f"Found SCA cert, but status ID is not 'disabled' ({status_id}). "
-                    "Refreshing entitlement certs..."
-                )
-                refresh_service.refresh()
-                sca_mode_detected = False
-
-        return sca_mode_detected
-
-    def _print_status(self, service_status, is_sca):
-        """
-        Print only status
-        :return: Print overall status
-        """
-
-        self._print_status_banner()
-
-        ca_message = ""
-        status_message = service_status["status"]
-
-        if is_sca:
-            ca_message = _(
-                "Content Access Mode is set to Simple Content Access. "
-                "This host has access to content, regardless of subscription status.\n"
-            )
-            status_message = _("Registered")
-
-        print(_("Overall Status: {status}\n{message}").format(status=status_message, message=ca_message))
-
-    def _print_reasons(self, service_status):
-        """
-        Print reasons for overall status
-        :param service_status:
-        :return: None
-        """
-        reasons = service_status["reasons"]
-
-        columns = get_terminal_width()
-        for name in reasons:
-            print(format_name(name + ":", 0, columns))
-            for message in reasons[name]:
-                print("- {name}".format(name=format_name(message, 2, columns)))
-            print("")
-
-    def _print_syspurpose_status(self, on_date):
-        """
-        Print syspurpose status
-        :return: None
-        """
-        try:
-            store = syspurposelib.get_sys_purpose_store()
-            if store:
-                store.sync()
-        except (OSError, ConnectionException) as ne:
-            log.exception(ne)
-
-        syspurpose_cache = inj.require(inj.SYSTEMPURPOSE_COMPLIANCE_STATUS_CACHE)
-        syspurpose_cache.load_status(self.cp, self.identity.uuid, on_date)
-        print(_("System Purpose Status: {status}").format(status=syspurpose_cache.get_overall_status()))
-
-        syspurpose_status_code = syspurpose_cache.get_overall_status_code()
-        if syspurpose_status_code != "matched":
-            reasons = syspurpose_cache.get_status_reasons()
-            if reasons is not None:
-                for reason in reasons:
-                    print("- {reason}".format(reason=reason))
-        print("")
-
     def _do_command(self):
         """
         Print status and all reasons it is not valid
         """
 
         # First get/check if provided date is valid
-        on_date = self._get_date_cli_option()
+        self._get_date_cli_option()
+
+        self._print_status_banner()
 
         # In case we are not registered, then simply print that and avoid
         # all the rest of the checks
         if not self.is_consumer_cert_present():
-            self._print_status_banner()
             print(_("Overall Status: Not registered\n"))
             return 1
 
-        service_status = entitlement.EntitlementService(cp=self.cp).get_status(on_date)
-
-        is_sca = self._determine_whether_content_access_mode_is_sca(service_status)
-
-        self._print_status(service_status, is_sca)
-
-        if not is_sca:
-            self._print_reasons(service_status)
-
-            self._print_syspurpose_status(on_date)
-
-        if service_status["valid"]:
-            result = 0
-        else:
-            result = 1
-
-        return result
+        print(_("Overall Status: Registered\n"))
+        return 0
--- test/cli_command/test_status.py
+++ test/cli_command/test_status.py
@@ -1,29 +1,11 @@
 from subscription_manager import managercli
-from rhsm.certificate2 import CONTENT_ACCESS_CERT_TYPE
 
-from ..stubs import StubUEP, StubIdentity
+from ..stubs import StubIdentity
 from ..fixture import SubManFixture, Capture
 
-from unittest.mock import Mock, patch
+from unittest.mock import Mock
 
 from subscription_manager import injection as inj
-
-
-MOCK_SERVICE_STATUS_SCA = {
-    "status": "Disabled",
-    "status_id": "disabled",
-    "reasons": {},
-    "reason_ids": {},
-    "valid": True,
-}
-
-MOCK_SERVICE_STATUS_ENTITLEMENT = {
-    "status": "Current",
-    "status_id": "valid",
-    "reasons": {},
-    "reason_ids": {},
-    "valid": True,
-}
 
 
 class TestStatusCommand(SubManFixture):
@@ -32,79 +14,16 @@ class TestStatusCommand(SubManFixture):
     def setUp(self):
         super(TestStatusCommand, self).setUp()
         self.cc = self.command_class()
-        patcher = patch("subscription_manager.cli_command.status.entitlement")
-        self.entitlement_mock = patcher.start()
-        self.addCleanup(patcher.stop)
-        self.mock_entitlement_instance = Mock()
-        self.mock_entitlement_instance.get_status = Mock(return_value=MOCK_SERVICE_STATUS_SCA)
-        self.entitlement_mock.EntitlementService = Mock(return_value=self.mock_entitlement_instance)
 
-    def test_status_sca_mode_registered(self):
+    def test_status_registered(self):
         """
-        Test status, when SCA mode is used
+        Test status, when the system is registered
         """
-        self.cc.cp = StubUEP()
-        self.cc.cp.setSyspurposeCompliance({"status": "disabled"})
-        self.cc.cp._capabilities = ["syspurpose"]
         self.cc.options = Mock()
         self.cc.options.on_date = None
-        mock_cert = Mock()
-        mock_cert.entitlement_type = CONTENT_ACCESS_CERT_TYPE
-        cert_list = [mock_cert]
-        self.cc.entitlement_dir = Mock()
-        self.cc.entitlement_dir.list_with_content_access = Mock(return_value=cert_list)
-        self.cc.entcertlib = Mock()
         with Capture() as cap:
             self.cc._do_command()
         self.assertIn("Overall Status: Registered", cap.out)
-        self.assertIn("Content Access Mode is set to Simple Content Access.", cap.out)
-
-    def test_current_status_entitlement_mode(self):
-        """
-        Test status, when old entitlement mode is used
-        """
-        # Note that server sent response with "Current" status
-        self.mock_entitlement_instance.get_status = Mock(return_value=MOCK_SERVICE_STATUS_ENTITLEMENT)
-        self.cc.cp = StubUEP()
-        self.cc.cp.setSyspurposeCompliance({"status": "valid"})
-        self.cc.cp._capabilities = ["syspurpose"]
-        self.cc.options = Mock()
-        self.cc.options.on_date = None
-        # There is not SCA certificate (only old entitlement)
-        mock_cert = Mock()
-        mock_cert.entitlement_type = "entitlement"
-        cert_list = [mock_cert]
-        self.cc.entitlement_dir = Mock()
-        self.cc.entitlement_dir.list_with_content_access = Mock(return_value=cert_list)
-        self.cc.entcertlib = Mock()
-        with Capture() as cap:
-            self.cc._do_command()
-        self.assertIn("Overall Status: Current", cap.out)
-        self.assertIn("System Purpose Status: Matched", cap.out)
-
-    def test_status_content_access_mode_changed(self):
-        """
-        Test status, when old entitlement mode is used
-        """
-        # Note that server sent response with "Current" status
-        self.mock_entitlement_instance.get_status = Mock(return_value=MOCK_SERVICE_STATUS_ENTITLEMENT)
-        self.cc.cp = StubUEP()
-        self.cc.cp.setSyspurposeCompliance({"status": "valid"})
-        self.cc.cp._capabilities = ["syspurpose"]
-        self.cc.options = Mock()
-        self.cc.options.on_date = None
-        # But entitlement directory still contain SCA certificate
-        mock_cert = Mock()
-        mock_cert.entitlement_type = CONTENT_ACCESS_CERT_TYPE
-        cert_list = [mock_cert]
-        self.cc.entitlement_dir = Mock()
-        self.cc.entitlement_dir.list_with_content_access = Mock(return_value=cert_list)
-        self.cc.entcertlib = Mock()
-        # sub-man should be able to resurrect from this situation
-        with Capture() as cap:
-            self.cc._do_command()
-        self.assertIn("Overall Status: Current", cap.out)
-        self.assertIn("System Purpose Status: Matched", cap.out)
 
     def test_status_unregistered(self):
         """
@@ -116,54 +35,3 @@ class TestStatusCommand(SubManFixture):
         with Capture() as cap:
             self.cc._do_command()
         self.assertIn("Overall Status: Not registered", cap.out)
-
-    def test_purpose_status_success(self):
-        self.cc.cp = StubUEP()
-        self.cc.cp.setSyspurposeCompliance({"status": "valid"})
-        self.cc.cp._capabilities = ["syspurpose"]
-        self.cc.options = Mock()
-        self.cc.options.on_date = None
-        self.cc.entcertlib = Mock()
-        self.cc._determine_whether_content_access_mode_is_sca = Mock(return_value=False)
-        with Capture() as cap:
-            self.cc._do_command()
-        self.assertTrue("System Purpose Status: Matched" in cap.out)
-
-    def test_purpose_status_consumer_lack(self):
-        self.cc.cp = StubUEP()
-        self.cc.cp.setSyspurposeCompliance({"status": "unknown"})
-        self.cc.cp._capabilities = ["syspurpose"]
-        self.cc.options = Mock()
-        self.cc.options.on_date = None
-        self.cc.entcertlib = Mock()
-        self.cc._determine_whether_content_access_mode_is_sca = Mock(return_value=False)
-        with Capture() as cap:
-            self.cc._do_command()
-        self.assertTrue("System Purpose Status: Unknown" in cap.out)
-
-    def test_purpose_status_consumer_no_capability(self):
-        self.cc.cp = StubUEP()
-        self.cc.cp.setSyspurposeCompliance({"status": "unknown"})
-        self.cc.cp._capabilities = []
-        self.cc.options = Mock()
-        self.cc.options.on_date = None
-        self.cc.entcertlib = Mock()
-        self.cc._determine_whether_content_access_mode_is_sca = Mock(return_value=False)
-        with Capture() as cap:
-            self.cc._do_command()
-        self.assertTrue("System Purpose Status: Unknown" in cap.out)
-
-    def test_purpose_status_mismatch(self):
-        self.cc.cp = StubUEP()
-        self.cc.cp.setSyspurposeCompliance(
-            {"status": "mismatched", "reasons": ["unsatisfied usage: Production"]}
-        )
-        self.cc.cp._capabilities = ["syspurpose"]
-        self.cc.options = Mock()
-        self.cc.options.on_date = None
-        self.cc.entcertlib = Mock()
-        self.cc._determine_whether_content_access_mode_is_sca = Mock(return_value=False)
-        with Capture() as cap:
-            self.cc._do_command()
-        self.assertTrue("System Purpose Status: Mismatched" in cap.out)
-        self.assertTrue("unsatisfied usage: Production" in cap.out)
