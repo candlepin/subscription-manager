candlepin/subscription-manager

commit f445b6486a962d12185a5afe69e768d0a605e175

Move python-rhsm build into subscription-manager
Most invasive changes were:
 - Moved most of the python files to sitearch (/usr/lib64 instead of
   /usr/lib), since that is where setuptools installs them due to
   building the certificate extension in python-rhsm
 - Changes to get setup.py to be python3 compatible
 - Excluded some Python 3 artifacts in the spec file for now, since we
   are not yet building subscription-manager for Python 3.
 - Moved tests for python-rhsm from `python-rhsm/test/` to `test/rhsm`.
 - Removed the python-rhsm README.
 - Only build Python 3 packages in Fedora
 - Rename python-rhsm to subscription-manager-rhsm

This also meant python3-python-rhsm becomes
python3-subscription-manager-rhsm... and python-rhsm-certificates
becomes subscription-manager-rhsm-certificates.

This required some changes to our jobs in order to work correctly.

Makefile
@@ -13,6 +13,7 @@
 #   just the fastest or easiest way.
 
 SHELL := /bin/bash
+PYTHON := python
 PREFIX ?= /
 SYSCONF ?= etc
 INSTALL_DIR = usr/share
@@ -21,7 +22,7 @@ OS = $(shell lsb_release -i | awk '{ print $$3 }' | awk -F. '{ print $$1}')
 OS_VERSION = $(shell lsb_release -r | awk '{ print $$2 }' | awk -F. '{ print $$1}')
 OS_DIST ?= $(shell rpm --eval='%dist')
 
-PYTHON_VER ?= $(shell python -c 'import sys; print("python%s.%s" % sys.version_info[:2])')
+PYTHON_VER ?= $(shell $(PYTHON) -c 'import sys; print("python%s.%s" % sys.version_info[:2])')
 
 PYTHON_SITELIB ?= usr/lib/$(PYTHON_VER)/site-packages
 # Note the underscore used instead of a hyphen
@@ -104,8 +105,8 @@ STYLEFILES=$(PYFILES) $(BIN_FILES)
 build: rhsmcertd rhsm-icon
 # Install doesn't perform a build if it doesn't have too.  Best to clean out
 # any cruft so developers don't end up install old builds.
-	./setup.py clean --all
-	./setup.py build --quiet --gtk-version=$(GTK_VERSION) --rpm-version=$(VERSION)
+	$(PYTHON) ./setup.py clean --all
+	$(PYTHON) ./setup.py build --quiet --gtk-version=$(GTK_VERSION) --rpm-version=$(VERSION)
 
 # we never "remake" this makefile, so add a target so
 # we stop searching for implicit rules on how to remake it
@@ -116,7 +117,8 @@ clean:
 	rm -f *.pyc *.pyo *~ *.bak *.tar.gz
 	rm -f bin/rhsmcertd
 	rm -f bin/rhsm-icon
-	./setup.py clean --all
+	$(PYTHON) ./setup.py clean --all
+	rm -rf cover/ htmlcov/ docs/sphinx/_build/ build/ dist/
 
 rhsmcertd: $(DAEMONS_SRC_DIR)/rhsmcertd.c
 	$(CC) $(CFLAGS) $(RHSMCERTD_CFLAGS) -DLIBEXECDIR='"$(LIBEXEC_DIR)"' $(DAEMONS_SRC_DIR)/rhsmcertd.c -o bin/rhsmcertd $(LDFLAGS) $(RHSMCERTD_LDFLAGS)
@@ -274,7 +276,7 @@ install-post-boot: install-firstboot install-initial-setup
 
 .PHONY: install-via-setup
 install-via-setup:
-	./setup.py install --root $(PREFIX) --gtk-version=$(GTK_VERSION) --rpm-version=$(VERSION) --with-systemd=$(WITH_SYSTEMD) --prefix=/usr
+	$(PYTHON) ./setup.py install --root $(PREFIX) --gtk-version=$(GTK_VERSION) --rpm-version=$(VERSION) --with-systemd=$(WITH_SYSTEMD) --prefix=/usr
 
 .PHONY: install
 install: install-via-setup install-files
@@ -335,37 +337,48 @@ install-files: dbus-install install-conf install-plugins install-post-boot insta
 
 .PHONY: check
 check:
-	python setup.py -q nosetests -c playpen/noserc.dev
+	$(PYTHON) setup.py -q nosetests -c playpen/noserc.dev
+
+.PHONY: version_check
+version_check:
+# needs https://github.com/alikins/pyqver
+	-@TMPFILE=`mktemp` || exit 1; \
+	pyqver2.py -v -m 2.5  $(STYLEFILES) | tee $$TMPFILE; \
+	! test -s $$TMPFILE
 
 .PHONY: coverage
 coverage:
 ifdef ghprbPullId
 	# Pull the PR id from the Jenkins environment and use it as a seed so that each PR
 	# uses a consistant test ordering.
-	./setup.py -q nosetests --randomly-seed=$(ghprbPullId) -c playpen/noserc.ci
+	$(PYTHON) ./setup.py -q nosetests --randomly-seed=$(ghprbPullId) -c playpen/noserc.ci
 else
-	./setup.py -q nosetests -c playpen/noserc.ci
+	$(PYTHON) ./setup.py -q nosetests -c playpen/noserc.ci
 endif
+
+.PHONY: docs
+docs:
+	$(PYTHON) setup.py build_sphinx
 
 .PHONY: gettext
 gettext:
 	# Extract strings from our source files. any comments on the line above
 	# the string marked for translation beginning with "translators" will be
 	# included in the pot file.
-	./setup.py gettext
+	$(PYTHON) ./setup.py gettext
 
 .PHONY: update-po
 update-po:
-	./setup.py update_trans
+	$(PYTHON) ./setup.py update_trans
 
 .PHONY: uniq-po
 uniq-po:
-	./setup.py uniq_trans
+	$(PYTHON) ./setup.py uniq_trans
 
 # just run a check to make sure these compile
 .PHONY: polint
 polint:
-	./setup.py gettext --lint
+	$(PYTHON) ./setup.py gettext --lint
 
 .PHONY: just-strings
 just-strings:
@@ -397,22 +410,22 @@ gen-test-long-po:
 
 .PHONY: lint
 lint:
-	./setup.py lint
+	$(PYTHON) ./setup.py lint
 
 .PHONY: flake8
 flake8:
-	./setup.py flake8
+	$(PYTHON) ./setup.py flake8
 
 .PHONY: rpmlint
 rpmlint:
-	./setup.py lint_rpm
+	$(PYTHON) ./setup.py lint_rpm
 
 .PHONY: stylish
 stylish: lint
 
 .PHONY: install-pip-requirements
 install-pip-requirements:
-	@pip install -r test-requirements.txt
+	@pip install -I -r test-requirements.txt
 
 .PHONY: jenkins
 jenkins: install-pip-requirements build stylish coverage

subscription-manager.spec
@@ -7,7 +7,15 @@
 %global use_firstboot 0
 %global use_kitchen 1
 %global use_inotify 1
+%global use_python3 0%{?fedora}
 %global rhsm_plugins_dir  /usr/share/rhsm-plugins
+# on recent Fedora and RHEL 7, let's not use m2crypto
+%global use_m2crypto (0%{?fedora} < 23 && 0%{?rhel} < 7)
+
+# SLES 11 and RHEL6 don't have macros defined for python2; just "python".  Alias them.
+%{!?__python2:%global __python2 %__python}
+%{!?python2_sitearch:%global python2_sitearch %python_sitearch}
+%{?python_provide:%python_provide python-rhsm}
 
 %if %{use_systemd}
 # Note that %gtk3 will be undefined if it's not used
@@ -105,7 +113,7 @@ Requires:  python-ethtool
 Requires:  python-iniparse
 Requires:  python-decorator
 Requires:  virt-what
-Requires:  python-rhsm >= 1.20.2
+Requires:  subscription-manager-rhsm >= %{version}
 Requires:  python-decorator
 Requires:  python-six
 Requires:  python-dateutil
@@ -167,6 +175,11 @@ BuildRequires: systemd-rpm-macros
 # We need the systemd RPM macros
 BuildRequires: systemd
 %endif
+
+# The %{?something:foo} expands to foo only when something is **defined**.  Likewise the
+# %{!?something:foo} expands only when something is **not defined**.
+BuildRequires: %{?suse_version:python-devel >= 2.6} %{!?suse_version:python2-devel}
+BuildRequires: openssl-devel
 
 %description
 The Subscription Manager package provides programs and libraries to allow users
@@ -296,6 +309,66 @@ the remote in the currently deployed .origin file.
 %endif
 
 
+%package -n subscription-manager-rhsm
+Summary: A Python library to communicate with a Red Hat Unified Entitlement Platform
+Group: Development/Libraries
+
+%if %use_m2crypto
+Requires: %{?suse_version:python-m2crypto} %{!?suse_version:m2crypto}
+%endif
+Requires: python-dateutil
+Requires: python-iniparse
+# rpm-python is an old name for python2-rpm but RHEL6 uses the old name
+Requires: rpm-python
+Requires: python-six
+Requires: subscription-manager-rhsm-certificates = %{version}-%{release}
+Provides: python-rhsm = %{version}-%{release}
+Obsoletes: python-rhsm <= 1.20.2-1
+
+%description -n subscription-manager-rhsm
+A small library for communicating with the REST interface of a Red Hat Unified
+Entitlement Platform. This interface is used for the management of system
+entitlements, certificates, and access to content.
+
+
+%if %{use_python3}
+%package -n python3-subscription-manager-rhsm
+Summary: A Python library to communicate with a Red Hat Unified Entitlement Platform
+BuildRequires: python3-devel
+BuildRequires: python3-setuptools
+BuildRequires: python3-six
+
+Requires: python3-dateutil
+Requires: python3-iniparse
+Requires: python3-rpm
+Requires: python3-six
+# M2Crypto isn't even used in new Fedoras and RHEL 8
+Requires: subscription-manager-rhsm-certificates = %{version}-%{release}
+Provides: python3-python-rhsm = %{version}-%{release}
+Obsoletes: python3-python-rhsm <= 1.20.2-1
+
+# Required by Fedora packaging guidelines
+%{?python_provide:%python_provide python3-python-rhsm}
+
+%description -n python3-subscription-manager-rhsm
+A small library for communicating with the REST interface of a Red Hat Unified
+Entitlement Platform. This interface is used for the management of system
+entitlements, certificates, and access to content.
+%endif
+
+
+%package -n subscription-manager-rhsm-certificates
+Summary: Certificates required to communicate with a Red Hat Unified Entitlement Platform
+Group: Development/Libraries
+Provides: python-rhsm-certificates = %{version}-%{release}
+Obsoletes: python-rhsm-certificates <= 1.20.2-1
+
+%description -n subscription-manager-rhsm-certificates
+This package contains certificates required for communicating with the REST interface
+of a Red Hat Unified Entitlement Platform, used for the management of system entitlements
+and to receive access to content.
+
+
 %prep
 %setup -q
 
@@ -306,12 +379,23 @@ make -f Makefile VERSION=%{version}-%{release} CFLAGS="%{optflags}" \
 %install
 rm -rf %{buildroot}
 make -f Makefile install VERSION=%{version}-%{release} \
-    PREFIX=%{buildroot} PYTHON_SITELIB=%{python_sitelib} \
+    PYTHON=/usr/bin/python2 \
+    PREFIX=%{buildroot} PYTHON_SITELIB=%{python_sitearch} \
     OS_VERSION=%{?fedora}%{?rhel}%{?suse_version} OS_DIST=%{dist} \
     %{?install_ostree} %{?post_boot_tool} %{?gtk_version} \
     %{?install_yum_plugins} %{?install_dnf_plugins} \
     %{?install_zypper_plugins} \
     %{?with_systemd}
+%if %{use_python3}
+make -f Makefile install VERSION=%{version}-%{release} \
+    PYTHON=/usr/bin/python3 \
+    PREFIX=%{buildroot} PYTHON_SITELIB=%{python3_sitearch} \
+    OS_VERSION=%{?fedora}%{?rhel}%{?suse_version} OS_DIST=%{dist} \
+    %{?install_ostree} %{?post_boot_tool} %{?gtk_version} \
+    %{?install_yum_plugins} %{?install_dnf_plugins} \
+    %{?install_zypper_plugins} \
+    %{?with_systemd}
+%endif
 
 %if 0%{?suse_version}
 %suse_update_desktop_file -n -r subscription-manager-gui Settings PackageManager
@@ -343,6 +427,7 @@ install -m 644 %{_builddir}/%{buildsubdir}/etc-conf/redhat-entitlement-authority
 
 %{__mkdir_p} %{buildroot}%{_sysconfdir}/etc/rhsm/ca
 install -m 644 %{_builddir}/%{buildsubdir}/etc-conf/redhat-entitlement-authority.pem %{buildroot}/%{_sysconfdir}/rhsm/ca/redhat-entitlement-authority.pem
+install -m 644 %{_builddir}/%{buildsubdir}/etc-conf/ca/redhat-uep.pem %{buildroot}/%{_sysconfdir}/rhsm/ca/redhat-uep.pem
 
 %clean
 rm -rf %{buildroot}
@@ -365,18 +450,21 @@ rm -rf %{buildroot}
     %dir %{_sysconfdir}/yum.repos.d
 %endif
 
-%dir %{python_sitelib}/rhsmlib/candlepin
-%dir %{python_sitelib}/rhsmlib/compat
-%dir %{python_sitelib}/rhsmlib/dbus
-%dir %{python_sitelib}/rhsmlib/dbus/facts
-%dir %{python_sitelib}/rhsmlib/dbus/objects
-%dir %{python_sitelib}/rhsmlib/facts
-%dir %{python_sitelib}/rhsmlib/services
-%dir %{python_sitelib}/subscription_manager-%{version}-*.egg-info
-%dir %{python_sitelib}/subscription_manager/api
-%dir %{python_sitelib}/subscription_manager/branding
-%dir %{python_sitelib}/subscription_manager/model
-%dir %{python_sitelib}/subscription_manager/plugin
+%dir %{python_sitearch}/rhsmlib/candlepin
+%dir %{python_sitearch}/rhsmlib/compat
+%dir %{python_sitearch}/rhsmlib/dbus
+%dir %{python_sitearch}/rhsmlib/dbus/facts
+%dir %{python_sitearch}/rhsmlib/dbus/objects
+%dir %{python_sitearch}/rhsmlib/facts
+%dir %{python_sitearch}/rhsmlib/services
+%dir %{python_sitearch}/subscription_manager-%{version}-*.egg-info
+%dir %{python_sitearch}/subscription_manager/api
+%dir %{python_sitearch}/subscription_manager/branding
+%dir %{python_sitearch}/subscription_manager/model
+%dir %{python_sitearch}/subscription_manager/plugin
+%if %{use_python3}
+%exclude %{python3_sitearch}/rhsmlib
+%endif
 %dir %{_var}/spool/rhsm
 %dir %{_prefix}/share/polkit-1
 %dir %{_prefix}/share/polkit-1/actions
@@ -446,25 +534,29 @@ rm -rf %{buildroot}
 %{_sysconfdir}/bash_completion.d/rhsm-icon
 %{_sysconfdir}/bash_completion.d/rhsmcertd
 
-%dir %{python_sitelib}/subscription_manager
+%dir %{python_sitearch}/subscription_manager
 
 # code, python modules and packages
-%{python_sitelib}/subscription_manager-*.egg-info/*
-%{python_sitelib}/subscription_manager/*.py*
-%{python_sitelib}/subscription_manager/api/*.py*
-%{python_sitelib}/subscription_manager/branding/*.py*
-%{python_sitelib}/subscription_manager/model/*.py*
-%{python_sitelib}/subscription_manager/plugin/__init__.py*
+%{python_sitearch}/subscription_manager-*.egg-info/*
+%{python_sitearch}/subscription_manager/*.py*
+%{python_sitearch}/subscription_manager/api/*.py*
+%{python_sitearch}/subscription_manager/branding/*.py*
+%{python_sitearch}/subscription_manager/model/*.py*
+%{python_sitearch}/subscription_manager/plugin/__init__.py*
+%if %{use_python3}
+%exclude %{python3_sitearch}/subscription_manager-*.egg-info/*
+%exclude %{python3_sitearch}/subscription_manager
+%endif
 
 # our gtk2/gtk3 compat modules
-%dir %{python_sitelib}/subscription_manager/ga_impls
-%{python_sitelib}/subscription_manager/ga_impls/__init__.py*
+%dir %{python_sitearch}/subscription_manager/ga_impls
+%{python_sitearch}/subscription_manager/ga_impls/__init__.py*
 
 %if 0%{?gtk3}
-%{python_sitelib}/subscription_manager/ga_impls/ga_gtk3.py*
+%{python_sitearch}/subscription_manager/ga_impls/ga_gtk3.py*
 %else
-%dir %{python_sitelib}/subscription_manager/ga_impls/ga_gtk2
-%{python_sitelib}/subscription_manager/ga_impls/ga_gtk2/*.py*
+%dir %{python_sitearch}/subscription_manager/ga_impls/ga_gtk2
+%{python_sitearch}/subscription_manager/ga_impls/ga_gtk2/*.py*
 %endif
 
 # subscription-manager plugins
@@ -487,15 +579,18 @@ rm -rf %{buildroot}
 %endif
 
 # rhsmlib
-%dir %{python_sitelib}/rhsmlib
-%{python_sitelib}/rhsmlib/*.py*
-%{python_sitelib}/rhsmlib/candlepin/*.py*
-%{python_sitelib}/rhsmlib/compat/*.py*
-%{python_sitelib}/rhsmlib/facts/*.py*
-%{python_sitelib}/rhsmlib/services/*.py*
-%{python_sitelib}/rhsmlib/dbus/*.py*
-%{python_sitelib}/rhsmlib/dbus/facts/*.py*
-%{python_sitelib}/rhsmlib/dbus/objects/*.py*
+%dir %{python_sitearch}/rhsmlib
+%{python_sitearch}/rhsmlib/*.py*
+%{python_sitearch}/rhsmlib/candlepin/*.py*
+%{python_sitearch}/rhsmlib/compat/*.py*
+%{python_sitearch}/rhsmlib/facts/*.py*
+%{python_sitearch}/rhsmlib/services/*.py*
+%{python_sitearch}/rhsmlib/dbus/*.py*
+%{python_sitearch}/rhsmlib/dbus/facts/*.py*
+%{python_sitearch}/rhsmlib/dbus/objects/*.py*
+%if %{use_python3}
+%exclude %{python3_sitearch}/rhsmlib
+%endif
 
 %{_datadir}/polkit-1/actions/com.redhat.*.policy
 %{_datadir}/dbus-1/system-services/com.redhat.*.service
@@ -511,13 +606,19 @@ rm -rf %{buildroot}
 %endif
 
 # Incude rt CLI tool
-%dir %{python_sitelib}/rct
-%{python_sitelib}/rct/*.py*
+%dir %{python_sitearch}/rct
+%{python_sitearch}/rct/*.py*
+%if %{use_python3}
+%exclude %{python3_sitearch}/rct
+%endif
 %attr(755,root,root) %{_bindir}/rct
 
 # Include consumer debug CLI tool
-%dir %{python_sitelib}/rhsm_debug
-%{python_sitelib}/rhsm_debug/*.py*
+%dir %{python_sitearch}/rhsm_debug
+%{python_sitearch}/rhsm_debug/*.py*
+%if %{use_python3}
+%exclude %{python3_sitearch}/rhsm_debug
+%endif
 %attr(755,root,root) %{_bindir}/rhsm-debug
 
 %doc
@@ -533,10 +634,10 @@ rm -rf %{buildroot}
 %defattr(-,root,root,-)
 %attr(755,root,root) %{_sbindir}/subscription-manager-gui
 %if 0%{?suse_version}
-%dir %{python_sitelib}/subscription_manager/gui/data
-%dir %{python_sitelib}/subscription_manager/gui/data/glade
-%dir %{python_sitelib}/subscription_manager/gui/data/icons
-%dir %{python_sitelib}/subscription_manager/gui/data/ui
+%dir %{python_sitearch}/subscription_manager/gui/data
+%dir %{python_sitearch}/subscription_manager/gui/data/glade
+%dir %{python_sitearch}/subscription_manager/gui/data/icons
+%dir %{python_sitearch}/subscription_manager/gui/data/ui
 %dir %{_datadir}/appdata
 %dir %{_datadir}/gnome
 %dir %{_datadir}/gnome/help
@@ -547,11 +648,11 @@ rm -rf %{buildroot}
 %endif
 %{_bindir}/rhsm-icon
 
-%dir %{python_sitelib}/subscription_manager/gui
-%{python_sitelib}/subscription_manager/gui/*.py*
-%{python_sitelib}/subscription_manager/gui/data/ui/*.ui
-%{python_sitelib}/subscription_manager/gui/data/glade/*.glade
-%{python_sitelib}/subscription_manager/gui/data/icons/*.svg
+%dir %{python_sitearch}/subscription_manager/gui
+%{python_sitearch}/subscription_manager/gui/*.py*
+%{python_sitearch}/subscription_manager/gui/data/ui/*.ui
+%{python_sitearch}/subscription_manager/gui/data/glade/*.glade
+%{python_sitearch}/subscription_manager/gui/data/icons/*.svg
 
 %{_datadir}/applications/subscription-manager-gui.desktop
 %{_datadir}/icons/hicolor/16x16/apps/*.png
@@ -580,8 +681,8 @@ rm -rf %{buildroot}
 
 %files -n subscription-manager-migration
 %defattr(-,root,root,-)
-%dir %{python_sitelib}/subscription_manager/migrate
-%{python_sitelib}/subscription_manager/migrate/*.py*
+%dir %{python_sitearch}/subscription_manager/migrate
+%{python_sitearch}/subscription_manager/migrate/*.py*
 %attr(755,root,root) %{_sbindir}/rhn-migrate-classic-to-rhsm
 
 %doc
@@ -598,11 +699,12 @@ rm -rf %{buildroot}
 %dir %{_sysconfdir}/docker
 %dir %{_sysconfdir}/docker/certs.d
 %dir %{_sysconfdir}/rhsm/ca
-%dir %{python_sitelib}/subscription_manager/plugin
+%dir %{python_sitearch}/subscription_manager/plugin
 %endif
 %{_sysconfdir}/rhsm/pluginconf.d/container_content.ContainerContentPlugin.conf
 %{rhsm_plugins_dir}/container_content.py*
-%{python_sitelib}/subscription_manager/plugin/container.py*
+%{python_sitearch}/subscription_manager/plugin/container.py*
+
 # Copying Red Hat CA cert into each directory:
 %attr(755,root,root) %dir %{_sysconfdir}/docker/certs.d/cdn.redhat.com
 %attr(644,root,root) %{_sysconfdir}/rhsm/ca/redhat-entitlement-authority.pem
@@ -614,7 +716,7 @@ rm -rf %{buildroot}
 %defattr(-,root,root,-)
 %{_sysconfdir}/rhsm/pluginconf.d/ostree_content.OstreeContentPlugin.conf
 %{rhsm_plugins_dir}/ostree_content.py*
-%{python_sitelib}/subscription_manager/plugin/ostree/*.py*
+%{python_sitearch}/subscription_manager/plugin/ostree/*.py*
 %endif
 
 
@@ -646,8 +748,30 @@ rm -rf %{buildroot}
 %if %use_dnf
 %files -n dnf-plugin-subscription-manager
 %defattr(-,root,root,-)
-%{python_sitelib}/dnf-plugins/*
+%{python_sitearch}/dnf-plugins/*
+%if %{use_python3}
+%exclude %{python3_sitearch}/dnf-plugins/*
 %endif
+%endif
+
+
+%files -n subscription-manager-rhsm
+%defattr(-,root,root,-)
+%dir %{python2_sitearch}/rhsm
+%{python2_sitearch}/rhsm/*
+
+%if %{use_python3}
+%files -n python3-subscription-manager-rhsm
+%defattr(-,root,root,-)
+%dir %{python3_sitearch}/rhsm
+%{python3_sitearch}/rhsm/*
+%endif
+
+%files -n subscription-manager-rhsm-certificates
+%attr(755,root,root) %dir %{_sysconfdir}/rhsm
+%attr(755,root,root) %dir %{_sysconfdir}/rhsm/ca
+
+%attr(644,root,root) %{_sysconfdir}/rhsm/ca/redhat-uep.pem
 
 
 %post

python-rhsm/Makefile
@@ -1,78 +1,0 @@
-SRC_DIR = src/rhsm
-
-STYLETESTS ?=
-PYFILES=`find  ${SRC_DIR} -name "*.py"`
-TESTFILES=`find test/ -name "*.py"`
-STYLEFILES=$(PYFILES)
-# note, set STYLETEST to something if you want
-# make stylish to check the tests code
-# as well
-
-ifdef STYLETESTS
-STYLEFILES+=$(TESTFILES)
-endif
-
-docs:
-	python setup.py build_sphinx
-
-check:
-	python setup.py -q nosetests -c playpen/noserc.dev
-
-coverage: coverage-jenkins
-
-coverage-html: coverage-jenkins
-
-coverage-jenkins:
-	python setup.py -q nosetests -c playpen/noserc.ci
-
-clean:
-	rm -f *~ *.bak *.tar.gz
-	find . -name "*.py[com]" | xargs rm -f
-	python setup.py clean --all
-	rm -rf cover/ htmlcov/ docs/sphinx/_build/ build/ dist/
-
-version_check:
-# needs https://github.com/alikins/pyqver
-	-@TMPFILE=`mktemp` || exit 1; \
-	pyqver2.py -v -m 2.5  $(STYLEFILES) | tee $$TMPFILE; \
-	! test -s $$TMPFILE
-
-flake8:
-	@TMPFILE=`mktemp` || exit 1; \
-	python setup.py -q flake8 -q | tee $$TMPFILE; \
-	! test -s $$TMPFILE
-
-pylint:
-	-@PYTHONPATH="src/:/usr/share/rhn:/usr/share/rhsm" pylint --rcfile=pylintrc $(STYLEFILES)
-
-tablint:
-	@! GREP_COLOR='7;31' grep --color -nP "^\W*\t" $(STYLEFILES)
-
-trailinglint:
-	@! GREP_COLOR='7;31'  grep --color -nP "[ \t]$$" $(STYLEFILES)
-
-whitespacelint: tablint trailinglint
-
-# look for things that are likely debugging code left in by accident
-debuglint:
-	@! GREP_COLOR='7;31' grep --color -nP "pdb.set_trace|pydevd.settrace|import ipdb|import pdb|import pydevd" $(STYLEFILES)
-
-gettext_lint:
-	@TMPFILE=`mktemp` || exit 1; \
-	pcregrep -n --color=auto -M  "_\(.*[\'|\"].*[\'|\"]\s*\+\s*[\"|\'].*[\"|\'].*\)" $(STYLEFILES) | tee $$TMPFILE; \
-	! test -s $$TMPFILE
-
-rpmlint:
-	@TMPFILE=`mktemp` || exit 1; \
-	rpmlint -f rpmlint.config python-rhsm.spec | grep -v "^.*packages and .* specfiles checked\;" | tee $$TMPFILE; \
-	! test -s $$TMPFILE
-
-versionlint:
-	@TMPFILE=`mktemp` || exit 1; \
-	pyqver2.py -m 2.7 -l $(STYLEFILES) | tee $$TMPFILE; \
-	! test -s $$TMPFILE
-
-.PHONY: stylish
-stylish: flake8 versionlint whitespacelint gettext_lint rpmlint debuglint
-
-jenkins: stylish coverage-jenkins

commit e29f6decb97711fe6d95f80dc030d7197d34d64e

build, spec: Rename a build flag to match its purpose (part 1)
The term 'sitelib' is used for locations where pure-Python modules are
installed, and 'sitearch' is where Python extension modules (written in
C or C++) are.  eg., Fedora uses the RPM macros python3_sitelib and
python3_sitearch [1].

In this case, the PYTHON_SITELIB build flag was being used to denote the
'sitearch' location because it was being assigned the value of Fedora's
python_sitearch.  Therefore, rename PYTHON_SITELIB as PYTHON_SITEARCH to
make the name of the flag match its purpose.

Fallout from f445b6486a962d12185a5afe69e768d0a605e175

[1] https://docs.fedoraproject.org/en-US/packaging-guidelines/Python/

Makefile
@@ -27,13 +27,13 @@ OS_DIST ?= $(shell rpm --eval='%dist')
 PYTHON_VER ?= $(shell $(PYTHON) -c 'import sys; print("python%s.%s" % sys.version_info[:2])')
 
 ifeq ($(OS_DIST), debian)
-  PYTHON_SITELIB ?= $(PREFIX)/lib/$(PYTHON_VER)/site-packages
+  PYTHON_SITEARCH ?= $(PREFIX)/lib/$(PYTHON_VER)/site-packages
 else
-  PYTHON_SITELIB ?= $(PREFIX)/lib64/$(PYTHON_VER)/site-packages
+  PYTHON_SITEARCH ?= $(PREFIX)/lib64/$(PYTHON_VER)/site-packages
 endif
 DNF_PLUGIN_PYTHON_SITELIB ?= $(PREFIX)/lib/$(PYTHON_VER)/site-packages
 # Note the underscore used instead of a hyphen
-PYTHON_INST_DIR = $(PYTHON_SITELIB)/subscription_manager
+PYTHON_INST_DIR = $(PYTHON_SITEARCH)/subscription_manager
 
 # Where various bits of code live in the git repo
 SRC_DIR := src/subscription_manager
@@ -207,7 +207,7 @@ install-via-setup: install-subpackages-via-setup
 	mv $(DESTDIR)/$(PREFIX)/bin/rhsm-service $(DESTDIR)/$(LIBEXEC_DIR)/
 	mv $(DESTDIR)/$(PREFIX)/bin/rhsm-facts-service $(DESTDIR)/$(LIBEXEC_DIR)/
 	mv $(DESTDIR)/$(PREFIX)/bin/rhsm-package-profile-uploader $(DESTDIR)/$(LIBEXEC_DIR)/
-	find $(DESTDIR)/$(PYTHON_SITELIB) -name requires.txt -exec sed -i '/dbus-python/d' {} \;
+	find $(DESTDIR)/$(PYTHON_SITEARCH) -name requires.txt -exec sed -i '/dbus-python/d' {} \;
 
 
 .PHONY: install-subpackages-via-setup

subscription-manager.spec
@@ -380,7 +380,7 @@ popd
 %install
 make -f Makefile install VERSION=%{version}-%{release} \
     PYTHON=%{__python} PREFIX=%{_prefix} \
-    DESTDIR=%{buildroot} PYTHON_SITELIB=%{python_sitearch} \
+    DESTDIR=%{buildroot} PYTHON_SITEARCH=%{python_sitearch} \
     OS_VERSION=%{?fedora}%{?rhel}%{?suse_version} OS_DIST=%{dist} \
     COMPLETION_DIR=%{completion_dir} \
     RUN_DIR=%{run_dir} \