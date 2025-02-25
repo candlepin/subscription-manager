candlepin/subscription-manager

commit 5e488ac158763f422352452bb09595bd68596541

1581445: ENT-564: rhsm configuration manage_repos is not working on RHEL8
* BZ: https://bugzilla.redhat.com/show_bug.cgi?id=1581445
* Moved dnf plugins to /usr/lib/python3.6/site-packages/dnf-plugins

Makefile
@@ -25,6 +25,7 @@ OS_DIST ?= $(shell rpm --eval='%dist')
 PYTHON_VER ?= $(shell $(PYTHON) -c 'import sys; print("python%s.%s" % sys.version_info[:2])')
 
 PYTHON_SITELIB ?= $(PREFIX)/lib64/$(PYTHON_VER)/site-packages
+DNF_PLUGIN_PYTHON_SITELIB ?= $(PREFIX)/lib/$(PYTHON_VER)/site-packages
 # Note the underscore used instead of a hyphen
 PYTHON_INST_DIR = $(PYTHON_SITELIB)/subscription_manager
 
@@ -111,7 +112,11 @@ build: rhsmcertd rhsm-icon
 # Install doesn't perform a build if it doesn't have too.  Best to clean out
 # any cruft so developers don't end up install old builds.
 	$(PYTHON) ./setup.py clean --all
-	$(PYTHON) ./setup.py build --quiet --gtk-version=$(GTK_VERSION) --rpm-version=$(VERSION)
+	if [ "$(OS)" = "RHEL" -a "$(OS_VERSION)" -gt 7 ] || [ "$(OS)" = "FEDORA" -a "$(OS_VERSION)" -gt 21 ] ; then \
+            $(PYTHON) ./setup.py build --quiet --gtk-version=$(GTK_VERSION) --rpm-version=$(VERSION) --use-dnf=true; \
+        else \
+            $(PYTHON) ./setup.py build --quiet --gtk-version=$(GTK_VERSION) --rpm-version=$(VERSION) --use-dnf=false; \
+        fi; \
 
 # we never "remake" this makefile, so add a target so
 # we stop searching for implicit rules on how to remake it
@@ -212,8 +217,10 @@ install-plugins:
 
 	if [ "$(INSTALL_DNF_PLUGINS)" = "true" ] ; then \
 		echo "Installing DNF plugins" ; \
-		install -d $(DESTDIR)/$(PYTHON_SITELIB)/dnf-plugins/ ; \
-		install -m 644 -p src/dnf-plugins/*.py $(DESTDIR)/$(PYTHON_SITELIB)/dnf-plugins/ ; \
+		install -d $(DESTDIR)/$(DNF_PLUGIN_PYTHON_SITELIB)/dnf-plugins/ ; \
+		install -d $(DESTDIR)/etc/dnf/plugins/ ; \
+		install -m 644 -p src/dnf-plugins/*.py $(DESTDIR)/$(DNF_PLUGIN_PYTHON_SITELIB)/dnf-plugins/ ; \
+		install -m 644 etc-conf/plugin/*.conf $(DESTDIR)/etc/dnf/plugins/ ; \
 	fi;
 
 	# ostree stuff

rpmlint.config
@@ -1,12 +1,16 @@
-# this seems ok to me
+# This seems ok to me
 addFilter('invalid-url')
-# all yum plugins seem to hardcode paths
+
+# All yum and dnf plugins seem to hardcode paths
 addFilter("hardcoded-library-path .*lib/yum-plugins/.*")
-# filter out zypper plugins too!
+addFilter("hardcoded-library-path .*lib/dnf-plugins/.*")
+
+# Filter out zypper plugins too!
 addFilter("hardcoded-library-path .*lib/zypp/plugins/.*")
-#sytemd tmpfiles are in /usr/lib
+
+# Systemd tmpfiles are in /usr/lib
 addFilter("hardcoded-library-path .*lib/tmpfiles.d/.*")
 
-# ignore failing suse specific checks
+# Ignore failing suse specific checks
 setBadness("suse-dbus-unauthorized-service", 0)
 setBadness("polkit-unauthorized-privilege", 0)

setup.py
@@ -45,20 +45,23 @@ from build_ext import i18n, lint, template, utils
 class rpm_version_release_build_py(_build_py):
     user_options = _build_py.user_options + [
         ('gtk-version=', None, 'GTK version this is built for'),
-        ('rpm-version=', None, 'version and release of the RPM this is built for')]
+        ('rpm-version=', None, 'version and release of the RPM this is built for'),
+        ('use-dnf=', None, 'True if dnf is used, False if yum is used')]
 
     def initialize_options(self):
         _build_py.initialize_options(self)
         self.rpm_version = None
         self.gtk_version = None
         self.versioned_packages = []
+        self.use_dnf = None
 
     def finalize_options(self):
         _build_py.finalize_options(self)
-        self.set_undefined_options('build', ('rpm_version', 'rpm_version'), ('gtk_version', 'gtk_version'))
+        self.set_undefined_options('build', ('rpm_version', 'rpm_version'), ('gtk_version', 'gtk_version'),
+            ('use_dnf', 'use_dnf'))
 
     def run(self):
-        log.info("Building with GTK_VERSION=%s and RPM_VERSION=%s" % (self.gtk_version, self.rpm_version))
+        log.info("Building with GTK_VERSION=%s, RPM_VERSION=%s and USE_DNF = %s" % (self.gtk_version, self.rpm_version, self.use_dnf))
         _build_py.run(self)
         # create a "version.py" that includes the rpm version
         # info passed to our new build_py args
@@ -72,6 +75,7 @@ class rpm_version_release_build_py(_build_py):
                         for l in f.readlines():
                             l = l.replace("RPM_VERSION", str(self.rpm_version))
                             l = l.replace("GTK_VERSION", str(self.gtk_version))
+                            l = l.replace("USE_DNF", str(self.use_dnf))
                             lines.append(l)
 
                     with open(version_file, 'w') as f:
@@ -85,6 +89,7 @@ class install(_install):
     user_options = _install.user_options + [
         ('gtk-version=', None, 'GTK version this is built for'),
         ('rpm-version=', None, 'version and release of the RPM this is built for'),
+        ('use-dnf=', None, 'True if dnf is used, False if yum is used'),
         ('with-systemd=', None, 'whether to install w/ systemd support or not'),
         ('with-subman-gui=', None, 'whether to install subman GUI or not'),
         ('with-cockpit-desktop-entry=', None, 'whether to install desktop entry for subman cockpit plugin or not')
@@ -94,24 +99,28 @@ class install(_install):
         _install.initialize_options(self)
         self.rpm_version = None
         self.gtk_version = None
+        self.use_dnf = None
         self.with_systemd = None
         self.with_subman_gui = None
         self.with_cockpit_desktop_entry = None
 
     def finalize_options(self):
         _install.finalize_options(self)
-        self.set_undefined_options('build', ('rpm_version', 'rpm_version'), ('gtk_version', 'gtk_version'))
+        self.set_undefined_options('build', ('rpm_version', 'rpm_version'), ('gtk_version', 'gtk_version'),
+            ('use_dnf', 'use_dnf'))
 
 
 class build(_build):
     user_options = _build.user_options + [
         ('gtk-version=', None, 'GTK version this is built for'),
-        ('rpm-version=', None, 'version and release of the RPM this is built for')]
+        ('rpm-version=', None, 'version and release of the RPM this is built for'),
+        ('use-dnf=', None, 'True if dnf is used, False if yum is used')]
 
     def initialize_options(self):
         _build.initialize_options(self)
         self.rpm_version = None
         self.gtk_version = None
+        self.use_dnf = None
         self.git_tag_prefix = "subscription-manager-"
 
     def finalize_options(self):
@@ -121,6 +130,9 @@ class build(_build):
 
         if not self.gtk_version:
             self.gtk_version = self.get_gtk_version()
+
+        if not self.use_dnf:
+            self.use_dnf = False
 
     def get_git_describe(self):
         try:

src/subscription_manager/repolib.py
@@ -27,6 +27,7 @@ import subscription_manager.injection as inj
 from subscription_manager.cache import OverrideStatusCache, WrittenOverrideCache
 from subscription_manager import utils
 from subscription_manager import model
+from subscription_manager import version
 from subscription_manager.model import ent_cert
 from six.moves.urllib.parse import parse_qs, urlparse, urlunparse, urlencode
 from six.moves import configparser
@@ -75,6 +76,7 @@ class YumPluginManager(object):
     """
 
     YUM_PLUGIN_DIR = '/etc/yum/pluginconf.d'
+    DNF_PLUGIN_DIR = '/etc/dnf/plugins'
 
     # List of yum plugins in YUM_PLUGIN_DIR which are automatically enabled
     # during sub-man CLI/GUI start
@@ -128,11 +130,16 @@ class YumPluginManager(object):
 
         # List of successfully enabled plugins
         enabled_yum_plugins = []
+        plugin_dir = ""
+        if version.use_dnf:
+            plugin_dir = cls.DNF_PLUGIN_DIR
+        else:
+            plugin_dir = cls.YUM_PLUGIN_DIR
 
         # Go through the list of yum plugins and try to find configuration
         # file of these plugins.
         for yum_plugin_name in cls.YUM_PLUGINS:
-            yum_plugin_file_name = cls.YUM_PLUGIN_DIR + '/' + yum_plugin_name + '.conf'
+            yum_plugin_file_name = plugin_dir + '/' + yum_plugin_name + '.conf'
             yum_plugin_config = ConfigParser()
             try:
                 result = yum_plugin_config.read(yum_plugin_file_name)

src/subscription_manager/version.py
@@ -3,3 +3,4 @@ from __future__ import print_function, division, absolute_import
 # The values in this file are populated by setup.py build
 rpm_version = "RPM_VERSION"
 gtk_version = "GTK_VERSION"
+use_dnf = "USE_DNF"

subscription-manager.spec
@@ -58,6 +58,7 @@
 
 %if %{with python3}
 %global python_sitearch %python3_sitearch
+%global python_sitelib %python3_sitelib
 %global __python %__python3
 %global py_package_prefix python%{python3_pkgversion}
 %global rhsm_package_name %{py_package_prefix}-subscription-manager-rhsm
@@ -541,9 +542,6 @@ desktop-file-validate %{buildroot}/usr/share/applications/subscription-manager-c
 
 %find_lang rhsm
 
-#%if %{use_subman_gui}
-#%find_lang %{name} --with-gnome
-#%endif
 
 # fake out the redhat.repo file
 %if %{use_yum} || %{use_dnf}
@@ -588,6 +586,12 @@ find %{buildroot} -name \*.py -exec touch -r %{SOURCE0} '{}' \;
     %dir %{_sysconfdir}/yum
     %dir %{_sysconfdir}/yum/pluginconf.d
     %dir %{_prefix}/lib/yum-plugins/
+%endif
+
+%if %{use_dnf}
+    %dir %{_sysconfdir}/dnf
+    %dir %{_sysconfdir}/dnf/plugins
+    %dir %{_prefix}/lib/dnf-plugins/
 %endif
 
 %if %{use_yum} || %{use_dnf}
@@ -654,6 +658,14 @@ find %{buildroot} -name \*.py -exec touch -r %{SOURCE0} '{}' \;
     %config(noreplace) %attr(644,root,root) %{_sysconfdir}/yum/pluginconf.d/subscription-manager.conf
     %config(noreplace) %attr(644,root,root) %{_sysconfdir}/yum/pluginconf.d/product-id.conf
     %config(noreplace) %attr(644,root,root) %{_sysconfdir}/yum/pluginconf.d/search-disabled-repos.conf
+%endif
+
+# dnf plugin config
+%if %{use_dnf}
+    # remove the repo file when we are deleted
+    %config(noreplace) %attr(644,root,root) %{_sysconfdir}/dnf/plugins/subscription-manager.conf
+    %config(noreplace) %attr(644,root,root) %{_sysconfdir}/dnf/plugins/product-id.conf
+    %config(noreplace) %attr(644,root,root) %{_sysconfdir}/dnf/plugins/search-disabled-repos.conf
 %endif
 
 # misc system config
@@ -960,7 +972,7 @@ find %{buildroot} -name \*.py -exec touch -r %{SOURCE0} '{}' \;
 %if %use_dnf
 %files -n dnf-plugin-subscription-manager
 %defattr(-,root,root,-)
-%{python_sitearch}/dnf-plugins/*
+%{python_sitelib}/dnf-plugins/*
 %endif
 
 

test/test_repolib.py
@@ -947,7 +947,7 @@ class TestYumPluginManager(unittest.TestCase):
     This class is intended for testing YumPluginManager
     """
 
-    ORIGINAL_YUM_PLUGIN_DIR = YumPluginManager.YUM_PLUGIN_DIR
+    ORIGINAL_DNF_PLUGIN_DIR = YumPluginManager.DNF_PLUGIN_DIR
     ORIGINAL_YUM_PLUGINS = YumPluginManager.YUM_PLUGINS
 
     def init_plugin_conf_files(self, conf_string):
@@ -964,7 +964,7 @@ class TestYumPluginManager(unittest.TestCase):
         self.plug_file_name_01 = plug_file_name_01
         self.plug_file_name_02 = plug_file_name_02
         self.tmp_dir = tmp_dir
-        YumPluginManager.YUM_PLUGIN_DIR = tmp_dir
+        YumPluginManager.DNF_PLUGIN_DIR = tmp_dir
         YumPluginManager.YUM_PLUGINS = [
             plug_file_name_01.replace(tmp_dir, '').replace('.conf', ''),
             plug_file_name_02.replace(tmp_dir, '').replace('.conf', '')
@@ -974,7 +974,7 @@ class TestYumPluginManager(unittest.TestCase):
         """
         Restore original constants in YumPluginManager
         """
-        YumPluginManager.YUM_PLUGIN_DIR = self.ORIGINAL_YUM_PLUGIN_DIR
+        YumPluginManager.DNF_PLUGIN_DIR = self.ORIGINAL_DNF_PLUGIN_DIR
         YumPluginManager.YUM_PLUGINS = self.ORIGINAL_YUM_PLUGINS
         os.unlink(self.plug_file_name_01)
         os.unlink(self.plug_file_name_02)
@@ -990,7 +990,7 @@ class TestYumPluginManager(unittest.TestCase):
         self.assertEqual(len(plugin_list), 2)
         for plugin_conf_file_name in YumPluginManager.YUM_PLUGINS:
             yum_plugin_config = ConfigParser()
-            result = yum_plugin_config.read(YumPluginManager.YUM_PLUGIN_DIR + '/' + plugin_conf_file_name + '.conf')
+            result = yum_plugin_config.read(YumPluginManager.DNF_PLUGIN_DIR + '/' + plugin_conf_file_name + '.conf')
             self.assertGreater(len(result), 0)
             is_plugin_enabled = yum_plugin_config.getint('main', 'enabled')
             self.assertEqual(is_plugin_enabled, 1)
@@ -1006,7 +1006,7 @@ class TestYumPluginManager(unittest.TestCase):
         self.assertEqual(len(plugin_list), 0)
         for plugin_conf_file_name in YumPluginManager.YUM_PLUGINS:
             yum_plugin_config = ConfigParser()
-            result = yum_plugin_config.read(YumPluginManager.YUM_PLUGIN_DIR + '/' + plugin_conf_file_name + '.conf')
+            result = yum_plugin_config.read(YumPluginManager.DNF_PLUGIN_DIR + '/' + plugin_conf_file_name + '.conf')
             self.assertGreater(len(result), 0)
             is_plugin_enabled = yum_plugin_config.getboolean('main', 'enabled')
             self.assertEqual(is_plugin_enabled, False)
@@ -1022,7 +1022,7 @@ class TestYumPluginManager(unittest.TestCase):
         self.assertEqual(len(plugin_list), 0)
         for plugin_conf_file_name in YumPluginManager.YUM_PLUGINS:
             yum_plugin_config = ConfigParser()
-            result = yum_plugin_config.read(YumPluginManager.YUM_PLUGIN_DIR + '/' + plugin_conf_file_name + '.conf')
+            result = yum_plugin_config.read(YumPluginManager.DNF_PLUGIN_DIR + '/' + plugin_conf_file_name + '.conf')
             self.assertGreater(len(result), 0)
             is_plugin_enabled = yum_plugin_config.getint('main', 'enabled')
             self.assertEqual(is_plugin_enabled, 1)
@@ -1038,7 +1038,7 @@ class TestYumPluginManager(unittest.TestCase):
         self.assertEqual(len(plugin_list), 0)
         for plugin_conf_file_name in YumPluginManager.YUM_PLUGINS:
             yum_plugin_config = ConfigParser()
-            result = yum_plugin_config.read(YumPluginManager.YUM_PLUGIN_DIR + '/' + plugin_conf_file_name + '.conf')
+            result = yum_plugin_config.read(YumPluginManager.DNF_PLUGIN_DIR + '/' + plugin_conf_file_name + '.conf')
             self.assertGreater(len(result), 0)
             # The file was not modified. We have to read value with with getboolean()
             is_plugin_enabled = yum_plugin_config.getboolean('main', 'enabled')
@@ -1055,7 +1055,7 @@ class TestYumPluginManager(unittest.TestCase):
         self.assertEqual(len(plugin_list), 2)
         for plugin_conf_file_name in YumPluginManager.YUM_PLUGINS:
             yum_plugin_config = ConfigParser()
-            result = yum_plugin_config.read(YumPluginManager.YUM_PLUGIN_DIR + '/' + plugin_conf_file_name + '.conf')
+            result = yum_plugin_config.read(YumPluginManager.DNF_PLUGIN_DIR + '/' + plugin_conf_file_name + '.conf')
             self.assertGreater(len(result), 0)
             is_plugin_enabled = yum_plugin_config.getint('main', 'enabled')
             self.assertEqual(is_plugin_enabled, 1)
@@ -1071,7 +1071,7 @@ class TestYumPluginManager(unittest.TestCase):
         self.assertEqual(len(plugin_list), 2)
         for plugin_conf_file_name in YumPluginManager.YUM_PLUGINS:
             yum_plugin_config = ConfigParser()
-            result = yum_plugin_config.read(YumPluginManager.YUM_PLUGIN_DIR + '/' + plugin_conf_file_name + '.conf')
+            result = yum_plugin_config.read(YumPluginManager.DNF_PLUGIN_DIR + '/' + plugin_conf_file_name + '.conf')
             self.assertGreater(len(result), 0)
             is_plugin_enabled = yum_plugin_config.getint('main', 'enabled')
             self.assertEqual(is_plugin_enabled, 1)

commit 5653f4c3c10b435aa5b00398bd0d5bb9a68600a2

build: Rename a build flag to match its purpose (part 2)
The location specified by the DNF_PLUGIN_PYTHON_SITELIB build flag was
not specific to DNF plugins.  It was the top-level 'sitelib' location
where pure-Python modules are installed.  Therefore, rename the flag as
PYTHON_SITELIB to make the name of the flag match its purpose.

Fallout from 5e488ac158763f422352452bb09595bd68596541

Makefile
@@ -31,7 +31,7 @@ ifeq ($(OS_DIST), debian)
 else
   PYTHON_SITEARCH ?= $(PREFIX)/lib64/$(PYTHON_VER)/site-packages
 endif
-DNF_PLUGIN_PYTHON_SITELIB ?= $(PREFIX)/lib/$(PYTHON_VER)/site-packages
+PYTHON_SITELIB ?= $(PREFIX)/lib/$(PYTHON_VER)/site-packages
 # Note the underscore used instead of a hyphen
 PYTHON_INST_DIR = $(PYTHON_SITEARCH)/subscription_manager
 
@@ -159,14 +159,14 @@ install-plugins:
 
 	if [ "$(INSTALL_DNF_PLUGINS)" = "true" ] ; then \
 		echo "Installing DNF plugins" ; \
-		install -d $(DESTDIR)/$(DNF_PLUGIN_PYTHON_SITELIB)/dnf-plugins/ ; \
+		install -d $(DESTDIR)/$(PYTHON_SITELIB)/dnf-plugins/ ; \
 		install -d $(DESTDIR)/etc/dnf/plugins/ ; \
 		install -m 644 -p src/plugins/dnf/product_id.py \
-		    $(DESTDIR)/$(DNF_PLUGIN_PYTHON_SITELIB)/dnf-plugins/product-id.py ; \
+		    $(DESTDIR)/$(PYTHON_SITELIB)/dnf-plugins/product-id.py ; \
 		install -m 644 -p src/plugins/dnf/subscription_manager.py \
-		    $(DESTDIR)/$(DNF_PLUGIN_PYTHON_SITELIB)/dnf-plugins/subscription-manager.py ; \
+		    $(DESTDIR)/$(PYTHON_SITELIB)/dnf-plugins/subscription-manager.py ; \
 		install -m 644 -p src/plugins/dnf/upload_profile.py \
-		    $(DESTDIR)/$(DNF_PLUGIN_PYTHON_SITELIB)/dnf-plugins/upload-profile.py ; \
+		    $(DESTDIR)/$(PYTHON_SITELIB)/dnf-plugins/upload-profile.py ; \
 		install -m 644 etc-conf/plugin/product-id.conf $(DESTDIR)/etc/dnf/plugins/ ; \
 		install -m 644 etc-conf/plugin/subscription-manager.conf $(DESTDIR)/etc/dnf/plugins/ ; \
 	fi;