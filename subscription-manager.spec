candlepin/subscription-manager

commit a0468a45999bc9cf988a59470219f4da73f0dfaa

spec: drop support for Fedora < 33
Currently, Fedora 33 is the earlier supported version, and any previous
version is EOL.

subscription-manager.spec
@@ -24,7 +24,7 @@
 %bcond_without python3
 %endif
 
-%if !(0%{?fedora} < 30 && %{with python3}) || 0%{?rhel}
+%if 0%{?fedora} || 0%{?rhel}
 %bcond_with python2_rhsm
 %else
 %bcond_without python2_rhsm
@@ -42,7 +42,7 @@
 %endif
 
 %global use_dnf (%{with python3} && (0%{?fedora} || (0%{?rhel}))) || (0%{?rhel})
-%global create_libdnf_rpm (0%{?fedora} > 32 || 0%{?rhel} > 8)
+%global create_libdnf_rpm (0%{?fedora} || 0%{?rhel} > 8)
 %global use_cockpit 0%{?fedora} || 0%{?rhel}
 
 %if %{with python3}
@@ -352,18 +352,13 @@ Group: Productivity/Networking/System
 Group: System Environment/Base
 %endif
 
-%if (0%{?fedora} >= 29 || 0%{?rhel})
+%if (0%{?fedora} || 0%{?rhel})
 BuildRequires: cmake
 BuildRequires: gcc
 BuildRequires: json-c-devel
 BuildRequires: libdnf-devel >= 0.22.5
 Requires: json-c
 Requires: libdnf >= 0.22.5
-%endif
-
-# See BZ 1581410 - avoid a circular dependency
-%if (0%{?fedora} < 29)
-Requires: %{name} >= %{version}-%{release}
 %endif
 
 %if %{with python3}

commit fb17ff35ef514cec3141aa9873ccb98871699801

spec: Remove a condition that's always false
The DNF plugin is never built on Fedora.  Therefore, it's pointless to
check for Fedora when building it.

In the past, the DNF plugin used to be built on Fedora 29 to 32, and the
libdnf plugin used to be built on Fedora 33 onwards.  That's why it made
sense to check for Fedora for the DNF plugin.  However, that's no longer
the case.

Fallout from a0468a45999bc9cf988a59470219f4da73f0dfaa

subscription-manager.spec
@@ -258,7 +258,7 @@ e.g. microdnf.
 %package -n dnf-plugin-subscription-manager
 Summary: Subscription Manager plugins for DNF
 
-%if (0%{?fedora} || 0%{?rhel})
+%if 0%{?rhel}
 BuildRequires: cmake
 BuildRequires: gcc
 BuildRequires: json-c-devel