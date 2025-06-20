candlepin/subscription-manager

commit 2da9d9973b85fd938d4c53dd3b995b82d0ec33b7

Skip to load default plugins on suse/sles

src/subscription_manager/repolib.py
@@ -182,7 +182,15 @@ class YumPluginManager:
 
         dist_info = HardwareCollector().get_distribution()
 
-        if dist_info[4] == "debian" or "debian" in dist_info[5]:
+        dist_id = dist_info[4]
+        dist_id_like = dist_info[5]
+
+        # If the system is Debian/SUSE based, do not enable yum plugins
+        if (
+            dist_id == "debian" or "debian" in dist_id_like or
+            dist_id == "sles" or "suse" in dist_id_like
+        ):
+            log.debug("The system is Debian/SUSE based. Skipping the enablement of yum plugins.")
             return []
 
         log.debug("The rhsm.auto_enable_yum_plugins is enabled")

test/test_repolib.py
@@ -1432,6 +1432,28 @@ class TestYumPluginManager(unittest.TestCase):
         plugin_list = YumPluginManager.enable_pkg_plugins()
         self.assertEqual(len(plugin_list), 0)
 
+    @patch(
+        "rhsmlib.facts.hwprobe.HardwareCollector.get_distribution",
+        return_value=("", "", "", "", "sles", []),
+    )
+    def test_enable_pkg_plugins_sles(self, mock_get_distribution):
+        """
+        Test not enabling of yum plugins on sles based systems (Test 1 of 2)
+        """
+        plugin_list = YumPluginManager.enable_pkg_plugins()
+        self.assertEqual(len(plugin_list), 0)
+
+    @patch(
+        "rhsmlib.facts.hwprobe.HardwareCollector.get_distribution",
+        return_value=("", "", "", "", "", ["suse"]),
+    )
+    def test_enable_pkg_plugins_suse_like(self, mock_get_distribution):
+        """
+        Test not enabling of yum plugins on suse based systems (Test 2 of 2)
+        """
+        plugin_list = YumPluginManager.enable_pkg_plugins()
+        self.assertEqual(len(plugin_list), 0)
+
     @patch.object(repolib, "conf", ConfigFromString(config_string=AUTO_ENABLE_PKG_PLUGINS_ENABLED))
     @patch(
         "rhsmlib.facts.hwprobe.HardwareCollector.get_distribution",

commit c86920793f35d471d4c4b1dfb694092fd9d90f8a

chore: Fix formatting of code from previous commit
* Fix style issue introduced in previous commit:
  2da9d9973b85fd938d4c53dd3b995b82d0ec33b7

Signed-off-by: Jiri Hnidek <jhnidek@redhat.com>

src/subscription_manager/repolib.py
@@ -186,10 +186,7 @@ class YumPluginManager:
         dist_id_like = dist_info[5]
 
         # If the system is Debian/SUSE based, do not enable yum plugins
-        if (
-            dist_id == "debian" or "debian" in dist_id_like or
-            dist_id == "sles" or "suse" in dist_id_like
-        ):
+        if dist_id == "debian" or "debian" in dist_id_like or dist_id == "sles" or "suse" in dist_id_like:
             log.debug("The system is Debian/SUSE based. Skipping the enablement of yum plugins.")
             return []
 