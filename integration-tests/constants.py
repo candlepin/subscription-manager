9d2a8b8c0ffe9a65b47dc915e35c23ff58109e7e
Jan Stavel <jstavel@redhat.com>
2025-07-17 10:27:54 +0200

    CCT-743: integration tests for DBus Products object

--- integration-tests/constants.py
+++ integration-tests/constants.py
@@ -1,10 +1,11 @@
 from dasbus.identifier import DBusObjectIdentifier, DBusServiceIdentifier
 from dasbus.connection import SystemMessageBus
-
+from pathlib import Path
 
 HOST_DETAILS: str = "/var/lib/insights/host-details.json"
 MACHINE_ID_FILE: str = "/etc/insights-client/machine-id"
 RHSM_CONFIG_FILE_PATH: str = "/etc/rhsm/rhsm.conf"
+TEST_PRODUCT_CERT_PATHS: list[Path] = list((Path(__file__).parent / "files" / "product").iterdir())
 
 RHSM_NAMESPACE = ("com", "redhat", "RHSM1")
 
@@ -23,3 +24,5 @@ RHSM_ENTITLEMENT = DBusObjectIdentifier(namespace=RHSM_NAMESPACE, basename="Enti
 RHSM_SYSPURPOSE = DBusObjectIdentifier(namespace=RHSM_NAMESPACE, basename="Syspurpose")
 
 RHSM_CONSUMER = DBusObjectIdentifier(namespace=RHSM_NAMESPACE, basename="Consumer")
+
+RHSM_PRODUCTS = DBusObjectIdentifier(namespace=RHSM_NAMESPACE, basename="Products")
--- integration-tests/utils.py
+++ integration-tests/utils.py
@@ -1,4 +1,6 @@
 import time
+import re
+from typing import Generator
 
 
 def loop_until(predicate, poll_sec=5, timeout_sec=120):
@@ -19,3 +21,47 @@ def loop_until(predicate, poll_sec=5, timeout_sec=120):
         time.sleep(poll_sec)
         ok = predicate()
     return ok
+
+
+def installed_products(subman) -> list[dict[str, str]]:
+    """
+    The function returns a list of products that subscription-manager provides
+    """
+    subman_response = subman.run("list", "--installed", check=False)
+    #
+    # [root@ad5327bd-5cc5-4a6c-b162-f1cf6d9bd61f integration-tests]# subscription-manager list --installed
+    # +-------------------------------------------+
+    # Installed Product Status
+    # +-------------------------------------------+
+    # Product Name: Red Hat Enterprise Linux for x86_64 Beta
+    # Product ID:   486
+    # Version:      10.1 Beta
+    # Arch:         x86_64
+    #
+    # Product Name: Red Hat Enterprise Linux Builder for x86_64 Beta
+    # Product ID:   495
+    # Version:      10.1 Beta
+    # Arch:         x86_64
+    #
+
+    def read_products(lines: list[str]) -> Generator[dict[str, str], None, None]:
+        """
+        parse an output from the command to find product's properties
+        """
+        product = {}
+        for line in lines:
+            if not line.strip():  # an empty line between two products
+                if len(product.items()) > 0:
+                    yield product
+                    product = {}
+                else:
+                    continue
+            result = re.search(r"^([^:]+):(.*)", line.strip())
+            if result:
+                pair = [g.strip() for g in result.groups()]
+                product[pair[0]] = pair[1]
+
+        if len(product.items()) > 0:
+            yield product
+
+    return list(read_products(subman_response.stdout.splitlines()))
