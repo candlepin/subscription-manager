43519d83c4f0074e2d8186422e7d140f54c834e3
Jan Stavel <jstavel@redhat.com>
2025-07-30 15:32:19 +0200

    CCT-742: integration tests for DBus Syspurpose object

--- integration-tests/constants.py
+++ integration-tests/constants.py
@@ -1,10 +1,12 @@
 from dasbus.identifier import DBusObjectIdentifier, DBusServiceIdentifier
 from dasbus.connection import SystemMessageBus
-
+from pathlib import Path
 
 HOST_DETAILS: str = "/var/lib/insights/host-details.json"
 MACHINE_ID_FILE: str = "/etc/insights-client/machine-id"
 RHSM_CONFIG_FILE_PATH: str = "/etc/rhsm/rhsm.conf"
+RHSM_SYSPURPOSE_FILE_PATH: Path = Path("/etc/rhsm/syspurpose/syspurpose.json")
+RHSM_VALID_FIELDS_FILE_PATH: Path = Path("/etc/rhsm/syspurpose/valid_fields.json")
 
 RHSM_NAMESPACE = ("com", "redhat", "RHSM1")
 
