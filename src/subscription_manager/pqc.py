de56764da71be2078af5aa05ecfdd50f2ff0ee8e
Joshua Locash <jlocash@redhat.com>
2026-05-14 09:32:12 -0400

    fix: drop __smoke_test__ and __main__ from pqc.py
    Signed-off-by: Joshua Locash <jlocash@redhat.com>

--- src/subscription_manager/pqc.py
+++ src/subscription_manager/pqc.py
@@ -1,8 +1,7 @@
 from rhsm import _certificate
 
 """
-This is POC of new functionality of subscription-manager. This module helps to negotiate
-PQC with candlepin server.
+This module helps to negotiate PQC with candlepin server.
 """
 
 
@@ -24,34 +23,3 @@ def get_public_key_algorithms():
     retrieved directly from OpenSSL via the C bindings.
     """
     return _certificate.get_public_key_algorithms()
-
-
-def get_pub_key_and_sign_algorithms():
-    """
-    Returns a list containing supported public key and signature algorithms.
-    """
-    pub_key_algorithms = get_public_key_algorithms()
-    sig_algorithms = get_signature_algorithms()
-    all_algorithms = set(pub_key_algorithms).union(set(sig_algorithms))
-    return list(all_algorithms)
-
-
-def __smoke_test__():
-    """
-    Smoke testing of get_public_key_algorithms()
-    """
-    pub_key_algorithms = get_public_key_algorithms()
-    sig_algorithms = get_signature_algorithms()
-    print("Supported public key & signature algorithms:")
-    for algorithm in pub_key_algorithms:
-        if algorithm in sig_algorithms:
-            print(f"- {algorithm} (sig+pub)")
-        else:
-            print(f"- {algorithm} (pub only)")
-    for algorithm in sig_algorithms:
-        if algorithm not in pub_key_algorithms:
-            print(f"- {algorithm} (sig only)")
-
-
-if __name__ == "__main__":
-    __smoke_test__()
