0ef49623f2bdcf0efe457a62bbe443235fa15c1d
Joshua Locash <jlocash@redhat.com>
2026-05-20 08:24:36 -0400

    fix(pqc): raise RuntimeError when get_signature_algorithms or get_public_key_algorithms throw
    * When an exception is caught from underlying C method, it is re-raised as a RuntimeError with an appropriate error message
    
    Signed-off-by: Joshua Locash <jlocash@redhat.com>

--- src/subscription_manager/pqc.py
+++ src/subscription_manager/pqc.py
@@ -12,7 +12,10 @@ def get_signature_algorithms():
     Returns a list of OID strings representing available signature algorithms,
     retrieved directly from OpenSSL via the C bindings.
     """
-    return _certificate.get_signature_algorithms()
+    try:
+        return _certificate.get_signature_algorithms()
+    except Exception as e:
+        raise RuntimeError("Failed to get list of signature algorithms") from e
 
 
 def get_public_key_algorithms():
@@ -22,4 +25,7 @@ def get_public_key_algorithms():
     Returns a list of OID strings representing available public key algorithms,
     retrieved directly from OpenSSL via the C bindings.
     """
-    return _certificate.get_public_key_algorithms()
+    try:
+        return _certificate.get_public_key_algorithms()
+    except Exception as e:
+        raise RuntimeError("Failed to get list of public key algorithms") from e
