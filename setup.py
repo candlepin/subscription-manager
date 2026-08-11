90a899ebffd25efc989e2f044fb8cd8235b697f2
Christopher Snyder <csnyder@redhat.com>
2026-08-11 14:43:52 -0400

    Automatic commit of package [subscription-manager] release [1.30.16-1].
    Created by command:
    
    /usr/bin/tito tag
    
    Signed-off-by: Christopher Snyder <csnyder@redhat.com>

--- setup.py
+++ setup.py
@@ -262,7 +262,7 @@ cmdclass = {
 
 setup(
     name="subscription-manager",
-    version="1.30.15",
+    version="1.30.16",
     url="http://www.candlepinproject.org",
     description="Manage subscriptions for Red Hat products.",
     license="GPLv2",
